d51aa9b785c66daf20d3946464b9a5c2e61c3a33
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2018-08-21 16:49:14 +0900

    kubelet/cm: ignore cgroups error when running in userns
    This is a hacky POC; we need to implement rootless PCM in the proper way.
    Especially, pcm.Exists(existingPodName) needs to be implemented to return true
    even when cgroups is not available.
    
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- pkg/kubelet/dockershim/cm/container_manager_linux.go
+++ pkg/kubelet/dockershim/cm/container_manager_linux.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/opencontainers/runc/libcontainer/cgroups/fs"
 	"github.com/opencontainers/runc/libcontainer/configs"
+	rsystem "github.com/opencontainers/runc/libcontainer/system"
 	utilversion "k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/klog"
@@ -95,7 +96,10 @@ func (m *containerManager) doWork() {
 	//   1. Ensure processes run in the cgroups if m.cgroupsManager is not nil.
 	//   2. Ensure processes have the OOM score applied.
 	if err := kubecm.EnsureDockerInContainer(version, dockerOOMScoreAdj, m.cgroupsManager); err != nil {
-		klog.Errorf("Unable to ensure the docker processes run in the desired containers: %v", err)
+		// if we are in userns, the operation is likely to fail, unless cgroupfs is properly chown-ed.
+		if !rsystem.RunningInUserNS() {
+			klog.Errorf("Unable to ensure the docker processes run in the desired containers: %v", err)
+		}
 	}
 }
 
