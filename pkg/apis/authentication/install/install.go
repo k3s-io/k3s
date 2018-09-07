439563abe9128dc28a4429d3e18d35a66a6b078f
Darren Shepherd <darren@rancher.com>
2018-09-07 16:06:28 -0700

    Remove authentication/v1beta1

--- pkg/apis/authentication/install/install.go
+++ pkg/apis/authentication/install/install.go
@@ -24,7 +24,6 @@ import (
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
 	"k8s.io/kubernetes/pkg/apis/authentication"
 	"k8s.io/kubernetes/pkg/apis/authentication/v1"
-	"k8s.io/kubernetes/pkg/apis/authentication/v1beta1"
 )
 
 func init() {
@@ -34,7 +33,6 @@ func init() {
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(authentication.AddToScheme(scheme))
-	utilruntime.Must(v1beta1.AddToScheme(scheme))
 	utilruntime.Must(v1.AddToScheme(scheme))
-	utilruntime.Must(scheme.SetVersionPriority(v1.SchemeGroupVersion, v1beta1.SchemeGroupVersion))
+	utilruntime.Must(scheme.SetVersionPriority(v1.SchemeGroupVersion))
 }
