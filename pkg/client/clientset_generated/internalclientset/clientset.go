24ccccc301c657f7a0ab9454cd97b67977480872
Darren Shepherd <darren@rancher.com>
2018-09-07 19:02:04 -0700

    Remove events

--- pkg/client/clientset_generated/internalclientset/clientset.go
+++ pkg/client/clientset_generated/internalclientset/clientset.go
@@ -30,7 +30,6 @@ import (
 	batchinternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/batch/internalversion"
 	coordinationinternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/coordination/internalversion"
 	coreinternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/core/internalversion"
-	eventsinternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/events/internalversion"
 	extensionsinternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/extensions/internalversion"
 	networkinginternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/networking/internalversion"
 	policyinternalversion "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/policy/internalversion"
@@ -50,7 +49,6 @@ type Interface interface {
 	Autoscaling() autoscalinginternalversion.AutoscalingInterface
 	Batch() batchinternalversion.BatchInterface
 	Coordination() coordinationinternalversion.CoordinationInterface
-	Events() eventsinternalversion.EventsInterface
 	Extensions() extensionsinternalversion.ExtensionsInterface
 	Networking() networkinginternalversion.NetworkingInterface
 	Policy() policyinternalversion.PolicyInterface
@@ -72,7 +70,6 @@ type Clientset struct {
 	autoscaling           *autoscalinginternalversion.AutoscalingClient
 	batch                 *batchinternalversion.BatchClient
 	coordination          *coordinationinternalversion.CoordinationClient
-	events                *eventsinternalversion.EventsClient
 	extensions            *extensionsinternalversion.ExtensionsClient
 	networking            *networkinginternalversion.NetworkingClient
 	policy                *policyinternalversion.PolicyClient
@@ -120,11 +117,6 @@ func (c *Clientset) Batch() batchinternalversion.BatchInterface {
 // Coordination retrieves the CoordinationClient
 func (c *Clientset) Coordination() coordinationinternalversion.CoordinationInterface {
 	return c.coordination
-}
-
-// Events retrieves the EventsClient
-func (c *Clientset) Events() eventsinternalversion.EventsInterface {
-	return c.events
 }
 
 // Extensions retrieves the ExtensionsClient
@@ -210,10 +202,6 @@ func NewForConfig(c *rest.Config) (*Clientset, error) {
 	if err != nil {
 		return nil, err
 	}
-	cs.events, err = eventsinternalversion.NewForConfig(&configShallowCopy)
-	if err != nil {
-		return nil, err
-	}
 	cs.extensions, err = extensionsinternalversion.NewForConfig(&configShallowCopy)
 	if err != nil {
 		return nil, err
@@ -262,7 +250,6 @@ func NewForConfigOrDie(c *rest.Config) *Clientset {
 	cs.autoscaling = autoscalinginternalversion.NewForConfigOrDie(c)
 	cs.batch = batchinternalversion.NewForConfigOrDie(c)
 	cs.coordination = coordinationinternalversion.NewForConfigOrDie(c)
-	cs.events = eventsinternalversion.NewForConfigOrDie(c)
 	cs.extensions = extensionsinternalversion.NewForConfigOrDie(c)
 	cs.networking = networkinginternalversion.NewForConfigOrDie(c)
 	cs.policy = policyinternalversion.NewForConfigOrDie(c)
@@ -286,7 +273,6 @@ func New(c rest.Interface) *Clientset {
 	cs.autoscaling = autoscalinginternalversion.New(c)
 	cs.batch = batchinternalversion.New(c)
 	cs.coordination = coordinationinternalversion.New(c)
-	cs.events = eventsinternalversion.New(c)
 	cs.extensions = extensionsinternalversion.New(c)
 	cs.networking = networkinginternalversion.New(c)
 	cs.policy = policyinternalversion.New(c)
