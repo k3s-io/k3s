4f4f88b67016b8b331a7933191c0e1291f4cbe60
Darren Shepherd <darren@rancher.com>
2018-09-08 20:58:32 -0700

    Remove settings

--- staging/src/k8s.io/client-go/kubernetes/scheme/register.go
+++ staging/src/k8s.io/client-go/kubernetes/scheme/register.go
@@ -47,7 +47,6 @@ import (
 	schedulingv1 "k8s.io/api/scheduling/v1"
 	schedulingv1alpha1 "k8s.io/api/scheduling/v1alpha1"
 	schedulingv1beta1 "k8s.io/api/scheduling/v1beta1"
-	settingsv1alpha1 "k8s.io/api/settings/v1alpha1"
 	storagev1 "k8s.io/api/storage/v1"
 	storagev1beta1 "k8s.io/api/storage/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -89,7 +88,6 @@ var localSchemeBuilder = runtime.SchemeBuilder{
 	schedulingv1alpha1.AddToScheme,
 	schedulingv1beta1.AddToScheme,
 	schedulingv1.AddToScheme,
-	settingsv1alpha1.AddToScheme,
 	storagev1beta1.AddToScheme,
 	storagev1.AddToScheme,
 }
