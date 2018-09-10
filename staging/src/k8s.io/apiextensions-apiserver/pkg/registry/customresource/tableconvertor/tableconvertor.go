d4f4602f7a266d043b548c29e8889d8c0ded3473
Darren Shepherd <darren@rancher.com>
2018-09-10 13:23:09 -0700

    Remove SwaggerDoc

--- staging/src/k8s.io/apiextensions-apiserver/pkg/registry/customresource/tableconvertor/tableconvertor.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/registry/customresource/tableconvertor/tableconvertor.go
@@ -33,13 +33,11 @@ import (
 	"k8s.io/client-go/util/jsonpath"
 )
 
-var swaggerMetadataDescriptions = metav1.ObjectMeta{}.SwaggerDoc()
-
 // New creates a new table convertor for the provided CRD column definition. If the printer definition cannot be parsed,
 // error will be returned along with a default table convertor.
 func New(crdColumns []apiextensions.CustomResourceColumnDefinition) (rest.TableConvertor, error) {
 	headers := []metav1beta1.TableColumnDefinition{
-		{Name: "Name", Type: "string", Format: "name", Description: swaggerMetadataDescriptions["name"]},
+		{Name: "Name", Type: "string", Format: "name", Description: ""},
 	}
 	c := &convertor{
 		headers: headers,
