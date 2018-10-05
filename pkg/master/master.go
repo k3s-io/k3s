061cbe05cb26a214b2cff1cae07e08e50e77515c
Darren Shepherd <darren@rancher.com>
2018-10-04 22:31:18 -0700

    Remove scheduling/v1alpha1

--- pkg/master/master.go
+++ pkg/master/master.go
@@ -41,7 +41,6 @@ import (
 	policyapiv1beta1 "k8s.io/api/policy/v1beta1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	rbacv1beta1 "k8s.io/api/rbac/v1beta1"
-	schedulingv1alpha1 "k8s.io/api/scheduling/v1alpha1"
 	schedulingapiv1beta1 "k8s.io/api/scheduling/v1beta1"
 	storageapiv1 "k8s.io/api/storage/v1"
 	storageapiv1beta1 "k8s.io/api/storage/v1beta1"
@@ -482,7 +481,6 @@ func DefaultAPIResourceConfigSource() *serverstorage.ResourceConfig {
 	// disable alpha versions explicitly so we have a full list of what's possible to serve
 	ret.DisableVersions(
 		batchapiv2alpha1.SchemeGroupVersion,
-		schedulingv1alpha1.SchemeGroupVersion,
 	)
 
 	return ret
