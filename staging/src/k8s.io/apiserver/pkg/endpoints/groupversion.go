df876f53b0a2ee7609ed545fecf8fe23b0631408
Darren Shepherd <darren@rancher.com>
2018-10-05 11:48:16 -0700

    Remove APIResponseCompression

--- staging/src/k8s.io/apiserver/pkg/endpoints/groupversion.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/groupversion.go
@@ -81,10 +81,6 @@ type APIGroupVersion struct {
 
 	MinRequestTimeout time.Duration
 
-	// EnableAPIResponseCompression indicates whether API Responses should support compression
-	// if the client requests it via Accept-Encoding
-	EnableAPIResponseCompression bool
-
 	// OpenAPIModels exposes the OpenAPI models to each individual handler.
 	OpenAPIModels openapiproto.Models
 }
@@ -98,7 +94,6 @@ func (g *APIGroupVersion) InstallREST(container *restful.Container) error {
 		group:                        g,
 		prefix:                       prefix,
 		minRequestTimeout:            g.MinRequestTimeout,
-		enableAPIResponseCompression: g.EnableAPIResponseCompression,
 	}
 
 	apiResources, ws, registrationErrors := installer.Install()
