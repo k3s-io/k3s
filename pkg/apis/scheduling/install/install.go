05d7fc76d770b5db018074289869e6cbd3d24626
Darren Shepherd <darren@rancher.com>
2018-10-04 22:31:18 -0700

    Remove scheduling/v1alpha1

--- pkg/apis/scheduling/install/install.go
+++ pkg/apis/scheduling/install/install.go
@@ -23,7 +23,6 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
 	"k8s.io/kubernetes/pkg/apis/scheduling"
-	"k8s.io/kubernetes/pkg/apis/scheduling/v1alpha1"
 	"k8s.io/kubernetes/pkg/apis/scheduling/v1beta1"
 )
 
@@ -35,6 +34,5 @@ func init() {
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(scheduling.AddToScheme(scheme))
 	utilruntime.Must(v1beta1.AddToScheme(scheme))
-	utilruntime.Must(v1alpha1.AddToScheme(scheme))
-	utilruntime.Must(scheme.SetVersionPriority(v1beta1.SchemeGroupVersion, v1alpha1.SchemeGroupVersion))
+	utilruntime.Must(scheme.SetVersionPriority(v1beta1.SchemeGroupVersion))
 }
