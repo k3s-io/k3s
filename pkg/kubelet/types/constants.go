174e986e9dc77f4b2c03cb65f8fe092901b2e13d
Darren Shepherd <darren@rancher.com>
2018-10-05 18:18:16 -0700

    Delete dockershim (docker support)

--- pkg/kubelet/types/constants.go
+++ pkg/kubelet/types/constants.go
@@ -21,7 +21,6 @@ const (
 	ResolvConfDefault = "/etc/resolv.conf"
 
 	// different container runtimes
-	DockerContainerRuntime = "docker"
 	RemoteContainerRuntime = "remote"
 
 	// User visible keys for managing node allocatable enforcement on the node.
