3e41f1c601d80edca14146d6ad76cc548e679a5c
Darren Shepherd <darren@rancher.com>
2018-12-27 01:56:50 -0700

    Remove BoundServiceAccountTokenVolume

--- plugin/pkg/auth/authorizer/rbac/bootstrappolicy/controller_policy.go
+++ plugin/pkg/auth/authorizer/rbac/bootstrappolicy/controller_policy.go
@@ -341,16 +341,6 @@ func buildControllerRoles() ([]rbacv1.ClusterRole, []rbacv1.ClusterRoleBinding) 
 		},
 	})
 
-	if utilfeature.DefaultFeatureGate.Enabled(features.BoundServiceAccountTokenVolume) {
-		addControllerRole(&controllerRoles, &controllerRoleBindings, rbacv1.ClusterRole{
-			ObjectMeta: metav1.ObjectMeta{Name: saRolePrefix + "root-ca-cert-publisher"},
-			Rules: []rbacv1.PolicyRule{
-				rbacv1helpers.NewRule("create", "update").Groups(legacyGroup).Resources("configmaps").RuleOrDie(),
-				eventsRule(),
-			},
-		})
-	}
-
 	return controllerRoles, controllerRoleBindings
 }
 
