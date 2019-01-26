3f2a951564ce13ef11cfd825dbfc2eabf962c809
Darren Shepherd <darren@rancher.com>
2019-01-25 21:52:29 -0700

    Ensure that br_netfilter module is loaded

--- pkg/agent/syssetup/setup.go
+++ pkg/agent/syssetup/setup.go
@@ -2,6 +2,7 @@ package syssetup
 
 import (
 	"io/ioutil"
+	"os/exec"
 
 	"github.com/sirupsen/logrus"
 )
@@ -11,6 +12,7 @@ var (
 )
 
 func Configure() error {
+	exec.Command("modprobe", "br_netfilter").Run()
 	if err := ioutil.WriteFile(callIPTablesFile, []byte("1"), 0640); err != nil {
 		logrus.Warnf("failed to write value 1 at %s: %v", callIPTablesFile, err)
 	}
