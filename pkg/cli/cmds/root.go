01b3bb315e9d932330a050c52dee56f6eb1a5af0
Darren Shepherd <darren@rancher.com>
2019-02-07 21:28:09 -0700

    Fix version printing on startup

--- pkg/cli/cmds/root.go
+++ pkg/cli/cmds/root.go
@@ -16,7 +16,7 @@ func NewApp() *cli.App {
 	app := cli.NewApp()
 	app.Name = appName
 	app.Usage = "Kubernetes, but small and simple"
-	app.Version = version.Version
+	app.Version = fmt.Sprintf("%s (%s)", version.Version, version.GitCommit)
 	cli.VersionPrinter = func(c *cli.Context) {
 		fmt.Printf("%s version %s\n", app.Name, app.Version)
 	}
