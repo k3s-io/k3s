857c0a156f1e23c901e2c26aa5a4449ddea3621f
Darren Shepherd <darren@rancher.com>
2019-02-23 22:38:24 -0700

    Set /proc/sys/net/ipv4/ip_forward on agent start

--- pkg/agent/syssetup/setup.go
+++ pkg/agent/syssetup/setup.go
@@ -9,12 +9,16 @@ import (
 
 var (
 	callIPTablesFile = "/proc/sys/net/bridge/bridge-nf-call-iptables"
+	forward          = "/proc/sys/net/ipv4/ip_forward"
 )
 
 func Configure() error {
 	exec.Command("modprobe", "br_netfilter").Run()
 	if err := ioutil.WriteFile(callIPTablesFile, []byte("1"), 0640); err != nil {
 		logrus.Warnf("failed to write value 1 at %s: %v", callIPTablesFile, err)
+	}
+	if err := ioutil.WriteFile(forward, []byte("1"), 0640); err != nil {
+		logrus.Warnf("failed to write value 1 at %s: %v", forward, err)
 	}
 	return nil
 }
