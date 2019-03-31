e8c5b2498c8c233a573b36b12c3668019f745d1f
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2019-03-31 14:55:56 +0200

    Change address to bind-address for scheduler and api

--- pkg/daemons/control/server.go
+++ pkg/daemons/control/server.go
@@ -102,7 +102,7 @@ func controllerManager(cfg *config.Control, runtime *config.ControlRuntime) {
 		"--cluster-cidr", cfg.ClusterIPRange.String(),
 		"--root-ca-file", runtime.TokenCA,
 		"--port", "10252",
-		"--address", "127.0.0.1",
+		"--bind-address", "127.0.0.1",
 		"--secure-port", "0",
 	}
 	if cfg.NoLeaderElect {
@@ -122,7 +122,7 @@ func scheduler(cfg *config.Control, runtime *config.ControlRuntime) {
 	args := []string{
 		"--kubeconfig", runtime.KubeConfigSystem,
 		"--port", "10251",
-		"--address", "127.0.0.1",
+		"--bind-address", "127.0.0.1",
 		"--secure-port", "0",
 	}
 	if cfg.NoLeaderElect {
