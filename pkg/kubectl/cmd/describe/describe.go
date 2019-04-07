297b309740da262a0a5d8f7701663e9a46ce76fe
Darren Shepherd <darren@rancher.com>
2019-04-06 19:39:23 -0700

    Remove kustomize

--- pkg/kubectl/cmd/describe/describe.go
+++ pkg/kubectl/cmd/describe/describe.go
@@ -131,7 +131,7 @@ func (o *DescribeOptions) Complete(f cmdutil.Factory, cmd *cobra.Command, args [
 		o.EnforceNamespace = false
 	}
 
-	if len(args) == 0 && cmdutil.IsFilenameSliceEmpty(o.FilenameOptions.Filenames, o.FilenameOptions.Kustomize) {
+	if len(args) == 0 && cmdutil.IsFilenameSliceEmpty(o.FilenameOptions.Filenames, "") {
 		return fmt.Errorf("You must specify the type of resource to describe. %s\n", cmdutil.SuggestAPIResources(o.CmdParent))
 	}
 
