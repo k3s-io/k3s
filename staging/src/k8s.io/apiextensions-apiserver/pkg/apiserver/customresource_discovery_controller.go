316b44729da63b58ba8f319253e361ff03297967
Darren Shepherd <darren@rancher.com>
2019-04-06 18:28:53 -0700

    Remove StorageVersionHash

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_discovery_controller.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_discovery_controller.go
@@ -95,7 +95,6 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 		}
 
 		foundThisVersion := false
-		var storageVersionHash string
 		for _, v := range crd.Spec.Versions {
 			if !v.Served {
 				continue
@@ -113,9 +112,6 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			}
 			if v.Name == version.Version {
 				foundThisVersion = true
-			}
-			if v.Storage {
-				storageVersionHash = discovery.StorageVersionHash(gv.Group, gv.Version, crd.Spec.Names.Kind)
 			}
 		}
 
@@ -138,7 +134,6 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			Verbs:              verbs,
 			ShortNames:         crd.Status.AcceptedNames.ShortNames,
 			Categories:         crd.Status.AcceptedNames.Categories,
-			StorageVersionHash: storageVersionHash,
 		})
 
 		subresources, err := apiextensions.GetSubresourcesForVersion(crd, version.Version)
