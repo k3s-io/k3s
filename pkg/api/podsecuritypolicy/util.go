060620ea6c1204cbaf9b0b602df920aafbf59994
Darren Shepherd <darren@rancher.com>
2019-04-07 00:26:01 -0700

    Remove CSI Alpha features

--- pkg/api/podsecuritypolicy/util.go
+++ pkg/api/podsecuritypolicy/util.go
@@ -35,9 +35,7 @@ func DropDisabledFields(pspSpec, oldPSPSpec *policy.PodSecurityPolicySpec) {
 		pspSpec.AllowedUnsafeSysctls = nil
 		pspSpec.ForbiddenSysctls = nil
 	}
-	if !utilfeature.DefaultFeatureGate.Enabled(features.CSIInlineVolume) {
-		pspSpec.AllowedCSIDrivers = nil
-	}
+	pspSpec.AllowedCSIDrivers = nil
 }
 
 func allowedProcMountTypesInUse(oldPSPSpec *policy.PodSecurityPolicySpec) bool {
