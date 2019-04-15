d34f771a67feedea9232d451d531878760bdeff7
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-04-15 09:30:32 -0700

    Ignore proxy settings for kubelet client
    Proxy settings may interfere with a kubelet client communicating with
    the API server, so set the proxy to nil.

--- cmd/kube-apiserver/app/server.go
+++ cmd/kube-apiserver/app/server.go
@@ -181,6 +181,7 @@ func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan
 
 	if DefaultProxyDialerFn != nil {
 		completedOptions.KubeletConfig.Dial = DefaultProxyDialerFn
+		completedOptions.KubeletConfig.Proxy = http.ProxyURL(nil)
 	}
 
 	kubeAPIServerConfig, insecureServingInfo, serviceResolver, pluginInitializer, admissionPostStartHook, err := CreateKubeAPIServerConfig(completedOptions, nodeTunneler, proxyTransport)
