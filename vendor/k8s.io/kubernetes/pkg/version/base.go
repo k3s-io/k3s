5d88cc71132cc8631d46a5859048e4fcf0a00c7a
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-08-05 11:39:53 -0700

    Update k8s to v1.14.5-k3s.1

--- vendor/k8s.io/kubernetes/pkg/version/base.go
+++ vendor/k8s.io/kubernetes/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "14"
-	gitVersion   = "v1.14.4-k3s.1"
-	gitCommit    = "ac992f3ef6c6d71e14366a1f65318e10eb3f4232"
+	gitVersion   = "v1.14.5-k3s.1"
+	gitCommit    = "52ef54a465650e500182c3836a6d3f88efb6a32e"
 	gitTreeState = "clean"
-	buildDate = "2019-07-14T07:04+00:00Z"
+	buildDate = "2019-08-05T18:08+00:00Z"
 )
