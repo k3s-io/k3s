d120ae97c9ef42721af694af83433d317e023253
Darren Shepherd <darren@rancher.com>
2019-09-04 21:11:52 -0700

    Update vendor

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "15"
-	gitVersion   = "v1.15.3-k3s.1"
-	gitCommit    = "77c93d8d16059eb6873a8a154bbd70518b4e9db4"
+	gitVersion   = "v1.15.3-k3s.2"
+	gitCommit    = "f0b8b2798b7adaed93d8e946e3c3ea0d6a38390f"
 	gitTreeState = "clean"
-	buildDate = "2019-08-30T18:26+00:00Z"
+	buildDate = "2019-09-05T04:04+00:00Z"
 )
