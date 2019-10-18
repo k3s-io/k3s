1df72d14b8f1b164f0c68e5eaffe50d08061a9b0
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-10-18 12:31:06 -0700

    Cleanup containerd config template spacing

--- pkg/agent/templates/templates.go
+++ pkg/agent/templates/templates.go
@@ -15,26 +15,26 @@ type ContainerdConfig struct {
 
 const ContainerdConfigTemplate = `
 [plugins.opt]
-path = "{{ .NodeConfig.Containerd.Opt }}"
+  path = "{{ .NodeConfig.Containerd.Opt }}"
 
 [plugins.cri]
-stream_server_address = "127.0.0.1"
-stream_server_port = "10010"
+  stream_server_address = "127.0.0.1"
+  stream_server_port = "10010"
 
 {{- if .IsRunningInUserNS }}
-disable_cgroup = true
-disable_apparmor = true
-restrict_oom_score_adj = true
+  disable_cgroup = true
+  disable_apparmor = true
+  restrict_oom_score_adj = true
 {{ end -}}
 
 {{- if .NodeConfig.AgentConfig.PauseImage }}
-sandbox_image = "{{ .NodeConfig.AgentConfig.PauseImage }}"
+  sandbox_image = "{{ .NodeConfig.AgentConfig.PauseImage }}"
 {{ end -}}
 
 {{- if not .NodeConfig.NoFlannel }}
-  [plugins.cri.cni]
-    bin_dir = "{{ .NodeConfig.AgentConfig.CNIBinDir }}"
-    conf_dir = "{{ .NodeConfig.AgentConfig.CNIConfDir }}"
+[plugins.cri.cni]
+  bin_dir = "{{ .NodeConfig.AgentConfig.CNIBinDir }}"
+  conf_dir = "{{ .NodeConfig.AgentConfig.CNIConfDir }}"
 {{ end -}}
 
 [plugins.cri.containerd.runtimes.runc]
@@ -45,22 +45,22 @@ sandbox_image = "{{ .NodeConfig.AgentConfig.PauseImage }}"
 [plugins.cri.registry.mirrors]{{end}}
 {{range $k, $v := .PrivateRegistryConfig.Mirrors }}
 [plugins.cri.registry.mirrors."{{$k}}"]
-	endpoint = [{{range $i, $j := $v.Endpoints}}{{if $i}}, {{end}}{{printf "%q" .}}{{end}}]
+  endpoint = [{{range $i, $j := $v.Endpoints}}{{if $i}}, {{end}}{{printf "%q" .}}{{end}}]
 {{end}}
 
 {{range $k, $v := .PrivateRegistryConfig.Configs }}
 {{ if $v.Auth }}
 [plugins.cri.registry.configs."{{$k}}".auth]
-	{{ if $v.Auth.Username }}username = "{{ $v.Auth.Username }}"{{end}}
-	{{ if $v.Auth.Password }}password = "{{ $v.Auth.Password }}"{{end}}
-	{{ if $v.Auth.Auth }}auth = "{{ $v.Auth.Auth }}"{{end}}
-	{{ if $v.Auth.IdentityToken }}identity_token = "{{ $v.Auth.IdentityToken }}"{{end}}
+  {{ if $v.Auth.Username }}username = "{{ $v.Auth.Username }}"{{end}}
+  {{ if $v.Auth.Password }}password = "{{ $v.Auth.Password }}"{{end}}
+  {{ if $v.Auth.Auth }}auth = "{{ $v.Auth.Auth }}"{{end}}
+  {{ if $v.Auth.IdentityToken }}identity_token = "{{ $v.Auth.IdentityToken }}"{{end}}
 {{end}}
 {{ if $v.TLS }}
 [plugins.cri.registry.configs."{{$k}}".tls]
-	{{ if $v.TLS.CAFile }}ca_file = "{{ $v.TLS.CAFile }}"{{end}}
-	{{ if $v.TLS.CertFile }}cert_file = "{{ $v.TLS.CertFile }}"{{end}}
-	{{ if $v.TLS.KeyFile }}key_file = "{{ $v.TLS.KeyFile }}"{{end}}
+  {{ if $v.TLS.CAFile }}ca_file = "{{ $v.TLS.CAFile }}"{{end}}
+  {{ if $v.TLS.CertFile }}cert_file = "{{ $v.TLS.CertFile }}"{{end}}
+  {{ if $v.TLS.KeyFile }}key_file = "{{ $v.TLS.KeyFile }}"{{end}}
 {{end}}
 {{end}}
 {{end}}
