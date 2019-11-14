82702a2a645514bec2ed95bdb344233ba0abafc5
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-11-14 11:56:24 -0700

    Bump k8s to v1.16.3-k3s.1

--- vendor/k8s.io/kubectl/pkg/version/base.go
+++ vendor/k8s.io/kubectl/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "16"
-	gitVersion   = "v1.16.2-k3s.1"
-	gitCommit    = "b8b17ba55f20e590df507fce333dfee13ab438c6"
+	gitVersion   = "v1.16.3-k3s.1"
+	gitCommit    = "bc4447825d97497c0b3ab4aa70da9b87f117094d"
 	gitTreeState = "clean"
-	buildDate = "2019-10-16T05:17Z"
+	buildDate = "2019-11-14T17:48:31Z"
 )
