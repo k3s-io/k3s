9b2538c2c422d8693734990d8208422414d61466
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-12-19 14:54:48 -0700

    Set wireguard persistent-keepalive on wg set peer

--- pkg/agent/flannel/setup.go
+++ pkg/agent/flannel/setup.go
@@ -60,7 +60,7 @@ const (
 	"PreStartupCommand": "wg genkey | tee privatekey | wg pubkey",
 	"PostStartupCommand": "export SUBNET_IP=$(echo $SUBNET | cut -d'/' -f 1); ip link del flannel.1 2>/dev/null; echo $PATH >&2; wg-add.sh flannel.1 && wg set flannel.1 listen-port 51820 private-key privatekey && ip addr add $SUBNET_IP/32 dev flannel.1 && ip link set flannel.1 up && ip route add $NETWORK dev flannel.1",
 	"ShutdownCommand": "ip link del flannel.1",
-	"SubnetAddCommand": "read PUBLICKEY; wg set flannel.1 peer $PUBLICKEY endpoint $PUBLIC_IP:51820 allowed-ips $SUBNET",
+	"SubnetAddCommand": "read PUBLICKEY; wg set flannel.1 peer $PUBLICKEY endpoint $PUBLIC_IP:51820 allowed-ips $SUBNET persistent-keepalive 25",
 	"SubnetRemoveCommand": "read PUBLICKEY; wg set flannel.1 peer $PUBLICKEY remove"
 }`
 )
