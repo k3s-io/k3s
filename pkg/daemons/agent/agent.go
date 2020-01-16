fa03a0df3c29ec8dcd551d5dee9adbfd71076f5f
Erik Wilson <Erik.E.Wilson@gmail.com>
2020-01-16 10:21:19 -0700

    Run kubelet with containerd flag
    The containerd flag was accidentally added to kubelet and is
    deprecated, but needed for cadvisor to properly connect with
    the k3s containerd socket, so adding for now.

--- pkg/daemons/agent/agent.go
+++ pkg/daemons/agent/agent.go
@@ -93,6 +93,7 @@ func startKubelet(cfg *config.Agent) {
 	if cfg.RuntimeSocket != "" {
 		argsMap["container-runtime"] = "remote"
 		argsMap["container-runtime-endpoint"] = cfg.RuntimeSocket
+		argsMap["containerd"] = cfg.RuntimeSocket
 		argsMap["serialize-image-pulls"] = "false"
 	} else if cfg.PauseImage != "" {
 		argsMap["pod-infra-container-image"] = cfg.PauseImage
