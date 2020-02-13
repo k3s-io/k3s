c5f003b2663a68a8dc637d8ce7c80659b2297b24
Erik Wilson <Erik.E.Wilson@gmail.com>
2020-02-13 16:49:39 -0700

    Upgrade k8s to v1.16.7

--- vendor/k8s.io/kubectl/pkg/version/base.go
+++ vendor/k8s.io/kubectl/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "16"
-	gitVersion   = "v1.16.6-k3s1"
-	gitCommit    = "3a33845dc46708de6e6b0a20045f018bd803cb0b"
+	gitVersion   = "v1.16.7-k3s1"
+	gitCommit    = "95bc2b1efad3fcfaf304fc166380b5a9e289feab"
 	gitTreeState = "clean"
-	buildDate = "2020-01-22T17:07:35Z"
+	buildDate = "2020-02-13T22:59:28Z"
 )
