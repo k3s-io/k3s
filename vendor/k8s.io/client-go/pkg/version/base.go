d833224d96ba86ce664d8a1401edc48eb0a96c0b
Brad Davidson <brad.davidson@rancher.com>
2020-07-15 18:21:14 -0700

    Update to v1.16.13-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "16"
-	gitVersion   = "v1.16.11-k3s1"
-	gitCommit    = "a2d85ac363941a5280ccd3a1415e38326c843dbe"
+	gitVersion   = "v1.16.13-k3s1"
+	gitCommit    = "0b9525c21107dd05b47060ade21a607549116813"
 	gitTreeState = "clean"
-	buildDate = "2020-06-17T18:17:34Z"
+	buildDate = "2020-07-16T00:35:22Z"
 )
