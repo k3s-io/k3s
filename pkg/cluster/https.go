c68620673c19150b2d33f2a43e9daf33a0156bc9
Menna Elmasry <65324011+MonzElmasry@users.noreply.github.com>
2020-09-14 23:14:30 +0200

    Add error logger to http server (#2242)
    * add error logger to http server
    
    Signed-off-by: MonzElmasry <menna.elmasry@rancher.com>

--- pkg/cluster/https.go
+++ pkg/cluster/https.go
@@ -3,6 +3,7 @@ package cluster
 import (
 	"context"
 	"crypto/tls"
+	"log"
 	"net"
 	"net/http"
 	"path/filepath"
@@ -60,7 +61,8 @@ func (c *Cluster) initClusterAndHTTPS(ctx context.Context) error {
 	}
 
 	server := http.Server{
-		Handler: handler,
+		Handler:  handler,
+		ErrorLog: log.New(logrus.StandardLogger().Writer(), "Cluster-Http-Server ", log.LstdFlags),
 	}
 
 	go func() {
