4a2ca4a1e714c6c79da9a3d784f7b037c8251146
MonzElmasry <menna.elmasry@rancher.com>
2020-09-17 01:24:02 +0200

    v1.17.12-k3s1
    Signed-off-by: MonzElmasry <menna.elmasry@rancher.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "17"
-	gitVersion   = "v1.17.11-k3s1"
-	gitCommit    = "51cfdf7fa2caf99d6ee48a3a5257d4177b8b4665"
+	gitVersion   = "v1.17.12-k3s1"
+	gitCommit    = "3a47527bc3f8a5ea50a5332060ebad8809b7bef8"
 	gitTreeState = "clean"
-	buildDate = "2020-08-13T18:43:40Z"
+	buildDate = "2020-09-16T20:26:31Z"
 )
