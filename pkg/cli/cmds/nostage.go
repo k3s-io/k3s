ec5fb19abb85caffdee9eba3db2db4aad5f36186
Brad Davidson <brad.davidson@rancher.com>
2020-09-28 11:38:08 -0700

    Update disables list when building with no_stage
    The --disable/--no-deploy flags actually turn off some built-in
    controllers, in addition to preventing manifests from getting loaded.
    Make it clear which controllers can still be disabled even when the
    packaged components are ommited by the no_stage build tag.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

---
+++ pkg/cli/cmds/nostage.go
@@ -0,0 +1,9 @@
+// +build no_stage
+
+package cmds
+
+const (
+	// The coredns and servicelb controllers can still be disabled, even if their manifests
+	// are missing. Same with CloudController/ccm.
+	DisableItems = "coredns, servicelb"
+)
