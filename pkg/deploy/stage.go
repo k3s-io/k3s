cbe1c7462836fcc5abcaaeb50b0a0bd5bf0b3565
Brad Davidson <brad.davidson@rancher.com>
2020-11-09 09:52:22 -0800

    Don't rewrite manifests that haven't changed
    Previously, manifests were unconditionally rewritten on startup. Since we
    are now using file mtime when deciding whether or not to apply the
    manifest, we need to avoid unecessarily modifying files to ensure that
    restarting a node does not always give it the most recently modified
    version of the file.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/deploy/stage.go
+++ pkg/deploy/stage.go
@@ -4,6 +4,8 @@ package deploy
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -13,6 +15,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Stage iterates over the embedded asset list. Assets are checked against the skips map
+// to determine if they should not be processed. If they should not be skipped, string substitutions
+// are performed using the templateVars map. Finally, content is checksummed to see if the file should
+// be rewritten. If the content has changed from the on-disk version, the file is rewritten.
 func Stage(dataDir string, templateVars map[string]string, skips map[string]bool) error {
 staging:
 	for _, name := range AssetNames() {
@@ -35,7 +41,19 @@ staging:
 		for k, v := range templateVars {
 			content = bytes.Replace(content, []byte(k), []byte(v), -1)
 		}
+
 		p := filepath.Join(dataDir, name)
+		contentHash := sha256.Sum256(content)
+		currentHash, err := fileSum256(p)
+		if err != nil {
+			return err
+		}
+
+		if bytes.Equal(contentHash[:], currentHash[:]) {
+			logrus.Info("Unchanged manifest: ", p)
+			continue
+		}
+
 		os.MkdirAll(filepath.Dir(p), 0700)
 		logrus.Info("Writing manifest: ", p)
 		if err := ioutil.WriteFile(p, content, 0600); err != nil {
@@ -44,4 +62,22 @@ staging:
 	}
 
 	return nil
+}
+
+// fileSum256 returns the sha256 digest of a file at a given path.
+func fileSum256(path string) (data []byte, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return data, nil
+		}
+		return data, err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return data, err
+	}
+	return h.Sum(nil), nil
 }
