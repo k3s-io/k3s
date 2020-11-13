4004fffcb84d0a8e73fa54fe8cdf2760e8f1b0fd
Brian Downs <brian.downs@gmail.com>
2020-11-13 10:10:39 -0700

    Update modules for v1.19.4
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "19"
-	gitVersion   = "v1.19.3-k3s1"
-	gitCommit    = "a38be968fd7010255807381f6d846c4082a838db"
+	gitVersion   = "v1.19.4-k3s1"
+	gitCommit    = "f12e8cfdaa72c5223c4b87ae4506f9b42cbd9f74"
 	gitTreeState = "clean"
-	buildDate = "2020-10-14T17:54:23Z"
+	buildDate = "2020-11-12T16:37:32Z"
 )
