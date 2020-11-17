77185ce6f5358a6b027cfac23b37665e11e35ed2
Brad Davidson <brad.davidson@rancher.com>
2020-11-17 12:21:12 -0800

    Clean up architecture handling
    * Remove unused release.sh and DAPPER_HOST_ARCH
    * Reliably use ARCH from version.sh
    * Export GOARCH and GOARM so that they are used by `go build`
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- scripts/binary_size_check.sh
+++ scripts/binary_size_check.sh
@@ -6,6 +6,8 @@ if [ "${DEBUG}" = 1 ]; then
     set -x
 fi
 
+. ./scripts/version.sh
+
 MAX_BINARY_SIZE=61000000
 BIN_SUFFIX="-${ARCH}"
 if [ ${ARCH} = amd64 ]; then
@@ -18,8 +20,9 @@ CMD_NAME="dist/artifacts/k3s${BIN_SUFFIX}"
 SIZE=$(stat -c '%s' ${CMD_NAME})
 
 if [ ${SIZE} -gt ${MAX_BINARY_SIZE} ]; then
-    echo "k3s binary ${CMD_NAME} size ${SIZE} exceeds acceptable size of ${MAX_BINARY_SIZE}"
+    echo "k3s binary ${CMD_NAME} size ${SIZE} exceeds max acceptable size of ${MAX_BINARY_SIZE} bytes"
     exit 1
 fi
 
+echo "k3s binary ${CMD_NAME} size ${SIZE} is less than max acceptable size of ${MAX_BINARY_SIZE} bytes"
 exit 0
