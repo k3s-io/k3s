e52b9f10ad8779a34dfa6cc991b85c033dc114c8
Brad Davidson <brad.davidson@rancher.com>
2020-12-23 23:20:28 -0800

    Update Kubernetes to v1.20.1-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "20"
-	gitVersion   = "v1.20.0-k3s1"
-	gitCommit    = "3559625e27197f60bcc39c4b3ecffc80bad7785e"
+	gitVersion   = "v1.20.1-k3s1"
+	gitCommit    = "dd4bf597945d641cf0e53b13f99b5bb9bc3f92e8"
 	gitTreeState = "clean"
-	buildDate = "2020-12-08T20:42:55Z"
+	buildDate = "2020-12-24T07:15:16Z"
 )
