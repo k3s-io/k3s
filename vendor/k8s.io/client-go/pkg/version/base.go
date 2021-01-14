40f3e37b2754b3a2105bd2a21cc4c52ec783efe5
Brad Davidson <brad.davidson@rancher.com>
2021-01-13 16:23:58 -0800

    Update Kubernetes to v1.17.17-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "17"
-	gitVersion   = "v1.17.15-k3s1"
-	gitCommit    = "6de5b4e17702a4184de16f8595a8abe7fbed209d"
+	gitVersion   = "v1.17.17-k3s1"
+	gitCommit    = "db29017b8486a7b90ab0a6561749458193809787"
 	gitTreeState = "clean"
-	buildDate = "2020-12-10T00:24:25Z"
+	buildDate = "2021-01-13T19:06:17Z"
 )
