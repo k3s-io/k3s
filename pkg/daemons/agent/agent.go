k3s-io/k3s

commit f3de60ff3191dc3a0ae7ce8393e388700f48ed88

When there is a defined cgroup for PID 1, assume we are containerized and set a root
Signed-off-by: Chris Kim <oats87g@gmail.com>

pkg/daemons/agent/agent.go
@@ -194,12 +194,31 @@ func checkCgroups() (root string, hasCFS bool, hasPIDs bool) {
 				if _, err := os.Stat(p); err == nil {
 					hasCFS = true
 				}
-			} else if system == "name=systemd" {
+			}
+		}
+	}
+
+	// Examine process ID 1 to see if there is a cgroup assigned to it.
+	// When we are not in a container, process 1 is likely to be systemd or some other service manager.
+	// When containerized, process 1 will be generally be in a cgroup, otherwise, we may be running in
+	// a host PID scenario but we don't support this.
+	g, err := os.Open("/proc/1/cgroup")
+	if err != nil {
+		return "", false, false
+	}
+	defer g.Close()
+	root = ""
+	scan = bufio.NewScanner(g)
+	for scan.Scan() {
+		parts := strings.Split(scan.Text(), ":")
+		if len(parts) < 3 {
+			continue
+		}
+		systems := strings.Split(parts[1], ",")
+		for _, system := range systems {
+			if system == "name=systemd" {
 				last := parts[len(parts)-1]
-				i := strings.LastIndex(last, ".slice")
-				if i > 0 {
-					root = "/systemd" + last[:i+len(".slice")]
-				} else {
+				if last != "/" {
 					root = "/systemd"
 				}
 			}

commit 127b96e897abd3633724274b4be51165ebcd42d8

fix cgroup2 support
Fix issue 900

cgroup2 support was introduced in PR 2584, but got broken in f3de60ff3191dc3a0ae7ce8393e388700f48ed88

It was failing with "F1210 19:13:37.305388    4955 server.go:181] cannot set feature gate SupportPodPidsLimit to false, feature is locked to true"

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

pkg/daemons/agent/agent.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/containerd/cgroups"
+	cgroupsv2 "github.com/containerd/cgroups/v2"
 	"github.com/opencontainers/runc/libcontainer/system"
 	"github.com/rancher/k3s/pkg/daemons/config"
 	"github.com/rancher/k3s/pkg/daemons/executor"
@@ -182,6 +184,26 @@ func checkCgroups() (kubeletRoot, runtimeRoot string, hasCFS, hasPIDs bool) {
 	}
 	defer f.Close()
 
+	v2 := cgroups.Mode() == cgroups.Unified
+	if v2 {
+		m, err := cgroupsv2.LoadManager("/sys/fs/cgroup", "/")
+		if err != nil {
+			return "", "", false, false
+		}
+		controllers, err := m.Controllers()
+		if err != nil {
+			return "", "", false, false
+		}
+		for _, c := range controllers {
+			switch c {
+			case "cpu":
+				hasCFS = true
+			case "pids":
+				hasPIDs = true
+			}
+		}
+	}
+
 	scan := bufio.NewScanner(f)
 	for scan.Scan() {
 		parts := strings.Split(scan.Text(), ":")
@@ -189,6 +211,7 @@ func checkCgroups() (kubeletRoot, runtimeRoot string, hasCFS, hasPIDs bool) {
 			continue
 		}
 		systems := strings.Split(parts[1], ",")
+		// when v2, systems = {""} (only contains a single empty string)
 		for _, system := range systems {
 			if system == "pids" {
 				hasPIDs = true
@@ -197,7 +220,7 @@ func checkCgroups() (kubeletRoot, runtimeRoot string, hasCFS, hasPIDs bool) {
 				if _, err := os.Stat(p); err == nil {
 					hasCFS = true
 				}
-			} else if system == "name=systemd" {
+			} else if system == "name=systemd" || v2 {
 				// If we detect that we are running under a `.scope` unit with systemd
 				// we can assume we are being directly invoked from the command line
 				// and thus need to set our kubelet root to something out of the context
@@ -233,8 +256,9 @@ func checkCgroups() (kubeletRoot, runtimeRoot string, hasCFS, hasPIDs bool) {
 				continue
 			}
 			systems := strings.Split(parts[1], ",")
+			// when v2, systems = {""} (only contains a single empty string)
 			for _, system := range systems {
-				if system == "name=systemd" {
+				if system == "name=systemd" || v2 {
 					last := parts[len(parts)-1]
 					if last != "/" && last != "/init.scope" {
 						kubeletRoot = "/" + version.Program