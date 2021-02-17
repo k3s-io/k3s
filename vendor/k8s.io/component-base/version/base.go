e7a3139e47fbc8b7661c18a507f28bac7ecf7eff
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2021-02-18 01:12:37 +0200

    Update to v1.18.16
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "18"
-	gitVersion   = "v1.18.15-k3s1"
-	gitCommit    = "92952488446669be6ff06c46c3e13de3fe395e75"
+	gitVersion   = "v1.18.16-k3s1"
+	gitCommit    = "060f37b373e261d212741b99aa70007d44992a77"
 	gitTreeState = "clean"
-	buildDate = "2021-01-13T19:13:58Z"
+	buildDate = "2021-02-17T22:58:19Z"
 )
