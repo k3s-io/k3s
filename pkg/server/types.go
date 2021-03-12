64017c5272cfb63287ab6d801e441c426a8dc2c7
Chris Kim <30601846+Oats87@users.noreply.github.com>
2021-03-12 07:27:51 -0800

    Define a Controllers and LeaderControllers on the server config (#3052)
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/server/types.go
+++ pkg/server/types.go
@@ -7,10 +7,14 @@ import (
 )
 
 type Config struct {
-	DisableAgent     bool
-	DisableServiceLB bool
-	ControlConfig    config.Control
-	Rootless         bool
-	SupervisorPort   int
-	StartupHooks     []func(context.Context, <-chan struct{}, string) error
+	DisableAgent      bool
+	DisableServiceLB  bool
+	ControlConfig     config.Control
+	Rootless          bool
+	SupervisorPort    int
+	StartupHooks      []func(context.Context, <-chan struct{}, string) error
+	LeaderControllers CustomControllers
+	Controllers       CustomControllers
 }
+
+type CustomControllers []func(ctx context.Context, sc *Context) error
