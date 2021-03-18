1c64b856059a7615f6ae0cdf684ea0a748800fd7
Jacob Blain Christen <jacob@rancher.com>
2021-03-18 13:46:11 -0700

    Update to v1.20.5
    Signed-off-by: Jacob Blain Christen <jacob@rancher.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "20"
-	gitVersion   = "v1.20.4-k3s1"
-	gitCommit    = "dbe940a8b30b544ac26d7947bb7dc7f62a5f484d"
+	gitVersion   = "v1.20.5-k3s1"
+	gitCommit    = "2a725098eaafe27dde4eadb4318c5b1561b9ab06"
 	gitTreeState = "clean"
-	buildDate = "2021-02-18T20:50:53Z"
+	buildDate = "2021-03-18T20:21:52Z"
 )
