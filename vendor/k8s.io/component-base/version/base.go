ac73524a77576338816dc061c6ee16f2e9302217
Jacob Blain Christen <jacob@rancher.com>
2021-03-18 17:19:47 -0700

    Update to Kubernetes v1.19.9 (#3095)
    * Update to Kubernetes v1.19.9
    
    Signed-off-by: Jacob Blain Christen <jacob@rancher.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "19"
-	gitVersion   = "v1.19.8-k3s1"
-	gitCommit    = "71b31507f0011b9fd9a4311fabeb17e43d7ff0e6"
+	gitVersion   = "v1.19.9-k3s1"
+	gitCommit    = "9af4aa7bad3f896a07140a9e82fdd8642373c5d8"
 	gitTreeState = "clean"
-	buildDate = "2021-02-17T22:28:17Z"
+	buildDate = "2021-03-18T21:01:32Z"
 )
