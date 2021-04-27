d386c8a4137f2991573ecfbd5823d13578babc9b
Thorsten Klein <iwilltry42@gmail.com>
2021-04-27 20:28:42 +0200

    Apply suggestions from code review

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -11,10 +11,10 @@ set -o pipefail
 # Moby License Apache 2.0: https://github.com/moby/moby/blob/ed89041433a031cafc0a0f19cfe573c31688d377/LICENSE														#
 #########################################################################################################################################
 if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-	# move the init process (PID 1) from the root group to the /init group,
+	# move the processes from the root group to the /init group,
   # otherwise writing subtree_control fails with EBUSY.
   mkdir -p /sys/fs/cgroup/init
-  echo 1 > /sys/fs/cgroup/init/cgroup.procs
+  xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
   # enable controllers
   sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
 fi
