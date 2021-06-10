ff0451c4dd79ed40e5393173a2fb90249f2cfefc
Brad Davidson <brad.davidson@rancher.com>
2021-06-10 11:27:36 -0700

    Add nodename to UA string for deploy controller
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 6ef000091acf2b4987c4c7a33f1a81645ff965cd)

--- pkg/server/context.go
+++ pkg/server/context.go
@@ -2,9 +2,14 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"runtime"
 
 	"github.com/k3s-io/helm-controller/pkg/generated/controllers/helm.cattle.io"
+	"github.com/rancher/k3s/pkg/deploy"
 	"github.com/rancher/k3s/pkg/generated/controllers/k3s.cattle.io"
+	"github.com/rancher/k3s/pkg/version"
 	"github.com/rancher/wrangler-api/pkg/generated/controllers/apps"
 	"github.com/rancher/wrangler-api/pkg/generated/controllers/batch"
 	"github.com/rancher/wrangler-api/pkg/generated/controllers/core"
@@ -12,6 +17,8 @@ import (
 	"github.com/rancher/wrangler/pkg/apply"
 	"github.com/rancher/wrangler/pkg/crd"
 	"github.com/rancher/wrangler/pkg/start"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/validation"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -37,6 +44,16 @@ func NewContext(ctx context.Context, cfg string) (*Context, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Construct a custom user-agent string for the apply client used by the deploy controller
+	// so that we can track which node's deploy controller most recently modified a resource.
+	nodeName := os.Getenv("NODE_NAME")
+	managerName := deploy.ControllerName + "@" + nodeName
+	if nodeName == "" || len(managerName) > validation.FieldManagerMaxLength {
+		logrus.Warn("Deploy controller node name is empty or too long, and will not be tracked via server side apply field management")
+		managerName = deploy.ControllerName
+	}
+	restConfig.UserAgent = fmt.Sprintf("%s/%s (%s/%s) %s/%s", managerName, version.Version, runtime.GOOS, runtime.GOARCH, version.Program, version.GitCommit)
 
 	if err := crds(ctx, restConfig); err != nil {
 		return nil, err
