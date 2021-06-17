559d0c4729493b62e4cda02f85ea3c3a5c3d7428
Derek Nola <derek.nola@suse.com>
2021-06-17 14:35:50 -0700

    Update to v1.19.12 (#3476)
    * Update to v1.19.12
    
    Signed-off-by: root <derek.nola@suse.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "19"
-	gitVersion   = "v1.19.11-k3s1"
-	gitCommit    = "4230b2ac644cf72064bf7cacdf2ffa13292b3c1f"
+	gitVersion   = "v1.19.12-k3s1"
+	gitCommit    = "3e32ea1663306f5e5b7f386fc190f0afefc80454"
 	gitTreeState = "clean"
-	buildDate = "2021-05-14T11:17:50Z"
+	buildDate = "2021-06-17T18:50:24Z"
 )
