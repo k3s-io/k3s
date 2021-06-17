5228b159d1cedb1716cb2e4eb1e53b02c21af652
root <derek.nola@suse.com>
2021-06-17 14:16:07 -0700

    Update to v1.20.8
    Signed-off-by: root <derek.nola@suse.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "20"
-	gitVersion   = "v1.20.7-k3s1"
-	gitCommit    = "def098cc98a34931afa07e8500cbf55253e67481"
+	gitVersion   = "v1.20.8-k3s1"
+	gitCommit    = "d3d0f184376c7a0e7eedac78e4e3c018da07eba9"
 	gitTreeState = "clean"
-	buildDate = "2021-05-14T10:11:46Z"
+	buildDate = "2021-06-17T20:33:07Z"
 )
