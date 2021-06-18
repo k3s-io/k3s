b916b03d68a0d3ea9607edc6026a6689fe6dfec3
dereknola <derek.nola@suse.com>
2021-06-18 11:48:56 -0700

    Update to v1.21.2
    Signed-off-by: dereknola <derek.nola@suse.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -4,7 +4,7 @@ var (
 	gitMajor = "1"
 	gitMinor = "21"
 	gitVersion   = "v1.21.2-k3s1"
-	gitCommit    = "54f1e1e7c631bbcf560622cd874e778b154dca39"
+	gitCommit    = "faffd6ecd237a32315a462a25c1cce1f24a9a506"
 	gitTreeState = "clean"
-	buildDate = "2021-06-17T23:03:35Z"
+	buildDate = "2021-06-18T03:51:40Z"
 )
