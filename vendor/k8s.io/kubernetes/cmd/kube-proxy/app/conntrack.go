e3b61ae27612563980e660a6b532336a0ad5d16f
Claudio Kuenzler <ck@claudiokuenzler.com>
2021-06-24 11:00:35 +0200

    Allow higher system values
    With this commit kube-proxy accepts current system values (retrieved by sysctl) which are higher than the internally known and expected values.
    This should solve the Rancher start problems documented in https://github.com/rancher/rancher/issues/33360 .
    Signed-off-by: Claudio Kuenzler <ck@claudiokuenzler.com>

--- vendor/k8s.io/kubernetes/cmd/kube-proxy/app/conntrack.go
+++ vendor/k8s.io/kubernetes/cmd/kube-proxy/app/conntrack.go
@@ -99,7 +99,7 @@ func (realConntracker) setIntSysCtl(name string, value int) error {
 	entry := "net/netfilter/" + name
 
 	sys := sysctl.New()
-	if val, _ := sys.GetSysctl(entry); val != value {
+	if val, _ := sys.GetSysctl(entry); val != value && val < value {
 		klog.Infof("Set sysctl '%v' to %v", entry, value)
 		if err := sys.SetSysctl(entry, value); err != nil {
 			return err
