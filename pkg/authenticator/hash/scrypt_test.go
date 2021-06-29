2bd4650ed2161d357b1702ef146b50d438f8a9f6
dereknola <derek.nola@suse.com>
2021-06-29 14:00:13 -0700

    Converted scrypt_tes.go
    Signed-off-by: dereknola <derek.nola@suse.com>

--- pkg/authenticator/hash/scrypt_test.go
+++ pkg/authenticator/hash/scrypt_test.go
@@ -3,28 +3,43 @@ package hash
 import (
 	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-var hasher = NewSCrypt()
-
-func TestBasicHash(t *testing.T) {
-	secretKey := "hello world"
-	hash, err := hasher.CreateHash(secretKey)
-	assert.Nil(t, err)
-	assert.NotNil(t, hash)
-
-	assert.Nil(t, hasher.VerifyHash(hash, secretKey))
-	assert.NotNil(t, hasher.VerifyHash(hash, "goodbye"))
-}
-
-func TestLongKey(t *testing.T) {
-	secretKey := strings.Repeat("A", 720)
-	hash, err := hasher.CreateHash(secretKey)
-	assert.Nil(t, err)
-	assert.NotNil(t, hash)
-
-	assert.Nil(t, hasher.VerifyHash(hash, secretKey))
-	assert.NotNil(t, hasher.VerifyHash(hash, secretKey+":wrong!"))
+func TestSCrypt_VerifyHash(t *testing.T) {
+	type args struct {
+		secretKey string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		setup    func() error // Optional, delete if unused
+		teardown func() error // Optional, delete if unused
+		wantErr  bool
+	}{
+		{
+			name: "Basic Hash Test",
+			args: args{
+				secretKey: "hello world",
+			},
+			setup:    func() error { return nil },
+			teardown: func() error { return nil },
+		},
+		{
+			name: "Long Hash Test",
+			args: args{
+				secretKey: strings.Repeat("A", 720),
+			},
+			setup:    func() error { return nil },
+			teardown: func() error { return nil },
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hasher := NewSCrypt()
+			hash, _ := hasher.CreateHash(tt.args.secretKey)
+			if err := hasher.VerifyHash(hash, tt.args.secretKey); (err != nil) != tt.wantErr {
+				t.Errorf("SCrypt.VerifyHash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
