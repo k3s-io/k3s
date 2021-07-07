a62d143936df6589b2d2d58413d810c23c0add99
Jamie Phillips <jamie.phillips@suse.com>
2021-07-07 15:50:18 -0400

    Fixing various bugs related to windows.
    This changes the crictl template for issues with the socket information. It also addresses a typo in the socket address. Last it makes tweaks to configuration that aren't required or had incorrect logic.
    
    Signed-off-by: Jamie Phillips <jamie.phillips@suse.com>
    
    
    spelling

--- pkg/agent/config/config_windows.go
+++ pkg/agent/config/config_windows.go
@@ -10,5 +10,5 @@ import (
 
 func applyContainerdStateAndAddress(nodeConfig *config.Node) {
 	nodeConfig.Containerd.State = filepath.Join(nodeConfig.Containerd.Root, "state")
-	nodeConfig.Containerd.Address = "npipe://///./pipe/containerd-containerd"
+	nodeConfig.Containerd.Address = "npipe:////./pipe/containerd-containerd"
 }
