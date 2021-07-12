06aaa0fcee15b954aa76f1dd5d3b2cb3ebd2845f
dereknola <derek.nola@suse.com>
2021-07-12 12:13:24 -0700

    Removed unneeded setup and teardown funcs
    Signed-off-by: dereknola <derek.nola@suse.com>

--- pkg/authenticator/hash/scrypt_test.go
+++ pkg/authenticator/hash/scrypt_test.go
@@ -10,27 +10,21 @@ func TestSCrypt_VerifyHash(t *testing.T) {
 		secretKey string
 	}
 	tests := []struct {
-		name     string
-		args     args
-		setup    func() error // Optional, delete if unused
-		teardown func() error // Optional, delete if unused
-		wantErr  bool
+		name    string
+		args    args
+		wantErr bool
 	}{
 		{
 			name: "Basic Hash Test",
 			args: args{
 				secretKey: "hello world",
 			},
-			setup:    func() error { return nil },
-			teardown: func() error { return nil },
 		},
 		{
 			name: "Long Hash Test",
 			args: args{
 				secretKey: strings.Repeat("A", 720),
 			},
-			setup:    func() error { return nil },
-			teardown: func() error { return nil },
 		},
 	}
 	for _, tt := range tests {
