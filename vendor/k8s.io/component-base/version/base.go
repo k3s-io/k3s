12be663c5319f9e97bf8871f385fb043a5f5c72a
Chris Kim <30601846+Oats87@users.noreply.github.com>
2021-07-16 09:55:59 -0700

    [release-1.20] Bump Kubernetes to v1.20.9 (#3655)
    * Bump golang to 1.15.14
    * Update to v1.20.9
    
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "20"
-	gitVersion   = "v1.20.8-k3s1"
-	gitCommit    = "d3d0f184376c7a0e7eedac78e4e3c018da07eba9"
+	gitVersion   = "v1.20.9-k3s1"
+	gitCommit    = "d30af26bd94ad3b90ba23bb51c962cfa46e15db7"
 	gitTreeState = "clean"
-	buildDate = "2021-06-17T20:33:07Z"
+	buildDate = "2021-07-15T23:42:22Z"
 )
