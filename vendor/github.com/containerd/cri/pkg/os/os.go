b9cc6409f4e14012796fbd5bd45af443157e2752
Brad Davidson <brad.davidson@rancher.com>
2021-07-20 13:01:45 -0700

    Bump containerd to v1.4.8-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/github.com/containerd/cri/pkg/os/os.go
+++ vendor/github.com/containerd/cri/pkg/os/os.go
@@ -20,7 +20,6 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 
 	"github.com/docker/docker/pkg/symlink"
 )
@@ -54,18 +53,6 @@ func (RealOS) RemoveAll(path string) error {
 // Stat will call os.Stat to get the status of the given file.
 func (RealOS) Stat(name string) (os.FileInfo, error) {
 	return os.Stat(name)
-}
-
-// ResolveSymbolicLink will follow any symbolic links
-func (RealOS) ResolveSymbolicLink(path string) (string, error) {
-	info, err := os.Lstat(path)
-	if err != nil {
-		return "", err
-	}
-	if info.Mode()&os.ModeSymlink != os.ModeSymlink {
-		return path, nil
-	}
-	return filepath.EvalSymlinks(path)
 }
 
 // FollowSymlinkInScope will call symlink.FollowSymlinkInScope.
