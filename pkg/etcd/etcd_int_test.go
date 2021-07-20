8aebaa953879c94927f3c331a2e46c751ccc4609
dereknola <derek.nola@suse.com>
2021-07-20 09:08:18 -0700

    Tests work for root now
    Signed-off-by: dereknola <derek.nola@suse.com>

--- pkg/etcd/etcd_int_test.go
+++ pkg/etcd/etcd_int_test.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -16,104 +17,109 @@ var serverCmd *exec.Cmd
 var serverScan *bufio.Scanner
 var _ = BeforeSuite(func() {
 	var err error
+	// if !tests.IsRoot() {
+	// 	Fail("User is not root")
+	// }
 	serverCmd, serverScan, err = tests.K3sCmdAsync("server", "--cluster-init")
-	Expect(err).NotTo(HaveOccurred())
+	Expect(err).ToNot(HaveOccurred())
 })
 
 var _ = Describe("etcd snapshots", func() {
 	When("a new etcd is created", func() {
 		It("starts up with no problems", func() {
-			tests.FindStringInCmdAsync(serverScan, "etcd data store connection OK")
+			// tests.FindStringInCmdAsync(serverScan, "etcd data store connection OK")
 			Eventually(func() (string, error) {
 				return tests.K3sCmd("kubectl", "get", "pods", "-A")
-			}, "10s", "2s").Should(MatchRegexp("kube-system.+coredns.+Running"))
+			}, "90s", "1s").Should(MatchRegexp("kube-system.+coredns.+1\\/1.+Running"))
 		})
 		It("saves an etcd snapshot", func() {
-			result, err := tests.K3sCmd("etcd-snapshot", "save")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
+			Expect(tests.K3sCmd("etcd-snapshot", "save")).
+				To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
 		})
-		// It("saves an etcd snapshot", func() {
-		// 	result, err := tests.K3sApp("etcd-snapshot", "save")
-		// 	Expect(err).NotTo(HaveOccurred())
-		// 	Expect(result.AllEntries()).To(ContainElement("Saving current etcd"))
-		// })
 		It("list snapshots", func() {
-			result, err := tests.K3sCmd("etcd-snapshot", "ls")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/on-demand`))
+			Expect(tests.K3sCmd("etcd-snapshot", "ls")).
+				To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/on-demand`))
 		})
 		It("deletes a snapshot", func() {
 			lsResult, err := tests.K3sCmd("etcd-snapshot", "ls")
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).ToNot(HaveOccurred())
 			reg, err := regexp.Compile(`on-demand[^\s]+`)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).ToNot(HaveOccurred())
 			snapshotName := reg.FindString(lsResult)
-			delResult, err := tests.K3sCmd("etcd-snapshot", "delete", snapshotName)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(delResult).To(ContainSubstring("Removing the given locally stored etcd snapshot"))
+			Expect(tests.K3sCmd("etcd-snapshot", "delete", snapshotName)).
+				To(ContainSubstring("Removing the given locally stored etcd snapshot"))
 		})
 	})
 	When("saving a custom name", func() {
 		It("starts with no snapshots", func() {
-			result, err := tests.K3sCmd("etcd-snapshot", "ls")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(BeEmpty())
+			Expect(tests.K3sCmd("etcd-snapshot", "ls")).To(BeEmpty())
 		})
 		It("saves an etcd snapshot with a custom name", func() {
-			result, err := tests.K3sCmd("etcd-snapshot", "save", "--name", "ALIVEBEEF")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(ContainSubstring("Saving etcd snapshot to /var/lib/rancher/k3s/server/db/snapshots/ALIVEBEEF"))
+			Expect(tests.K3sCmd("etcd-snapshot", "save", "--name", "ALIVEBEEF")).
+				To(ContainSubstring("Saving etcd snapshot to /var/lib/rancher/k3s/server/db/snapshots/ALIVEBEEF"))
 		})
 		It("deletes that snapshot", func() {
 			lsResult, err := tests.K3sCmd("etcd-snapshot", "ls")
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).ToNot(HaveOccurred())
 			reg, err := regexp.Compile(`ALIVEBEEF[^\s]+`)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).ToNot(HaveOccurred())
 			snapshotName := reg.FindString(lsResult)
-			delResult, err := tests.K3sCmd("etcd-snapshot", "delete", snapshotName)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(delResult).To(ContainSubstring("Removing the given locally stored etcd snapshot"))
+			Expect(tests.K3sCmd("etcd-snapshot", "delete", snapshotName)).
+				To(ContainSubstring("Removing the given locally stored etcd snapshot"))
 		})
 	})
 	When("using etcd snapshot prune", func() {
 		It("starts with no snapshots", func() {
-			result, err := tests.K3sCmd("etcd-snapshot", "ls")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(BeEmpty())
+			Expect(tests.K3sCmd("etcd-snapshot", "ls")).To(BeEmpty())
 		})
-		It("saves 3 different snapshots, hardcoded with the default CLI name", func() {
-			result, err := tests.K3sCmd("etcd-snapshot", "save", "--")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
-			result, err = tests.K3sCmd("etcd-snapshot", "save")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
-			result, err = tests.K3sCmd("etcd-snapshot", "save")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
+		It("saves 3 different snapshots", func() {
+			Expect(tests.K3sCmd("etcd-snapshot", "save", "-name", "PRUNE_TEST")).
+				To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
+			time.Sleep(1 * time.Second)
+			Expect(tests.K3sCmd("etcd-snapshot", "save", "-name", "PRUNE_TEST")).
+				To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
+			time.Sleep(1 * time.Second)
+			Expect(tests.K3sCmd("etcd-snapshot", "save", "-name", "PRUNE_TEST")).
+				To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
+			time.Sleep(1 * time.Second)
 		})
 		It("lists all 3 snapshots", func() {
 			lsResult, err := tests.K3sCmd("etcd-snapshot", "ls")
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).ToNot(HaveOccurred())
 			sepLines := strings.FieldsFunc(lsResult, func(c rune) bool {
 				return c == '\n'
 			})
-			Expect(lsResult).To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/on-demand`))
+			Expect(lsResult).To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/PRUNE_TEST`))
 			Expect(sepLines).To(HaveLen(3))
-			reg, _ := regexp.Compile(`on-demand[^\s]+`)
-			// Cleanup all test snapshots
-			for _, snap := range reg.FindAllString(lsResult, -1) {
-				delResult, err := tests.K3sCmd("etcd-snapshot", "delete", snap)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(delResult).To(ContainSubstring("Removing the given locally stored etcd snapshot"))
+		})
+		It("prunes snapshots down to 2", func() {
+			Expect(tests.K3sCmd("etcd-snapshot", "prune", "--snapshot-retention", "2", "--name", "PRUNE_TEST")).
+				To(BeEmpty())
+			lsResult, err := tests.K3sCmd("etcd-snapshot", "ls")
+			Expect(err).ToNot(HaveOccurred())
+			sepLines := strings.FieldsFunc(lsResult, func(c rune) bool {
+				return c == '\n'
+			})
+			Expect(lsResult).To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/PRUNE_TEST`))
+			Expect(sepLines).To(HaveLen(2))
+		})
+		It("cleans up remaining snapshots", func() {
+			lsResult, err := tests.K3sCmd("etcd-snapshot", "ls")
+			Expect(err).ToNot(HaveOccurred())
+			reg, err := regexp.Compile(`PRUNE_TEST[^\s]+`)
+			Expect(err).ToNot(HaveOccurred())
+			for _, snapshotName := range reg.FindAllString(lsResult, -1) {
+				Expect(tests.K3sCmd("etcd-snapshot", "delete", snapshotName)).
+					To(ContainSubstring("Removing the given locally stored etcd snapshot"))
 			}
 		})
 	})
 })
 
 var _ = AfterSuite(func() {
-	serverCmd.Process.Kill()
+	if serverCmd != nil {
+		Expect(serverCmd.Process.Kill()).To(Succeed())
+	}
 })
 
 func TestIntegration_Etcd(t *testing.T) {
--- pkg/util/tests/cmd.go
+++ pkg/util/tests/cmd.go
@@ -6,26 +6,9 @@ import (
 	"os/exec"
 	"os/user"
 	"strings"
-)
 
-// func K3sApp(cmdArgs ...string) (*logrustest.Hook, error) {
-// 	app := cmds.NewApp()
-// 	app.Commands = []cli.Command{
-// 		cmds.NewServerCommand(server.Run),
-// 		cmds.NewAgentCommand(agent.Run),
-// 		cmds.NewKubectlCommand(kubectl.Run),
-// 		cmds.NewCRICTL(crictl.Run),
-// 		cmds.NewEtcdSnapshotCommand(etcdsnapshot.Run,
-// 			cmds.NewEtcdSnapshotSubcommands(
-// 				etcdsnapshot.Delete,
-// 				etcdsnapshot.List,
-// 				etcdsnapshot.Prune,
-// 				etcdsnapshot.Run),
-// 		),
-// 	}
-// 	_, hook := logrustest.NewNullLogger()
-// 	return hook, app.Run(configfilearg.MustParse(cmdArgs))
-// }
+	"github.com/sirupsen/logrus"
+)
 
 func findK3sExecutable() string {
 	k3sBin := "dist/artifacts/k3s"
@@ -40,19 +23,23 @@ func findK3sExecutable() string {
 	return k3sBin
 }
 
-func K3sCmd(cmdName string, cmdArgs ...string) (string, error) {
-	k3sBin := findK3sExecutable()
+func IsRoot() bool {
 	currentUser, err := user.Current()
 	if err != nil {
-		return "", err
+		return false
 	}
+	return currentUser.Username == "root"
+}
+
+func K3sCmd(cmdName string, cmdArgs ...string) (string, error) {
+	k3sBin := findK3sExecutable()
 	// Only run sudo if not root
 	var cmd *exec.Cmd
-	if currentUser.Username == "root" {
+	if IsRoot() {
 		k3sCmd := append([]string{cmdName}, cmdArgs...)
 		cmd = exec.Command(k3sBin, k3sCmd...)
 	} else {
-		k3sCmd := append([]string{"-s", k3sBin, cmdName}, cmdArgs...)
+		k3sCmd := append([]string{k3sBin, cmdName}, cmdArgs...)
 		cmd = exec.Command("sudo", k3sCmd...)
 	}
 	byteOut, err := cmd.CombinedOutput()
@@ -61,6 +48,7 @@ func K3sCmd(cmdName string, cmdArgs ...string) (string, error) {
 
 func FindStringInCmdAsync(scanner *bufio.Scanner, target string) bool {
 	for scanner.Scan() {
+		logrus.Info(scanner.Text())
 		if strings.Contains(scanner.Text(), target) {
 			return true
 		}
@@ -71,20 +59,19 @@ func FindStringInCmdAsync(scanner *bufio.Scanner, target string) bool {
 //Launch a k3s command asynchronously
 func K3sCmdAsync(cmdName string, cmdArgs ...string) (*exec.Cmd, *bufio.Scanner, error) {
 	k3sBin := findK3sExecutable()
-	currentUser, err := user.Current()
-	if err != nil {
-		return nil, nil, err
-	}
 	// Only run sudo if not root
 	var cmd *exec.Cmd
-	if currentUser.Username == "root" {
+	if IsRoot() {
 		k3sCmd := append([]string{cmdName}, cmdArgs...)
 		cmd = exec.Command(k3sBin, k3sCmd...)
 	} else {
-		k3sCmd := append([]string{"-s", k3sBin, cmdName}, cmdArgs...)
+		k3sCmd := append([]string{k3sBin, cmdName}, cmdArgs...)
 		cmd = exec.Command("sudo", k3sCmd...)
 	}
-	cmdOut, _ := cmd.StderrPipe()
-	err = cmd.Start()
-	return cmd, bufio.NewScanner(cmdOut), err
+	// cmdOut, _ := cmd.StderrPipe()
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Start()
+	// return cmd, bufio.NewScanner(cmdOut), err
+	return cmd, nil, err
 }
