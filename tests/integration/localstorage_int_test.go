415424f0171714144ce1d68c765f2728e3a03f87
dereknola <derek.nola@suse.com>
2021-07-30 12:26:01 -0700

    Moved test utilities into central tests folder
    Signed-off-by: dereknola <derek.nola@suse.com>

--- tests/integration/localstorage_int_test.go
+++ tests/integration/localstorage_int_test.go
@@ -10,13 +10,13 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/rancher/k3s/pkg/util/tests"
+	"github.com/rancher/k3s/tests/testutil"
 )
 
 var serverCmd *exec.Cmd
 var _ = BeforeSuite(func() {
 	var err error
-	serverCmd, _, err = tests.K3sCmdAsync("server", "--cluster-init")
+	serverCmd, _, err = testutil.K3sCmdAsync("server", "--cluster-init")
 	Expect(err).ToNot(HaveOccurred())
 })
 
@@ -24,24 +24,24 @@ var _ = Describe("local storage", func() {
 	When("a new local storage is created", func() {
 		It("starts up with no problems", func() {
 			Eventually(func() (string, error) {
-				return tests.K3sCmd("kubectl", "get", "pods", "-A")
+				return testutil.K3sCmd("kubectl", "get", "pods", "-A")
 			}, "90s", "1s").Should(MatchRegexp("kube-system.+coredns.+1\\/1.+Running"))
 		})
 		It("creates a new pvc", func() {
-			Expect(tests.K3sCmd("kubectl", "create", "-f", "testdata/localstorage_pvc.yaml")).
+			Expect(testutil.K3sCmd("kubectl", "create", "-f", "testdata/localstorage_pvc.yaml")).
 				To(ContainSubstring("persistentvolumeclaim/local-path-pvc created"))
 		})
 		It("creates a new pod", func() {
-			Expect(tests.K3sCmd("kubectl", "create", "-f", "testdata/localstorage_pod.yaml")).
+			Expect(testutil.K3sCmd("kubectl", "create", "-f", "testdata/localstorage_pod.yaml")).
 				To(ContainSubstring("pod/volume-test created"))
 		})
 		time.Sleep(30 * time.Second)
 		It("shows storage up in kubectl", func() {
 			Eventually(func() (string, error) {
-				return tests.K3sCmd("kubectl", "get", "pv")
+				return testutil.K3sCmd("kubectl", "get", "pv")
 			}, "30s", "1s").Should(MatchRegexp(`pvc.+2Gi.+Bound`))
 			Eventually(func() (string, error) {
-				return tests.K3sCmd("kubectl", "get", "pvc")
+				return testutil.K3sCmd("kubectl", "get", "pvc")
 			}, "10s", "1s").Should(MatchRegexp(`local-path-pvc.+Bound`))
 		})
 		It("has proper folder permissions", func() {
@@ -50,7 +50,7 @@ var _ = Describe("local storage", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(fmt.Sprintf("%04o", fileStat.Mode().Perm())).To(Equal("0701"))
 
-			pvResult, err := tests.K3sCmd("kubectl", "get", "pv")
+			pvResult, err := testutil.K3sCmd("kubectl", "get", "pv")
 			Expect(err).ToNot(HaveOccurred())
 			reg, err := regexp.Compile(`pvc[^\s]+`)
 			Expect(err).ToNot(HaveOccurred())
@@ -60,16 +60,16 @@ var _ = Describe("local storage", func() {
 			Expect(fmt.Sprintf("%04o", fileStat.Mode().Perm())).To(Equal("0777"))
 		})
 		It("deletes properly", func() {
-			Expect(tests.K3sCmd("kubectl", "delete", "pod", "volume-test")).
+			Expect(testutil.K3sCmd("kubectl", "delete", "pod", "volume-test")).
 				To(ContainSubstring("pod \"volume-test\" deleted"))
-			Expect(tests.K3sCmd("kubectl", "delete", "pvc", "local-path-pvc")).
+			Expect(testutil.K3sCmd("kubectl", "delete", "pvc", "local-path-pvc")).
 				To(ContainSubstring("persistentvolumeclaim \"local-path-pvc\" deleted"))
 		})
 	})
 })
 
 var _ = AfterSuite(func() {
-	Expect(tests.K3sKillAsync(serverCmd)).To(Succeed())
+	Expect(testutil.K3sKillAsync(serverCmd)).To(Succeed())
 })
 
 func Test_IntegrationLocalStorage(t *testing.T) {
--- tests/testutil/runtime.go
+++ tests/testutil/runtime.go
@@ -1,4 +1,4 @@
-package tests
+package testutil
 
 import (
 	"os"
