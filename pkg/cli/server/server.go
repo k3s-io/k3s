0b8b73cda9e1ad4f07122ab6dc31583d86a356de
Hussein Galal <galal-hussein@users.noreply.github.com>
2021-08-03 19:53:21 +0200

    Fix initial start of etcd only nodes (#3748)
    * Fix initial start of etcd only nodes
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * more fixes
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * more fixes
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    (cherry picked from commit 2069cdf4eee6af8ba9c7523e62abaa511bac709e)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cli/server/server.go
+++ pkg/cli/server/server.go
@@ -371,6 +371,8 @@ func run(app *cli.Context, cfg *cmds.Server, leaderControllers server.CustomCont
 
 	logrus.Info("Starting " + version.Program + " " + app.App.Version)
 
+	notifySocket := os.Getenv("NOTIFY_SOCKET")
+
 	ctx := signals.SetupSignalHandler(context.Background())
 
 	if err := server.StartServer(ctx, &serverConfig); err != nil {
@@ -387,7 +389,8 @@ func run(app *cli.Context, cfg *cmds.Server, leaderControllers server.CustomCont
 		}
 
 		logrus.Info(version.Program + " is up and running")
-		if (cfg.DisableAgent || cfg.DisableAPIServer) && os.Getenv("NOTIFY_SOCKET") != "" {
+		if (cfg.DisableAgent || cfg.DisableAPIServer) && notifySocket != "" {
+			os.Setenv("NOTIFY_SOCKET", notifySocket)
 			systemd.SdNotify(true, "READY=1\n")
 		}
 	}()
