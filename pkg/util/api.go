e8d30101a652508afcff93ea85783d15d2ffc0b8
Brad Davidson <brad.davidson@rancher.com>
2021-09-10 14:02:51 -0700

    Wait for apiserver readyz instead of healthz
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 3449d5b9f9d5d044981fea40e6818be47013f042)

--- pkg/util/api.go
+++ pkg/util/api.go
@@ -1,10 +1,17 @@
 package util
 
 import (
+	"context"
+	"fmt"
 	"net"
+	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	clientset "k8s.io/client-go/kubernetes"
 )
 
 func GetAddresses(endpoint *v1.Endpoints) []string {
@@ -25,4 +32,33 @@ func GetAddresses(endpoint *v1.Endpoints) []string {
 		}
 	}
 	return serverAddresses
+}
+
+// WaitForAPIServerReady waits for the API Server's /readyz endpoint to report "ok" with timeout.
+// This is cribbed from the Kubernetes controller-manager app, but checks the readyz endpoint instead of the deprecated healthz endpoint.
+func WaitForAPIServerReady(client clientset.Interface, timeout time.Duration) error {
+	var lastErr error
+
+	err := wait.PollImmediate(time.Second, timeout, func() (bool, error) {
+		healthStatus := 0
+		result := client.Discovery().RESTClient().Get().AbsPath("/readyz").Do(context.TODO()).StatusCode(&healthStatus)
+		if result.Error() != nil {
+			lastErr = fmt.Errorf("failed to get apiserver /readyz status: %v", result.Error())
+			return false, nil
+		}
+		if healthStatus != http.StatusOK {
+			content, _ := result.Raw()
+			lastErr = fmt.Errorf("APIServer isn't ready: %v", string(content))
+			logrus.Warnf("APIServer isn't ready yet: %v. Waiting a little while.", string(content))
+			return false, nil
+		}
+
+		return true, nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("%v: %v", err, lastErr)
+	}
+
+	return nil
 }
