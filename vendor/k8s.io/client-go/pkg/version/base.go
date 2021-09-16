f16a94747137b5f473c7591485e4ed3c605c2102
Manuel Buil <mbuil@suse.com>
2021-09-16 17:28:27 +0200

    Update to v1.19.15
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "19"
-	gitVersion   = "v1.19.14-k3s1"
-	gitCommit    = "de864bbd93b6636951750854c6be3a9b07aa1fac"
+	gitVersion   = "v1.19.15-k3s1"
+	gitCommit    = "a14bf227db31db89a172864ca2a934ff1c637eef"
 	gitTreeState = "clean"
-	buildDate = "2021-08-12T22:23:54Z"
+	buildDate = "2021-09-16T15:13:53Z"
 )
