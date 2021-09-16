4ec3f7a0622a9404655eaa4e87388124cf5e7120
Brad Davidson <brad.davidson@rancher.com>
2021-09-16 12:59:58 -0700

    Update Kubernetes to v1.22.2-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "22"
-	gitVersion   = "v1.22.1-k3s1"
-	gitCommit    = "1d402161376010a3f98defedab2be2941b04abb1"
+	gitVersion   = "v1.22.2-k3s1"
+	gitCommit    = "c4ebe1924cba7aeae2a97729df3e0ac4a2ce5459"
 	gitTreeState = "clean"
-	buildDate = "2021-08-20T09:21:43Z"
+	buildDate = "2021-09-16T19:43:43Z"
 )
