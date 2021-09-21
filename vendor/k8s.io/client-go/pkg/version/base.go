d2aa8dad452534488508fbc9f73d193adf4a5088
Chris Kim <oats87g@gmail.com>
2021-09-20 17:38:41 -0700

    Update to Kubernetes v1.21.5
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "21"
-	gitVersion   = "v1.21.4-k3s1"
-	gitCommit    = "24d3954c6cd8b757150dd3b0ebf02ad005f07523"
+	gitVersion   = "v1.21.5-k3s1"
+	gitCommit    = "43d7f3c07ed0f5f1e5a6c7f8f0122ec731aebc05"
 	gitTreeState = "clean"
-	buildDate = "2021-08-12T22:35:54Z"
+	buildDate = "2021-09-16T16:49:15Z"
 )
