f8dee364c9ebaadac670e6be9fffece6ee272e0d
msimonelli331 <matt.simonelli8@gmail.com>
2021-10-13 14:02:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -707,7 +707,7 @@ rm -f ${BIN_DIR}/k3s
 rm -f ${KILLALL_K3S_SH}
 
 if [ "$INSTALL_K3S_SKIP_SELINUX_RPM" = true ] || can_skip_download; then
-    info "Skipping uninstall of SELinux RPM becuase we didn't install it"
+    info "Skipping uninstall of SELinux RPM because we didn't install it"
 else
     if type yum >/dev/null 2>&1; then
         yum remove -y k3s-selinux
