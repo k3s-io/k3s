f399bd29b1684e727fe251117b129fe1d97e10f4
Manuel Buil <mbuil@suse.com>
2021-10-22 09:05:50 +0200

    Update to flannel v0.14.1
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- vendor/github.com/k3s-io/go-powershell/utils/quote.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2017 Gorillalabs. All rights reserved.
-
-package utils
-
-import "strings"
-
-func QuoteArg(s string) string {
-	return "'" + strings.Replace(s, "'", "\"", -1) + "'"
-}
