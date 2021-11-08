cb0918ae3c8d5f1c4b02aa5f0580876519ec2d88
Luther Monson <luther.monson@gmail.com>
2021-11-08 11:52:54 -0700

    [release-1.22] updating to new signals package in wrangler (#4422)
    * updating to new signals package in wrangler
    
    Signed-off-by: Luther Monson <luther.monson@gmail.com>

--- pkg/agent/containerd/containerd.go
+++ pkg/agent/containerd/containerd.go
@@ -86,7 +86,7 @@ func Run(ctx context.Context, cfg *config.Node) error {
 		}
 
 		logrus.Infof("Running containerd %s", config.ArgString(args[1:]))
-		cmd := exec.Command(args[0], args[1:]...)
+		cmd := exec.CommandContext(ctx, args[0], args[1:]...)
 		cmd.Stdout = stdOut
 		cmd.Stderr = stdErr
 		cmd.Env = env
