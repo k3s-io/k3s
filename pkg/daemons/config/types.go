381d086cf0c18880c1c265a1ca6bf788398dc764
Chris Kim <30601846+Oats87@users.noreply.github.com>
2021-11-11 19:53:20 -0800

    [engine-1.21] Add etcd extra args support for K3s (#4470)
    * Add etcd extra args support for K3s
    
    Signed-off-by: Chris Kim <oats87g@gmail.com>
    
    * Add etcd custom argument integration test
    
    Signed-off-by: Chris Kim <oats87g@gmail.com>
    
    * Redux: Enable K3s integration test to run on existing cluster (#3905)
    
    * Made it possible to run int tests on existing cluster
    
    Signed-off-by: dereknola <derek.nola@suse.com>
    
    Signed-off-by: Chris Kim <oats87g@gmail.com>
    
    Co-authored-by: Derek Nola <derek.nola@suse.com>

--- pkg/daemons/config/types.go
+++ pkg/daemons/config/types.go
@@ -131,6 +131,7 @@ type Control struct {
 	ExtraAPIArgs             []string
 	ExtraControllerArgs      []string
 	ExtraCloudControllerArgs []string
+	ExtraEtcdArgs            []string
 	ExtraSchedulerAPIArgs    []string
 	NoLeaderElect            bool
 	JoinURL                  string
--- pkg/etcd/etcd_int_test.go
+++ pkg/etcd/etcd_int_test.go
@@ -12,13 +12,21 @@ import (
 )
 
 var server *testutil.K3sServer
+var serverArgs = []string{"--cluster-init"}
 var _ = BeforeSuite(func() {
-	var err error
-	server, err = testutil.K3sStartServer("--cluster-init")
-	Expect(err).ToNot(HaveOccurred())
+	if !testutil.IsExistingServer() {
+		var err error
+		server, err = testutil.K3sStartServer(serverArgs...)
+		Expect(err).ToNot(HaveOccurred())
+	}
 })
 
 var _ = Describe("etcd snapshots", func() {
+	BeforeEach(func() {
+		if testutil.IsExistingServer() && !testutil.ServerArgsPresent(serverArgs) {
+			Skip("Test needs k3s server with: " + strings.Join(serverArgs, " "))
+		}
+	})
 	When("a new etcd is created", func() {
 		It("starts up with no problems", func() {
 			Eventually(func() (string, error) {
@@ -44,9 +52,6 @@ var _ = Describe("etcd snapshots", func() {
 		})
 	})
 	When("saving a custom name", func() {
-		It("starts with no snapshots", func() {
-			Expect(testutil.K3sCmd("etcd-snapshot", "ls")).To(BeEmpty())
-		})
 		It("saves an etcd snapshot with a custom name", func() {
 			Expect(testutil.K3sCmd("etcd-snapshot", "save", "--name", "ALIVEBEEF")).
 				To(ContainSubstring("Saving etcd snapshot to /var/lib/rancher/k3s/server/db/snapshots/ALIVEBEEF"))
@@ -62,9 +67,6 @@ var _ = Describe("etcd snapshots", func() {
 		})
 	})
 	When("using etcd snapshot prune", func() {
-		It("starts with no snapshots", func() {
-			Expect(testutil.K3sCmd("etcd-snapshot", "ls")).To(BeEmpty())
-		})
 		It("saves 3 different snapshots", func() {
 			Expect(testutil.K3sCmd("etcd-snapshot", "save", "-name", "PRUNE_TEST")).
 				To(ContainSubstring("Saving current etcd snapshot set to k3s-etcd-snapshots"))
@@ -79,10 +81,9 @@ var _ = Describe("etcd snapshots", func() {
 		It("lists all 3 snapshots", func() {
 			lsResult, err := testutil.K3sCmd("etcd-snapshot", "ls")
 			Expect(err).ToNot(HaveOccurred())
-			sepLines := strings.FieldsFunc(lsResult, func(c rune) bool {
-				return c == '\n'
-			})
-			Expect(lsResult).To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/PRUNE_TEST`))
+			reg, err := regexp.Compile(`:///var/lib/rancher/k3s/server/db/snapshots/PRUNE_TEST`)
+			Expect(err).ToNot(HaveOccurred())
+			sepLines := reg.FindAllString(lsResult, -1)
 			Expect(sepLines).To(HaveLen(3))
 		})
 		It("prunes snapshots down to 2", func() {
@@ -90,10 +91,9 @@ var _ = Describe("etcd snapshots", func() {
 				To(BeEmpty())
 			lsResult, err := testutil.K3sCmd("etcd-snapshot", "ls")
 			Expect(err).ToNot(HaveOccurred())
-			sepLines := strings.FieldsFunc(lsResult, func(c rune) bool {
-				return c == '\n'
-			})
-			Expect(lsResult).To(MatchRegexp(`:///var/lib/rancher/k3s/server/db/snapshots/PRUNE_TEST`))
+			reg, err := regexp.Compile(`:///var/lib/rancher/k3s/server/db/snapshots/PRUNE_TEST`)
+			Expect(err).ToNot(HaveOccurred())
+			sepLines := reg.FindAllString(lsResult, -1)
 			Expect(sepLines).To(HaveLen(2))
 		})
 		It("cleans up remaining snapshots", func() {
@@ -110,7 +110,9 @@ var _ = Describe("etcd snapshots", func() {
 })
 
 var _ = AfterSuite(func() {
-	Expect(testutil.K3sKillServer(server)).To(Succeed())
+	if !testutil.IsExistingServer() {
+		Expect(testutil.K3sKillServer(server)).To(Succeed())
+	}
 })
 
 func Test_IntegrationEtcd(t *testing.T) {
