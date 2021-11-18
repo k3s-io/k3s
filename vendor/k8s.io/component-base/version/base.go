acf0887beb3b17474ff4f78ae7f3d521b34803cb
Chris Kim <oats87g@gmail.com>
2021-11-18 10:00:59 -0800

    Update to v1.22.4
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "22"
-	gitVersion   = "v1.22.3-k3s1"
-	gitCommit    = "5d8c744cb54fc54d261b2f14e8464049aad7e32c"
+	gitVersion   = "v1.22.4-k3s1"
+	gitCommit    = "00226be68bd6b70f8fca252a1027fc0a35d070b8"
 	gitTreeState = "clean"
-	buildDate = "2021-10-28T15:23:07Z"
+	buildDate = "2021-11-17T22:30:06Z"
 )
