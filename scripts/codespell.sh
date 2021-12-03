bc8c9794e2232a4c706e2d3643401fbdce8f5dd0
Brad Davidson <brad.davidson@rancher.com>
2021-12-03 13:44:05 -0800

    More codespell ignores
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- scripts/codespell.sh
+++ scripts/codespell.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 
 # Ignore vendor folder and check file names as well
 # Note: ignore "ba" in https://github.com/k3s-io/k3s/blob/4317a91/scripts/provision/vagrant#L54
-codespell --skip=.git,./vendor,MAINTAINERS --check-filenames --ignore-words-list=ba
+codespell --skip=.git,./vendor,MAINTAINERS,go.mod,go.sum --check-filenames --ignore-words-list=ba
 
 code=$?
 if [ $code -ne 0 ]; then
