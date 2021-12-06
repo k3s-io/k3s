7b6290083672764020ff30d94645ceb2a458ee4a
Hussein Galal <galal-hussein@users.noreply.github.com>
2021-12-06 19:45:21 +0200

    [Release-1.21] Add cert rotation command (#4632)
    * Add cert rotation command (#4495)
    
    * Add cert rotation command
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * add function to check for dynamic listener file
    
    Signed-off-by: Brian Downs <brian.downs@gmail.com>
    
    * Add dynamiclistener cert rotation support
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * fixes to the cert rotation
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * fix ci tests
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * fixes to certificate rotation command
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * more fixes
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    Co-authored-by: Brian Downs <brian.downs@gmail.com>
    
    * Upgrade dynamic listener
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    * go mod tidy
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>
    
    Co-authored-by: Brian Downs <brian.downs@gmail.com>

--- cmd/k3s/main.go
+++ cmd/k3s/main.go
@@ -34,6 +34,7 @@ func main() {
 	}
 
 	etcdsnapshotCommand := internalCLIAction(version.Program+"-"+cmds.EtcdSnapshotCommand, dataDir, os.Args)
+	certCommand := internalCLIAction(version.Program+"-"+cmds.CertCommand, dataDir, os.Args)
 
 	// Handle subcommand invocation (k3s server, k3s crictl, etc)
 	app := cmds.NewApp()
@@ -50,6 +51,10 @@ func main() {
 				etcdsnapshotCommand,
 				etcdsnapshotCommand,
 				etcdsnapshotCommand),
+		),
+		cmds.NewCertCommand(
+			cmds.NewCertSubcommands(
+				certCommand),
 		),
 	}
 
