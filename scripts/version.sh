c390292d12b6a985f48142719b7f2cb0d0979dda
Brad Davidson <brad.davidson@rancher.com>
2021-12-15 16:14:27 -0800

    Build script cleanups
    * Move runc and k3s-root versions into versions.sh
    * Remove commented-out cruft from build script
    * Other minor tweaks
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 8737e2e13f1c080cd4177e6ca79ab1cec9c4e352)

--- scripts/version.sh
+++ scripts/version.sh
@@ -37,7 +37,14 @@ if [ -z "$VERSION_K8S" ]; then
     VERSION_K8S="v0.0.0"
 fi
 
-VERSION_CNIPLUGINS="v0.8.6-k3s1"
+VERSION_RUNC=$(grep github.com/opencontainers/runc go.mod | head -n1 | awk '{print $4}')
+if [ -z "$VERSION_RUNC" ]; then
+    VERSION_RUNC="v0.0.0"
+fi
+
+VERSION_CNIPLUGINS="v0.9.1-k3s1"
+
+VERSION_ROOT="v0.9.1"
 
 if [[ -n "$GIT_TAG" ]]; then
     if [[ ! "$GIT_TAG" =~ ^"$VERSION_K8S"[+-] ]]; then
