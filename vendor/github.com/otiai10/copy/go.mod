a7228ce6ada603c7f6e2d43c90dfb296c7abedb6
Luther Monson <luther.monson@gmail.com>
2021-12-22 14:39:51 -0700

    remove vendor dir
    Signed-off-by: Luther Monson <luther.monson@gmail.com>

--- vendor/github.com/otiai10/copy/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/otiai10/copy
-
-go 1.14
-
-require github.com/otiai10/mint v1.3.2
--- vendor/github.com/rootless-containers/rootlesskit/pkg/port/builtin/opaque/opaque.go
+++
@@ -1,6 +0,0 @@
-package opaque
-
-const (
-	SocketPath         = "builtin.socketpath"
-	ChildReadyPipePath = "builtin.readypipepath"
-)
