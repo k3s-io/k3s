7c3a691f1aa856ee221026da326cb33962d0a000
Luther Monson <luther.monson@gmail.com>
2021-12-27 15:00:55 -0700

    remove vendor dir
    Signed-off-by: Luther Monson <luther.monson@gmail.com>

--- vendor/github.com/Microsoft/go-winio/pkg/etw/newprovider_unsupported.go
+++
@@ -1,9 +0,0 @@
-// +build windows
-// +build arm
-
-package etw
-
-// NewProviderWithID returns a nil provider on unsupported platforms.
-func NewProviderWithOptions(name string, options ...ProviderOpt) (provider *Provider, err error) {
-	return nil, nil
-}
