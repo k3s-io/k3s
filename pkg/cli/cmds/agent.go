b4c0a048fcc074dc832a4c95637595c62fe8bb26
Brad Davidson <brad.davidson@rancher.com>
2022-01-14 02:50:47 -0800

    Update packaged components
    Update images and manifests/charts for coredns, local-path-provisioner, traefik, and pause
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cli/cmds/agent.go
+++ pkg/cli/cmds/agent.go
@@ -111,7 +111,7 @@ var (
 		Name:        "pause-image",
 		Usage:       "(agent/runtime) Customized pause image for containerd or docker sandbox",
 		Destination: &AgentConfig.PauseImage,
-		Value:       "rancher/pause:3.1",
+		Value:       "rancher/mirrored-pause:3.5",
 	}
 	SnapshotterFlag = cli.StringFlag{
 		Name:        "snapshotter",
