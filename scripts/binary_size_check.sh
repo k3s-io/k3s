876355c6de210ff70d17ed490e41604f92c3bdff
Romeo Ninov <rninov@gmail.com>
2022-01-17 21:20:06 +0200

    Use double quotes for variables in IF
    Use double quotes for variables in if and use `-eq` for compare numbers

--- scripts/binary_size_check.sh
+++ scripts/binary_size_check.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "${DEBUG}" = 1 ]; then
+if [ "${DEBUG}" -eq 1 ]; then
     set -x
 fi
 
@@ -13,16 +13,16 @@ fi
 MAX_BINARY_MB=64
 MAX_BINARY_SIZE=$((MAX_BINARY_MB * 1024 * 1024))
 BIN_SUFFIX="-${ARCH}"
-if [ ${ARCH} = amd64 ]; then
+if [ "${ARCH}" = amd64 ]; then
     BIN_SUFFIX=""
-elif [ ${ARCH} = arm ]; then
+elif [ "${ARCH}" = arm ]; then
     BIN_SUFFIX="-armhf"
 fi
 
 CMD_NAME="dist/artifacts/k3s${BIN_SUFFIX}"
 SIZE=$(stat -c '%s' ${CMD_NAME})
 
-if [ ${SIZE} -gt ${MAX_BINARY_SIZE} ]; then
+if [ "${SIZE}" -gt |${MAX_BINARY_SIZE}| ]; then
   echo "k3s binary ${CMD_NAME} size ${SIZE} exceeds max acceptable size of ${MAX_BINARY_SIZE} bytes (${MAX_BINARY_MB} MiB)"
   exit 1
 fi
