86559341a42604bc26d4fcb9f65d97c35b09e413
Brian Downs <brian.downs@gmail.com>
2022-03-09 16:33:16 -0700

    remove unused code (#5247)

--- pkg/daemons/agent/agent_windows.go
+++ pkg/daemons/agent/agent_windows.go
@@ -30,11 +30,6 @@ func checkRuntimeEndpoint(cfg *config.Agent, argsMap map[string]string) {
 }
 
 func kubeProxyArgs(cfg *config.Agent) map[string]string {
-	bindAddress := "127.0.0.1"
-	_, IPv6only, _ := util.GetFirstString([]string{cfg.NodeIP})
-	if IPv6only {
-		bindAddress = "::1"
-	}
 	argsMap := map[string]string{
 		"proxy-mode":           "kernelspace",
 		"healthz-bind-address": "127.0.0.1",
@@ -53,11 +48,6 @@ func kubeProxyArgs(cfg *config.Agent) map[string]string {
 }
 
 func kubeletArgs(cfg *config.Agent) map[string]string {
-	bindAddress := "127.0.0.1"
-	_, IPv6only, _ := util.GetFirstString([]string{cfg.NodeIP})
-	if IPv6only {
-		bindAddress = "::1"
-	}
 	argsMap := map[string]string{
 		"healthz-bind-address":         "127.0.0.1",
 		"read-only-port":               "0",
