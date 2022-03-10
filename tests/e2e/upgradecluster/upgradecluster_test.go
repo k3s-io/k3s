da052f079e8b74a4a1e9b18f5610e05f26a491a3
Derek Nola <derek.nola@suse.com>
2022-03-10 14:54:39 -0800

    Fix comments around E2E ENVs
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/upgradecluster/upgradecluster_test.go
+++ tests/e2e/upgradecluster/upgradecluster_test.go
@@ -18,7 +18,9 @@ var serverCount = flag.Int("serverCount", 3, "number of server nodes")
 var agentCount = flag.Int("agentCount", 2, "number of agent nodes")
 
 // Environment Variables Info:
-// E2E_RELEASE_VERSION=v1.23.1+k3s2 or nil for latest commit from master
+// E2E_RELEASE_VERSION=v1.23.3+k3s1
+// OR
+// E2E_RELEASE_CHANNEL=(commit|latest|stable), commit pulls latest commit from master
 
 func Test_E2EUpgradeValidation(t *testing.T) {
 	RegisterFailHandler(Fail)
