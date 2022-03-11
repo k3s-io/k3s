360f18d1cf6c36169ccbd9ffa5760196840be91e
Brad Davidson <brad.davidson@rancher.com>
2022-03-10 17:01:53 -0800

    Always set pod-infra-container-image to protect it from image GC
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/daemons/agent/agent_linux.go
+++ pkg/daemons/agent/agent_linux.go
@@ -96,7 +96,8 @@ func kubeletArgs(cfg *config.Agent) map[string]string {
 	if cfg.RuntimeSocket != "" {
 		argsMap["serialize-image-pulls"] = "false"
 		checkRuntimeEndpoint(cfg, argsMap)
-	} else if cfg.PauseImage != "" {
+	}
+	if cfg.PauseImage != "" {
 		argsMap["pod-infra-container-image"] = cfg.PauseImage
 	}
 	if cfg.ImageServiceSocket != "" {
