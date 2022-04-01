469239e534bd16d30820835b0a053b52c788aeab
Terry Cain <terry@terrys-home.co.uk>
2022-04-01 23:02:06 +0100

    Added support for repeated extra arguments
    Problem:
    Specifying extra arguments for the API server for example is not supported as
    the arguments get stored in a map before being passed to the API server.
    
    Solution:
    Updated the GetArgs function to store the arguments in a map that can have
    multiple values. Some more logic is added so that repeated extra arguments
    retain their order when sorted whilst overall the arguments can still be
    sorted for improved readability when logged.
    
    Support has been added for prefixing and suffixing default argument values
    by using -= and += when specifying extra arguments.
    
    Signed-off-by: Terry Cain <terry@terrys-home.co.uk>

--- pkg/daemons/config/types_test.go
+++ pkg/daemons/config/types_test.go
@@ -79,6 +79,67 @@ func Test_UnitGetArgs(t *testing.T) {
 				"--iii=II",
 			},
 		},
+		{
+			name: "Multiple args with defaults Test",
+			args: args{
+				argsMap: map[string]string{
+					"aaa": "A",
+					"bbb": "B",
+				},
+				extraArgs: []string{
+					"--ccc=C",
+					"--bbb=DD",
+					"--bbb=AA",
+				},
+			},
+
+			want: []string{
+				"--aaa=A",
+				"--bbb=DD",
+				"--bbb=AA",
+				"--ccc=C",
+			},
+		},
+		{
+			name: "Multiple args with defaults and prefix",
+			args: args{
+				argsMap: map[string]string{
+					"aaa": "A",
+					"bbb": "B",
+				},
+				extraArgs: []string{
+					"--ccc=C",
+					"--bbb-=DD",
+				},
+			},
+
+			want: []string{
+				"--aaa=A",
+				"--bbb=DD",
+				"--bbb=B",
+				"--ccc=C",
+			},
+		},
+		{
+			name: "Multiple args with defaults and suffix",
+			args: args{
+				argsMap: map[string]string{
+					"aaa": "A",
+					"bbb": "B",
+				},
+				extraArgs: []string{
+					"--ccc=C",
+					"--bbb+=DD",
+				},
+			},
+
+			want: []string{
+				"--aaa=A",
+				"--bbb=B",
+				"--bbb=DD",
+				"--ccc=C",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
