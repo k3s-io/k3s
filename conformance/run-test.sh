9d2281a8db2410fe4798e0708b150b1b66fd4690
Dirk Mueller <dirk@dmllr.de>
2022-04-14 22:29:47 +0200

    update sonobuoy to 0.56.4 (#5419)
    This appears to be fixing the CI testing in local testing.
    
    Signed-off-by: Dirk Müller <dirk@dmllr.de>

--- conformance/run-test.sh
+++ conformance/run-test.sh
@@ -11,6 +11,6 @@ sed 's/localhost/server/g' /etc/rancher/k3s/k3s.yaml > /root/.kube/config
 export KUBECONFIG=/root/.kube/config
 cat /etc/rancher/k3s/k3s.yaml
 cat $KUBECONFIG
-sonobuoy run --sonobuoy-image=rancher/sonobuoy-sonobuoy:v0.19.0
+sonobuoy run --sonobuoy-image=rancher/sonobuoy-sonobuoy:v0.56.4
 sleep 15
 sonobuoy logs -f
