07bd2ddea93f302f103cc51134cdf51b75786ff1
Brad Davidson <brad.davidson@rancher.com>
2022-04-18 16:06:50 -0700

    Add systemd cgroup controller support
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/agent/containerd/config_linux.go
+++ pkg/agent/containerd/config_linux.go
@@ -45,18 +45,22 @@ func setupContainerdConfig(ctx context.Context, cfg *config.Node) error {
 	}
 
 	isRunningInUserNS := userns.RunningInUserNS()
-	_, _, hasCFS, hasPIDs := cgroups.CheckCgroups()
+	_, _, controllers := cgroups.CheckCgroups()
 	// "/sys/fs/cgroup" is namespaced
 	cgroupfsWritable := unix.Access("/sys/fs/cgroup", unix.W_OK) == nil
-	disableCgroup := isRunningInUserNS && (!hasCFS || !hasPIDs || !cgroupfsWritable)
+	disableCgroup := isRunningInUserNS && (!controllers["cpu"] || !controllers["pids"] || !cgroupfsWritable)
 	if disableCgroup {
 		logrus.Warn("cgroup v2 controllers are not delegated for rootless. Disabling cgroup.")
 	}
+
+	systemdCgroup := controllers["cpuset"] && os.Getenv("NOTIFY_SOCKET") != ""
+	cfg.AgentConfig.Systemd = systemdCgroup
 
 	var containerdTemplate string
 	containerdConfig := templates.ContainerdConfig{
 		NodeConfig:            cfg,
 		DisableCgroup:         disableCgroup,
+		SystemdCgroup:         systemdCgroup,
 		IsRunningInUserNS:     isRunningInUserNS,
 		PrivateRegistryConfig: privRegistries.Registry,
 		ExtraRuntimes:         findNvidiaContainerRuntimes(os.DirFS(string(os.PathSeparator))),
