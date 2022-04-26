de467deb6b536f39faaeb28c038a39aa97baabdc
Derek Nola <derek.nola@suse.com>
2022-04-26 15:35:13 -0700

    Rancher deployment script
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/scripts/rancher.sh
+++ tests/e2e/scripts/rancher.sh
@@ -5,10 +5,16 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
+
+echo  "Give K3s time to startup"
+sleep 15
 kubectl create namespace cattle-system
 kubectl create namespace cert-manager
 kubectl apply --validate=false -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
 
-helm install cert-manager jetstack/cert-manager --namespace cert-manager
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --kubeconfig /etc/rancher/k3s/k3s.yaml
 kubectl get pods --namespace cert-manager
-helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname="$node_ip".nip.io
+helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname="$node_ip".nip.io --kubeconfig /etc/rancher/k3s/k3s.yaml
+echo "Give Rancher time to startup"
+sleep 80
+echo https://10.10.10.100.nip.io/dashboard/?setup=$(kubectl get secret --namespace cattle-system bootstrap-secret -o go-template='{{.data.bootstrapPassword|base64decode}}')
--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -1,13 +1,14 @@
 def defaultOSConfigure(vm)
-    if vm.box.include?("generic/ubuntu")
-      vm.provision "Set DNS", type: "shell", inline: "systemd-resolve --set-dns=8.8.8.8 --interface=eth0"
-      vm.provision "Install jq", type: "shell", inline: "apt install -y jq"
-    elsif vm.box.include?("Leap") || vm.box.include?("Tumbleweed")
-      vm.provision "Install jq", type: "shell", inline: "zypper install -y jq"
-    elsif vm.box.include?("alpine")
-      vm.provision "Install tools", type: "shell", inline: "apk add jq coreutils"
-    elsif vm.box.include?("microos")
-      vm.provision "Install jq", type: "shell", inline: "transactional-update pkg install -y jq"
-      vm.provision 'reload', run: 'once'
-    end 
+  box = vm.box.to_s
+  if box.include?("generic/ubuntu")
+    vm.provision "Set DNS", type: "shell", inline: "systemd-resolve --set-dns=8.8.8.8 --interface=eth0"
+    vm.provision "Install jq", type: "shell", inline: "apt install -y jq"
+  elsif box.include?("Leap") || box.include?("Tumbleweed")
+    vm.provision "Install jq", type: "shell", inline: "zypper install -y jq"
+  elsif box.include?("alpine")
+    vm.provision "Install tools", type: "shell", inline: "apk add jq coreutils"
+  elsif box.include?("microos")
+    vm.provision "Install jq", type: "shell", inline: "transactional-update pkg install -y jq"
+    vm.provision 'reload', run: 'once'
+  end 
 end
