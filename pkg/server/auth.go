93ac87451dbbb3d8e303a16904530f0db3b18943
Brad Davidson <brad.davidson@rancher.com>
2022-04-27 02:09:58 -0700

    Make supervisor errors parsable by Kubernetes client libs
    This gives nicer errors from Kubernetes components during startup, and
    reduces LOC a bit by using the upstream responsewriters module instead
    of writing the headers and body by hand.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 3d01ca13092377820b495fd64c748408cbb80e7b)

--- pkg/server/auth.go
+++ pkg/server/auth.go
@@ -5,7 +5,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/k3s-io/k3s/pkg/daemons/config"
+	"github.com/k3s-io/k3s/pkg/generated/clientset/versioned/scheme"
 	"github.com/sirupsen/logrus"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/endpoints/handlers/responsewriters"
 	"k8s.io/apiserver/pkg/endpoints/request"
 )
 
@@ -24,23 +29,23 @@ func doAuth(roles []string, serverConfig *config.Control, next http.Handler, rw 
 	switch {
 	case serverConfig == nil:
 		logrus.Errorf("Authenticate not initialized: serverConfig is nil")
-		rw.WriteHeader(http.StatusUnauthorized)
+		unauthorized(rw, req)
 		return
 	case serverConfig.Runtime.Authenticator == nil:
 		logrus.Errorf("Authenticate not initialized: serverConfig.Runtime.Authenticator is nil")
-		rw.WriteHeader(http.StatusUnauthorized)
+		unauthorized(rw, req)
 		return
 	}
 
 	resp, ok, err := serverConfig.Runtime.Authenticator.AuthenticateRequest(req)
 	if err != nil {
 		logrus.Errorf("Failed to authenticate request from %s: %v", req.RemoteAddr, err)
-		rw.WriteHeader(http.StatusUnauthorized)
+		unauthorized(rw, req)
 		return
 	}
 
 	if !ok || !hasRole(roles, resp.User.GetGroups()) {
-		rw.WriteHeader(http.StatusForbidden)
+		forbidden(rw, req)
 		return
 	}
 
@@ -55,4 +60,28 @@ func authMiddleware(serverConfig *config.Control, roles ...string) mux.Middlewar
 			doAuth(roles, serverConfig, next, rw, req)
 		})
 	}
+}
+
+func unauthorized(resp http.ResponseWriter, req *http.Request) {
+	responsewriters.ErrorNegotiated(
+		&apierrors.StatusError{ErrStatus: metav1.Status{
+			Status:  metav1.StatusFailure,
+			Code:    http.StatusUnauthorized,
+			Reason:  metav1.StatusReasonUnauthorized,
+			Message: "not authorized",
+		}},
+		scheme.Codecs.WithoutConversion(), schema.GroupVersion{}, resp, req,
+	)
+}
+
+func forbidden(resp http.ResponseWriter, req *http.Request) {
+	responsewriters.ErrorNegotiated(
+		&apierrors.StatusError{ErrStatus: metav1.Status{
+			Status:  metav1.StatusFailure,
+			Code:    http.StatusForbidden,
+			Reason:  metav1.StatusReasonForbidden,
+			Message: "forbidden",
+		}},
+		scheme.Codecs.WithoutConversion(), schema.GroupVersion{}, resp, req,
+	)
 }
