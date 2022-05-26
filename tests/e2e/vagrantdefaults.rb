b3f2065976cf691be15334fad0723bebf2d6e842
Derek Nola <derek.nola@suse.com>
2022-05-26 11:51:05 -0700

    remove global varibale from getInstallType
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -13,13 +13,13 @@ def defaultOSConfigure(vm)
   end 
 end
 
-def getInstallType(vm, release_version)
+def getInstallType(vm, release_version, branch)
   if !release_version.empty?
     return "INSTALL_K3S_VERSION=#{release_version}"
   else
     # Grabs the last 5 commit SHA's from the given branch, then purges any commits that do not have a passing CI build
     # MicroOS requires it not be in a /tmp/ or other root system folder
-    vm.provision "Get latest commit", type: "shell", path: "../scripts/latest_commit.sh", args: [GITHUB_BRANCH, "/tmp/k3s_commits"]
+    vm.provision "Get latest commit", type: "shell", path: "../scripts/latest_commit.sh", args: [branch, "/tmp/k3s_commits"]
     return "INSTALL_K3S_COMMIT=$(head\ -n\ 1\ /tmp/k3s_commits)"
   end
 end
