6a662a11282bcf1c662e91c9dffcdfcd451b7ab9
Darren Shepherd <darren@acorn.io>
2022-06-09 09:29:51 -0700

    Introduce servicelb-namespace parameter
    This parameter controls which namespace the klipper-lb pods will be create.
    It defaults to kube-system so that k3s does not by default create a new
    namespace. It can be changed if users wish to isolate the pods and apply
    some policy to them.
    
    Signed-off-by: Darren Shepherd <darren@acorn.io>
    (cherry picked from commit e6009b1edf41f886b9cad9bdd13482f79f1088da)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/server/types.go
+++ pkg/server/types.go
@@ -8,14 +8,15 @@ import (
 )
 
 type Config struct {
-	DisableAgent      bool
-	DisableServiceLB  bool
-	ControlConfig     config.Control
-	Rootless          bool
-	SupervisorPort    int
-	StartupHooks      []cmds.StartupHook
-	LeaderControllers CustomControllers
-	Controllers       CustomControllers
+	DisableAgent       bool
+	DisableServiceLB   bool
+	ControlConfig      config.Control
+	Rootless           bool
+	ServiceLBNamespace string
+	SupervisorPort     int
+	StartupHooks       []cmds.StartupHook
+	LeaderControllers  CustomControllers
+	Controllers        CustomControllers
 }
 
 type CustomControllers []func(ctx context.Context, sc *Context) error
