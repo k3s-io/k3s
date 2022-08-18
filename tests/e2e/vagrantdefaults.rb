66f9ebf5b51b5ca80ec14ef4d218b220c84ca1da
Derek Nola <derek.nola@suse.com>
2022-08-18 16:08:32 -0700

    Add e2e support for centos7 and rocky8
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -5,6 +5,12 @@ def defaultOSConfigure(vm)
     vm.provision "Install jq", type: "shell", inline: "apt install -y jq"
   elsif box.include?("Leap") || box.include?("Tumbleweed")
     vm.provision "Install jq", type: "shell", inline: "zypper install -y jq"
+  elsif box.include?("rocky8") || box.include?("rocky9")
+    vm.provision "Install jq", type: "shell", inline: "dnf install -y jq"
+    vm.provision "Disable firewall", type: "shell", inline: "systemctl stop firewalld"
+  elsif box.include?("centos7")
+    vm.provision "Install jq", type: "shell", inline: "yum install -y jq"
+    vm.provision "Disable firewall", type: "shell", inline: "systemctl stop firewalld"
   elsif box.include?("alpine")
     vm.provision "Install tools", type: "shell", inline: "apk add jq coreutils"
   elsif box.include?("microos")
