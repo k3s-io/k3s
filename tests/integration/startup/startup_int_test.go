3698fbb375d5e1625e0bdc442dc09c5c611c3d20
Derek Nola <derek.nola@suse.com>
2022-08-31 15:40:17 -0700

    Expand startup integration test (#6030)
    * add new data-dir subtest
    * Added node flag subtest
    * Fix to E2E tests
    * Convert existing test to new server logging
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    Signed-off-by: Nikolai Shields <nikolai@nikolaishields.com>

--- tests/integration/startup/startup_int_test.go
+++ tests/integration/startup/startup_int_test.go
@@ -1,11 +1,15 @@
 package integration
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	testutil "github.com/k3s-io/k3s/tests/integration"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	. "github.com/onsi/gomega/gstruct"
 	v1 "k8s.io/api/core/v1"
 )
 
@@ -22,7 +26,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).ToNot(HaveOccurred())
 })
 
-var _ = Describe("startup tests", func() {
+var _ = Describe("startup tests", Ordered, func() {
 
 	When("a default server is created", func() {
 		It("is created with no arguments", func() {
@@ -33,10 +37,11 @@ var _ = Describe("startup tests", func() {
 		It("has the default pods deployed", func() {
 			Eventually(func() error {
 				return testutil.K3sDefaultDeployments()
-			}, "90s", "5s").Should(Succeed())
+			}, "120s", "5s").Should(Succeed())
 		})
 		It("dies cleanly", func() {
 			Expect(testutil.K3sKillServer(startupServer)).To(Succeed())
+			Expect(testutil.K3sCleanup(-1, "")).To(Succeed())
 		})
 	})
 	When("a etcd backed server is created", func() {
@@ -49,10 +54,11 @@ var _ = Describe("startup tests", func() {
 		It("has the default pods deployed", func() {
 			Eventually(func() error {
 				return testutil.K3sDefaultDeployments()
-			}, "90s", "5s").Should(Succeed())
+			}, "120s", "5s").Should(Succeed())
 		})
 		It("dies cleanly", func() {
 			Expect(testutil.K3sKillServer(startupServer)).To(Succeed())
+			Expect(testutil.K3sCleanup(-1, "")).To(Succeed())
 		})
 	})
 	When("a server without traefik is created", func() {
@@ -66,6 +72,9 @@ var _ = Describe("startup tests", func() {
 			Eventually(func() error {
 				return testutil.CheckDeployments([]string{"coredns", "local-path-provisioner", "metrics-server"})
 			}, "90s", "10s").Should(Succeed())
+			nodes, err := testutil.ParseNodes()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodes).To(HaveLen(1))
 		})
 		It("dies cleanly", func() {
 			Expect(testutil.K3sKillServer(startupServer)).To(Succeed())
@@ -88,7 +97,7 @@ var _ = Describe("startup tests", func() {
 		It("has the node deployed with correct IPs", func() {
 			Eventually(func() error {
 				return testutil.K3sDefaultDeployments()
-			}, "90s", "10s").Should(Succeed())
+			}, "120s", "10s").Should(Succeed())
 
 			nodes, err := testutil.ParseNodes()
 			Expect(err).NotTo(HaveOccurred())
@@ -105,14 +114,86 @@ var _ = Describe("startup tests", func() {
 		})
 		It("dies cleanly", func() {
 			Expect(testutil.K3sKillServer(startupServer)).To(Succeed())
+			Expect(testutil.K3sCleanup(-1, "")).To(Succeed())
 			Expect(testutil.RunCommand("ip link del dummy2")).To(Equal(""))
 			Expect(testutil.RunCommand("ip link del dummy3")).To(Equal(""))
+		})
+	})
+	When("a server with different data-dir is created", func() {
+		var tempDir string
+		It("creates a temp directory", func() {
+			var err error
+			tempDir, err = os.MkdirTemp("", "k3s-data-dir")
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("is created with data-dir flag", func() {
+			var err error
+			startupServerArgs = []string{"--data-dir", tempDir}
+			startupServer, err = testutil.K3sStartServer(startupServerArgs...)
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("has the default pods deployed", func() {
+			Eventually(func() error {
+				return testutil.K3sDefaultDeployments()
+			}, "120s", "5s").Should(Succeed())
+			nodes, err := testutil.ParseNodes()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodes).To(HaveLen(1))
+		})
+		It("has the correct files in the temp data-dir", func() {
+			_, err := os.Stat(filepath.Join(tempDir, "server", "tls", "server-ca.key"))
+			Expect(err).ToNot(HaveOccurred())
+			_, err = os.Stat(filepath.Join(tempDir, "server", "token"))
+			Expect(err).ToNot(HaveOccurred())
+			_, err = os.Stat(filepath.Join(tempDir, "agent", "client-kubelet.crt"))
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("dies cleanly", func() {
+			Expect(testutil.K3sKillServer(startupServer)).To(Succeed())
+			Expect(testutil.K3sCleanup(-1, tempDir)).To(Succeed())
+		})
+	})
+	When("a server with different node options is created", func() {
+		It("is created with node-name with-node-id, node-label and node-taint flags", func() {
+			var err error
+			startupServerArgs = []string{"--node-name", "customnoder", "--with-node-id", "--node-label", "foo=bar", "--node-taint", "alice=bob:PreferNoSchedule"}
+			startupServer, err = testutil.K3sStartServer(startupServerArgs...)
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("has the default pods deployed", func() {
+			Eventually(func() error {
+				return testutil.K3sDefaultDeployments()
+			}, "120s", "5s").Should(Succeed())
+			nodes, err := testutil.ParseNodes()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodes).To(HaveLen(1))
+		})
+		var nodes []v1.Node
+		It("has a custom node name with id appended", func() {
+			var err error
+			nodes, err = testutil.ParseNodes()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodes).To(HaveLen(1))
+			Expect(nodes[0].Name).To(MatchRegexp(`-[0-9a-f]*`))
+			Expect(nodes[0].Name).To(ContainSubstring("customnoder"))
+		})
+		It("has proper node labels and taints", func() {
+			Expect(nodes[0].ObjectMeta.Labels).To(MatchKeys(IgnoreExtras, Keys{
+				"foo": Equal("bar"),
+			}))
+			Expect(nodes[0].Spec.Taints).To(ContainElement(v1.Taint{Key: "alice", Value: "bob", Effect: v1.TaintEffectPreferNoSchedule}))
+		})
+		It("dies cleanly", func() {
+			Expect(testutil.K3sKillServer(startupServer)).To(Succeed())
 		})
 	})
 })
 
 var _ = AfterSuite(func() {
 	if !testutil.IsExistingServer() {
+		if CurrentSpecReport().Failed() {
+			testutil.K3sDumpLog(startupServer)
+		}
 		Expect(testutil.K3sCleanup(testLock, "")).To(Succeed())
 	}
 })
