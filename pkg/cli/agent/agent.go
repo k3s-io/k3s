cd49101fc833886c88514c549a38d19be139e736
Derek Nola <derek.nola@suse.com>
2022-09-01 09:33:59 -0700

    Convert deprecated flags to fatal errors for v1.25 (#6069)
    * Replace warning with fatal errors.
    * Group system-default-registry under (agent/runtime)
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/cli/agent/agent.go
+++ pkg/cli/agent/agent.go
@@ -46,8 +46,7 @@ func Run(ctx *cli.Context) error {
 	}
 
 	if cmds.AgentConfig.Token == "" && cmds.AgentConfig.ClusterSecret != "" {
-		logrus.Warn("cluster-secret is deprecated, it will be removed in v1.25. Use --token instead.")
-		cmds.AgentConfig.Token = cmds.AgentConfig.ClusterSecret
+		logrus.Fatal("cluster-secret is deprecated. Use --token instead.")
 	}
 
 	if cmds.AgentConfig.Token == "" {
