67a8d7bb28eff831eebebe688eaeaf8823606fe6
Brad Davidson <brad.davidson@rancher.com>
2022-09-29 19:52:18 +0000

    Implement InstancesV2 instead of Instances
    ... and drop legacy ClusterID support.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -4,19 +4,13 @@ import (
 	"io"
 
 	"github.com/k3s-io/k3s/pkg/version"
-	"k8s.io/client-go/informers"
-	informercorev1 "k8s.io/client-go/informers/core/v1"
-	"k8s.io/client-go/tools/cache"
 	cloudprovider "k8s.io/cloud-provider"
 )
 
 type k3s struct {
-	nodeInformer          informercorev1.NodeInformer
-	nodeInformerHasSynced cache.InformerSynced
 }
 
 var _ cloudprovider.Interface = &k3s{}
-var _ cloudprovider.InformerUser = &k3s{}
 
 func init() {
 	cloudprovider.RegisterCloudProvider(version.Program, func(config io.Reader) (cloudprovider.Interface, error) {
@@ -27,17 +21,12 @@ func init() {
 func (k *k3s) Initialize(clientBuilder cloudprovider.ControllerClientBuilder, stop <-chan struct{}) {
 }
 
-func (k *k3s) SetInformers(informerFactory informers.SharedInformerFactory) {
-	k.nodeInformer = informerFactory.Core().V1().Nodes()
-	k.nodeInformerHasSynced = k.nodeInformer.Informer().HasSynced
-}
-
 func (k *k3s) Instances() (cloudprovider.Instances, bool) {
-	return k, true
+	return nil, false
 }
 
 func (k *k3s) InstancesV2() (cloudprovider.InstancesV2, bool) {
-	return nil, false
+	return k, true
 }
 
 func (k *k3s) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
@@ -61,5 +50,5 @@ func (k *k3s) ProviderName() string {
 }
 
 func (k *k3s) HasClusterID() bool {
-	return true
+	return false
 }
