641998474953da2e34cb8ca5c178e2ef28e0229d
Derek Nola <derek.nola@suse.com>
2022-11-03 13:37:01 -0700

    Add more tags for the rest of the critical args
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/daemons/config/types.go
+++ pkg/daemons/config/types.go
@@ -128,23 +128,23 @@ type Agent struct {
 // CriticalControlArgs contains parameters that all control plane nodes in HA must share
 // The cli tag is used to provide better error information to the user on mismatch
 type CriticalControlArgs struct {
-	ClusterDNSs           []net.IP
-	ClusterIPRanges       []*net.IPNet
-	ClusterDNS            net.IP
-	ClusterDomain         string
-	ClusterIPRange        *net.IPNet
-	DisableCCM            bool `cli:"disable-cloud-controller"`
-	DisableHelmController bool `cli:"disable-helm-controller"`
-	DisableNPC            bool `cli:"disable-network-policy"`
-	DisableServiceLB      bool `cli:"disable-service-lb"`
-	EncryptSecrets        bool `cli:"secrets-encryption"`
-	FlannelBackend        string
-	FlannelIPv6Masq       bool
-	FlannelExternalIP     bool
-	EgressSelectorMode    string
+	ClusterDNSs           []net.IP     `cli:"cluster-dns"`
+	ClusterIPRanges       []*net.IPNet `cli:"cluster-cidr"`
+	ClusterDNS            net.IP       `cli:"cluster-dns"`
+	ClusterDomain         string       `cli:"cluster-domain"`
+	ClusterIPRange        *net.IPNet   `cli:"cluster-cidr"`
+	DisableCCM            bool         `cli:"disable-cloud-controller"`
+	DisableHelmController bool         `cli:"disable-helm-controller"`
+	DisableNPC            bool         `cli:"disable-network-policy"`
+	DisableServiceLB      bool         `cli:"disable-service-lb"`
+	EncryptSecrets        bool         `cli:"secrets-encryption"`
+	FlannelBackend        string       `cli:"flannel-backend"`
+	FlannelIPv6Masq       bool         `cli:"flannel-ipv6-masq"`
+	FlannelExternalIP     bool         `cli:"flannel-external-ip"`
+	EgressSelectorMode    string       `cli:"egress-selector-mode"`
 	NoCoreDNS             bool
-	ServiceIPRange        *net.IPNet
-	ServiceIPRanges       []*net.IPNet
+	ServiceIPRange        *net.IPNet   `cli:"service-cidr"`
+	ServiceIPRanges       []*net.IPNet `cli:"service-cidr"`
 }
 
 type Control struct {
