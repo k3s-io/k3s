4e2e91e0897a3bb36b003769cea392a54bf6a81f
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2022-11-28 09:27:43 -0800

    Switch from Google Buckets to AWS S3 Buckets (#6497)
    * Add python pip pakacge to install aws cli
    
    Signed-off-by: Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
    
    * Upload build artifacts to aws s3 instead of gcp bucket
    
    Signed-off-by: Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
    
    * Upload logs to aws s3 instead of google buckets
    
    Signed-off-by: Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
    
    * Replace gcloud auth with aws credentials for artifact uploading to buckets
    
    Signed-off-by: Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
    
    * Replace usage of google bucket with aws s3 buckets
    
    Signed-off-by: Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
    
    Signed-off-by: Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>

--- package/rpm/install.sh
+++ package/rpm/install.sh
@@ -94,7 +94,7 @@ if [ "$INSTALL_K3S_DEBUG" = "true" ]; then
 fi
 
 GITHUB_URL=https://github.com/k3s-io/k3s/releases
-STORAGE_URL=https://storage.googleapis.com/k3s-ci-builds
+STORAGE_URL=https://k3s-ci-builds.s3.amazonaws.com
 DOWNLOADER=
 
 # --- helper functions for logs ---
