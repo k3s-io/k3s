d9eee72dc6c81a89022636739580d620a2857291
ShylajaDevadiga <shylaja.devadiga@suse.com>
2023-01-12 17:41:09 -0800

    generate report and upload test results
    Signed-off-by: ShylajaDevadiga <shylaja.devadiga@suse.com>

--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -41,15 +41,17 @@ def dockerInstall(vm)
   vm.provider "virtualbox" do |v|
     v.memory = NODE_MEMORY + 1024
   end
-  if vm.box.to_s.include?("ubuntu")
+  box = vm.box.to_s
+  if box.include?("ubuntu")
     vm.provision "shell", inline: "apt update; apt install -y docker.io"
-  end
-  if vm.box.to_s.include?("Leap")
+  elsif box.include?("Leap")
     vm.provision "shell", inline: "zypper install -y docker apparmor-parser"
-  end
-  if vm.box.to_s.include?("microos")
+  elsif box.include?("microos")
     vm.provision "shell", inline: "transactional-update pkg install -y docker apparmor-parser"
     vm.provision 'docker-reload', type: 'reload', run: 'once'
     vm.provision "shell", inline: "systemctl enable --now docker"
+  elsif box.include?("rocky8") || box.include?("rocky9")
+    vm.provision "shell", inline: "dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+    vm.provision "shell", inline: "dnf install -y docker-ce"
   end
 end
