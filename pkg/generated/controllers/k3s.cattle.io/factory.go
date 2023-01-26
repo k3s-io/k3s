bb3208b257b62c808754f6f2c3e67bc90fa8a797
Brad Davidson <brad.davidson@rancher.com>
2023-01-26 23:48:30 +0000

    go generate
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/generated/controllers/k3s.cattle.io/factory.go
+++ pkg/generated/controllers/k3s.cattle.io/factory.go
@@ -19,6 +19,7 @@ limitations under the License.
 package k3s
 
 import (
+	"github.com/rancher/lasso/pkg/controller"
 	"github.com/rancher/wrangler/pkg/generic"
 	"k8s.io/client-go/rest"
 )
@@ -64,4 +65,8 @@ func NewFactoryFromConfigWithOptionsOrDie(config *rest.Config, opts *FactoryOpti
 
 func (c *Factory) K3s() Interface {
 	return New(c.ControllerFactory())
+}
+
+func (c *Factory) WithAgent(userAgent string) Interface {
+	return New(controller.NewSharedControllerFactoryWithAgent(userAgent, c.ControllerFactory()))
 }
