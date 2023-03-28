e89f83c9caf0a1a4248f5721361c702b466be1aa
Guilherme Macedo <guilherme.macedo@suse.com>
2023-03-27 22:34:25 -0300

    Improve Trivy configuration
    Signed-off-by: Guilherme Macedo <guilherme.macedo@suse.com>

--- scripts/image_scan.sh
+++ scripts/image_scan.sh
@@ -18,11 +18,20 @@ if [ -n ${DEBUG} ]; then
     set -x
 fi
 
-
-
 IMAGE=$1
 SEVERITIES="HIGH,CRITICAL"
+TRIVY_TEMPLATE='{{- $critical := 0 }}{{- $high := 0 }}
+{{- println "Target - Severity - ID - Package - Vulnerable Version - Fixed Version" -}}{{ print }}
+{{ range . }}
+    {{- $target := .Target -}}
+    {{ range .Vulnerabilities }}
+        {{- if  eq .Severity "CRITICAL" }}{{- $critical = add $critical 1 }}{{- end }}
+        {{- if  eq .Severity "HIGH" }}{{- $high = add $high 1 }}{{- end }}
+        {{- list $target .Severity .VulnerabilityID .PkgName .InstalledVersion .FixedVersion | join " - " | println -}}
+    {{- end -}}
+{{ end }}
+Vulnerabilities - Critical: {{ $critical }}, High: {{ $high }}{{ println }}'
 
-trivy --quiet image --severity ${SEVERITIES}  --no-progress --ignore-unfixed ${IMAGE}
+trivy --quiet image --severity ${SEVERITIES} --no-progress --ignore-unfixed --format template --template "${TRIVY_TEMPLATE}" ${IMAGE}
 
 exit 0
