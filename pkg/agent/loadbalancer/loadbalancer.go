61b02decd08527eca59d9b00bee13f06dcbb95e8
Brad Davidson <brad.davidson@rancher.com>
2023-03-31 20:51:27 +0000

    Fix issue with stale connections to removed LB server
    Track LB connections through each server so that they can be closed when it is removed.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit e54ceaa497ff8f6bc82ec6304e02bb37c60730a0)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/agent/loadbalancer/loadbalancer.go
+++ pkg/agent/loadbalancer/loadbalancer.go
@@ -14,10 +14,25 @@ import (
 	"inet.af/tcpproxy"
 )
 
+// server tracks the connections to a server, so that they can be closed when the server is removed.
+type server struct {
+	mutex       sync.Mutex
+	connections map[net.Conn]struct{}
+}
+
+// serverConn wraps a net.Conn so that it can be removed from the server's connection map when closed.
+type serverConn struct {
+	server *server
+	net.Conn
+}
+
+// LoadBalancer holds data for a local listener which forwards connections to a
+// pool of remote servers. It is not a proper load-balancer in that it does not
+// actually balance connections, but instead fails over to a new server only
+// when a connection attempt to the currently selected server fails.
 type LoadBalancer struct {
-	mutex  sync.Mutex
-	dialer *net.Dialer
-	proxy  *tcpproxy.Proxy
+	mutex sync.Mutex
+	proxy *tcpproxy.Proxy
 
 	serviceName          string
 	configFile           string
@@ -27,6 +42,7 @@ type LoadBalancer struct {
 	ServerURL            string
 	ServerAddresses      []string
 	randomServers        []string
+	servers              map[string]*server
 	currentServerAddress string
 	nextServerIndex      int
 	Listener             net.Listener
@@ -40,6 +56,8 @@ var (
 	ETCDServerServiceName = version.Program + "-etcd-server-load-balancer"
 )
 
+// New contstructs a new LoadBalancer instance. The default server URL, and
+// currently active servers, are stored in a file within the dataDir.
 func New(ctx context.Context, dataDir, serviceName, serverURL string, lbServerPort int, isIPv6 bool) (_lb *LoadBalancer, _err error) {
 	config := net.ListenConfig{Control: reusePort}
 	var localAddress string
@@ -76,11 +94,11 @@ func New(ctx context.Context, dataDir, serviceName, serverURL string, lbServerPo
 
 	lb := &LoadBalancer{
 		serviceName:          serviceName,
-		dialer:               &net.Dialer{},
 		configFile:           filepath.Join(dataDir, "etc", serviceName+".json"),
 		localAddress:         localAddress,
 		localServerURL:       localServerURL,
 		defaultServerAddress: defaultServerAddress,
+		servers:              make(map[string]*server),
 		ServerURL:            serverURL,
 	}
 
@@ -103,14 +121,28 @@ func New(ctx context.Context, dataDir, serviceName, serverURL string, lbServerPo
 	if err := lb.proxy.Start(); err != nil {
 		return nil, err
 	}
-	logrus.Infof("Running load balancer %s %s -> %v", serviceName, lb.localAddress, lb.randomServers)
+	logrus.Infof("Running load balancer %s %s -> %v [default: %s]", serviceName, lb.localAddress, lb.ServerAddresses, lb.defaultServerAddress)
 
 	return lb, nil
 }
 
 func (lb *LoadBalancer) SetDefault(serverAddress string) {
-	logrus.Infof("Updating load balancer %s default server address -> %s", lb.serviceName, serverAddress)
+	lb.mutex.Lock()
+	defer lb.mutex.Unlock()
+
+	_, hasOriginalServer := sortServers(lb.ServerAddresses, lb.defaultServerAddress)
+	// if the old default server is not currently in use, remove it from the server map
+	if server := lb.servers[lb.defaultServerAddress]; server != nil && !hasOriginalServer {
+		defer server.closeAll()
+		delete(lb.servers, lb.defaultServerAddress)
+	}
+	// if the new default server doesn't have an entry in the map, add one
+	if _, ok := lb.servers[serverAddress]; !ok {
+		lb.servers[serverAddress] = &server{connections: make(map[net.Conn]struct{})}
+	}
+
 	lb.defaultServerAddress = serverAddress
+	logrus.Infof("Updated load balancer %s default server address -> %s", lb.serviceName, serverAddress)
 }
 
 func (lb *LoadBalancer) Update(serverAddresses []string) {
@@ -120,7 +152,7 @@ func (lb *LoadBalancer) Update(serverAddresses []string) {
 	if !lb.setServers(serverAddresses) {
 		return
 	}
-	logrus.Infof("Updating load balancer %s server addresses -> %v", lb.serviceName, lb.randomServers)
+	logrus.Infof("Updated load balancer %s server addresses -> %v [default: %s]", lb.serviceName, lb.ServerAddresses, lb.defaultServerAddress)
 
 	if err := lb.writeConfig(); err != nil {
 		logrus.Warnf("Error updating load balancer %s config: %s", lb.serviceName, err)
@@ -139,18 +171,23 @@ func (lb *LoadBalancer) dialContext(ctx context.Context, network, address string
 	for {
 		targetServer := lb.currentServerAddress
 
-		conn, err := lb.dialer.DialContext(ctx, network, targetServer)
-		if err == nil {
-			return conn, nil
+		server := lb.servers[targetServer]
+		if server == nil || targetServer == "" {
+			logrus.Debugf("Nil server for load balancer %s: %s", lb.serviceName, targetServer)
+		} else {
+			conn, err := server.dialContext(ctx, network, targetServer)
+			if err == nil {
+				return conn, nil
+			}
+			logrus.Debugf("Dial error from load balancer %s: %s", lb.serviceName, err)
 		}
-		logrus.Debugf("Dial error from load balancer %s: %s", lb.serviceName, err)
 
 		newServer, err := lb.nextServer(targetServer)
 		if err != nil {
 			return nil, err
 		}
 		if targetServer != newServer {
-			logrus.Debugf("Dial server in load balancer %s failed over to %s", lb.serviceName, newServer)
+			logrus.Debugf("Failed over to new server for load balancer %s: %s", lb.serviceName, newServer)
 		}
 		if ctx.Err() != nil {
 			return nil, ctx.Err()
@@ -167,7 +204,7 @@ func (lb *LoadBalancer) dialContext(ctx context.Context, network, address string
 }
 
 func onDialError(src net.Conn, dstDialErr error) {
-	logrus.Debugf("Incoming conn %v, error dialing load balancer servers: %v", src.RemoteAddr().String(), dstDialErr)
+	logrus.Debugf("Incoming conn %s, error dialing load balancer servers: %v", src.RemoteAddr(), dstDialErr)
 	src.Close()
 }
 
