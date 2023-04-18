14add82e84e0fbb4ad726527a2c4909212faa733
Brad Davidson <brad.davidson@rancher.com>
2023-04-18 20:52:24 +0000

    Create CRDs with schema
    Fixes an issue where CRDs were being created without schema, allowing
    resources with invalid content to be created, later stalling the
    controller ListWatch event channel when the invalid resources could not
    be deserialized.
    
    This also requires moving Addon GVK tracking from a status field to
    an annotation, as the GroupVersionKind type has special handling
    internal to Kubernetes that prevents it from being serialized to the CRD
    when schema validation is enabled.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/generated/clientset/versioned/typed/k3s.cattle.io/v1/fake/fake_addon.go
+++ pkg/generated/clientset/versioned/typed/k3s.cattle.io/v1/fake/fake_addon.go
@@ -101,18 +101,6 @@ func (c *FakeAddons) Update(ctx context.Context, addon *v1.Addon, opts metav1.Up
 	return obj.(*v1.Addon), err
 }
 
-// UpdateStatus was generated because the type contains a Status member.
-// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-func (c *FakeAddons) UpdateStatus(ctx context.Context, addon *v1.Addon, opts metav1.UpdateOptions) (*v1.Addon, error) {
-	obj, err := c.Fake.
-		Invokes(testing.NewUpdateSubresourceAction(addonsResource, "status", c.ns, addon), &v1.Addon{})
-
-	if obj == nil {
-		return nil, err
-	}
-	return obj.(*v1.Addon), err
-}
-
 // Delete takes name of the addon and deletes it. Returns an error if one occurs.
 func (c *FakeAddons) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
 	_, err := c.Fake.
