36699bbd60291d89ce9edcd213ed5a5cc34c5786
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2023-04-19 15:33:12 +0200

    Changed command -v redirection for iptables bin check
    Signed-off-by: Roberto Bonafiglia <roberto.bonafiglia@suse.com>

--- install.sh
+++ install.sh
@@ -967,11 +967,11 @@ service_enable_and_start() {
         return
     fi
 
-    if command -v iptables-save &> /dev/null && command -v iptables-restore &> /dev/null
+    if command -v iptables-save 1> /dev/null && command -v iptables-restore 1> /dev/null
     then
 	    $SUDO iptables-save | grep -v KUBE- | grep -v CNI- | grep -iv flannel | $SUDO iptables-restore
     fi
-    if command -v ip6tables-save &> /dev/null && command -v ip6tables-restore &> /dev/null
+    if command -v ip6tables-save 1> /dev/null && command -v ip6tables-restore 1> /dev/null
     then
 	    $SUDO ip6tables-save | grep -v KUBE- | grep -v CNI- | grep -iv flannel | $SUDO ip6tables-restore
     fi
