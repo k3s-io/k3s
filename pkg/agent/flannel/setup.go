42c3bf89b32565181afd5cd3588c333de0130bd1
Brad Davidson <brad.davidson@rancher.com>
2023-05-04 17:46:40 +0000

    Bump cni plugins to v1.2.0-k3s1
    Also add bandwidth and firewall plugins. The bandwidth plugin is
    automatically registered with the appropriate capability, but the
    firewall plugin must be configured by the user if they want to use it.
    
    Ref: https://www.cni.dev/plugins/current/meta/firewall/
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/agent/flannel/setup.go
+++ pkg/agent/flannel/setup.go
@@ -40,6 +40,12 @@ const (
       "capabilities":{
         "portMappings":true
       }
+    },
+    {
+      "type":"bandwidth",
+      "capabilities":{
+        "bandwidth":true
+      }
     }
   ]
 }
