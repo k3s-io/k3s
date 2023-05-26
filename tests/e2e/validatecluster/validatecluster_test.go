5be4da124d67ada4f83d3721e343df0278e50d9c
Brad Davidson <brad.davidson@rancher.com>
2023-05-26 23:37:13 +0000

    Fix test file list
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 7b61aacb56eb72c045d1bbbeed0a99ac36eed115)

--- tests/e2e/validatecluster/validatecluster_test.go
+++ tests/e2e/validatecluster/validatecluster_test.go
@@ -342,17 +342,17 @@ var _ = Describe("Verify Create", Ordered, func() {
 		})
 		It("Validates certificates", func() {
 			const grepCert = "sudo ls -lt /var/lib/rancher/k3s/server/ | grep tls"
-			var expectResult = []string{"client-ca.crt",
-				"client-ca.key",
-				"client-ca.nochain.crt",
+			var expectResult = []string{
+				"client-ca.crt", "client-ca.key", "client-ca.nochain.crt",
+				"client-supervisor.crt", "client-supervisor.key",
 				"dynamic-cert.json", "peer-ca.crt",
 				"peer-ca.key", "server-ca.crt",
 				"server-ca.key", "request-header-ca.crt",
 				"request-header-ca.key", "server-ca.crt",
 				"server-ca.key", "server-ca.nochain.crt",
 				"service.current.key", "service.key",
-				"apiserver-loopback-client__.crt",
-				"apiserver-loopback-client__.key", "",
+				"apiserver-loopback-client__.crt", "apiserver-loopback-client__.key",
+				"",
 			}
 
 			var finalResult string
