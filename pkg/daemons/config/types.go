7769762fbcb0e10d4eb6ac9844cd7d8f73879353
Brad Davidson <brad.davidson@rancher.com>
2023-05-26 20:35:17 +0000

    Create new kubeconfig for supervisor use
    Only actual admin actions should use the admin kubeconfig; everything done by the supervisor/deploy/helm controllers will now use a distinct account for audit purposes.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 64a5f58f1ec559c9392839cc953f654551c2e0da)

--- pkg/daemons/config/types.go
+++ pkg/daemons/config/types.go
@@ -295,6 +295,7 @@ type ControlRuntime struct {
 	ServiceCurrentKey string
 
 	KubeConfigAdmin           string
+	KubeConfigSupervisor      string
 	KubeConfigController      string
 	KubeConfigScheduler       string
 	KubeConfigAPIServer       string
@@ -318,6 +319,8 @@ type ControlRuntime struct {
 
 	ClientAdminCert           string
 	ClientAdminKey            string
+	ClientSupervisorCert      string
+	ClientSupervisorKey       string
 	ClientControllerCert      string
 	ClientControllerKey       string
 	ClientSchedulerCert       string
