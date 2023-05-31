b33011281d22559a300164fbef432919a949fdbb
Brad Davidson <brad.davidson@rancher.com>
2023-05-31 18:54:03 +0000

    Soft-fail on node password verification if the secret cannot be created
    Allows nodes to join the cluster during a webhook outage. This also
    enhances auditability by creating Kubernetes events for the deferred
    verification.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 45d8c1a1a29aed729303e4f98a7e0f90466665fd)

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -121,6 +121,7 @@ func runControllers(ctx context.Context, config *Config) error {
 		controlConfig.Runtime.NodePasswdFile); err != nil {
 		logrus.Warn(errors.Wrap(err, "error migrating node-password file"))
 	}
+	controlConfig.Runtime.Event = sc.Event
 	controlConfig.Runtime.Core = sc.Core
 
 	for name, cb := range controlConfig.Runtime.ClusterControllerStarts {
