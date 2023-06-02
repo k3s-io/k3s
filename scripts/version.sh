51ab039a8b4a7c4300969be017c9650d0e15b5e6
Johnatas <you@example.com>
2023-06-02 02:54:43 -0300

    Pin Containerd version

--- scripts/version.sh
+++ scripts/version.sh
@@ -22,10 +22,9 @@ if [ -d .git ]; then
     fi
 fi
 
-VERSION_CONTAINERD=$(grep github.com/containerd/containerd go.mod | head -n1 | awk '{print $4}')
-if [ -z "$VERSION_CONTAINERD" ]; then
-    VERSION_CONTAINERD="v0.0.0"
-fi
+# We're building k3s against containerd 1.5 in go.mod because 1.6 has dependency
+# conflicts with Kubernetes, but we still need to bundle containerd 1.6.
+VERSION_CONTAINERD="v1.7.1-k3s1"
 
 VERSION_CRICTL=$(grep github.com/kubernetes-sigs/cri-tools go.mod | head -n1 | awk '{print $4}')
 if [ -z "$VERSION_CRICTL" ]; then
