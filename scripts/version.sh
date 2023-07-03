cdad99d86fe4e0952a53defda0e8c9a3cbc380d3
Johnatas <johnatas.santos@suse.com>
2023-07-03 19:21:42 -0300

    put back version.sh
    Signed-off-by: Johnatas <johnatas.santos@suse.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -22,9 +22,10 @@ if [ -d .git ]; then
     fi
 fi
 
-# We're building k3s against containerd 1.5 in go.mod because 1.6 has dependency
-# conflicts with Kubernetes, but we still need to bundle containerd 1.7
-VERSION_CONTAINERD="v1.7.1-k3s1"
+VERSION_CONTAINERD=$(grep github.com/containerd/containerd go.mod | head -n1 | awk '{print $4}')
+if [ -z "$VERSION_CONTAINERD" ]; then
+    VERSION_CONTAINERD="v0.0.0"
+fi
 
 VERSION_CRICTL=$(grep github.com/kubernetes-sigs/cri-tools go.mod | head -n1 | awk '{print $4}')
 if [ -z "$VERSION_CRICTL" ]; then
