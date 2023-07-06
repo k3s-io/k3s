4ab01f3941b2a344de79f3d0c29d152e9b70949f
Derek Nola <derek.nola@suse.com>
2023-07-06 12:34:51 -0700

    Warn that v1.28 will deprecate reencrypt/prepare (#7848)
    * Warn that v1.28 will deprecate reencrypt/prepare
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/cli/secretsencrypt/secrets_encrypt.go
+++ pkg/cli/secretsencrypt/secrets_encrypt.go
@@ -16,6 +16,7 @@ import (
 	"github.com/k3s-io/k3s/pkg/server"
 	"github.com/k3s-io/k3s/pkg/version"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"k8s.io/utils/pointer"
 )
@@ -151,6 +152,7 @@ func Prepare(app *cli.Context) error {
 	if err = cmds.InitLogging(); err != nil {
 		return err
 	}
+	logrus.Warnln("This command will be deprecated in v1.28, will be combined with rotate")
 	info, err := commandPrep(app, &cmds.ServerConfig)
 	if err != nil {
 		return err
@@ -196,6 +198,7 @@ func Reencrypt(app *cli.Context) error {
 	if err = cmds.InitLogging(); err != nil {
 		return err
 	}
+	logrus.Warnln("This command will be deprecated in v1.28, will be combined with rotate")
 	info, err := commandPrep(app, &cmds.ServerConfig)
 	if err != nil {
 		return err
