bd98112cbd62e9e2c91654f16909e7c8b4d318b8
Vitor Savian <vitor.savian@suse.com>
2023-08-10 12:23:10 -0300

    Fix for cluster-reset backup from s3 when etcd snapshots are disabled (#8155)
    * Fixed when the user disable the etcd snapshots, but want to backup from s3
    
    Signed-off-by: Vitor <vitor.savian@suse.com>

--- pkg/cli/server/server.go
+++ pkg/cli/server/server.go
@@ -168,7 +168,7 @@ func run(app *cli.Context, cfg *cmds.Server, leaderControllers server.CustomCont
 	serverConfig.ControlConfig.EtcdExposeMetrics = cfg.EtcdExposeMetrics
 	serverConfig.ControlConfig.EtcdDisableSnapshots = cfg.EtcdDisableSnapshots
 
-	if !cfg.EtcdDisableSnapshots {
+	if !cfg.EtcdDisableSnapshots || cfg.ClusterReset {
 		serverConfig.ControlConfig.EtcdSnapshotCompress = cfg.EtcdSnapshotCompress
 		serverConfig.ControlConfig.EtcdSnapshotName = cfg.EtcdSnapshotName
 		serverConfig.ControlConfig.EtcdSnapshotCron = cfg.EtcdSnapshotCron
