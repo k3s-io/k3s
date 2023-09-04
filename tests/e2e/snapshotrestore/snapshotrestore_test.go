99ae70a0adf2e991c72daf79eff9fc14591c563f
Vitor Savian <vitor.savian@suse.com>
2023-09-04 12:10:47 -0300

    Format
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>

--- tests/e2e/snapshotrestore/snapshotrestore_test.go
+++ tests/e2e/snapshotrestore/snapshotrestore_test.go
@@ -124,7 +124,7 @@ var _ = Describe("Verify snapshots and cluster restores work", Ordered, func() {
 		})
 
 	})
-	
+
 	Context("Cluster is reset normally", func() {
 		It("Resets the cluster", func() {
 			for _, nodeName := range serverNodeNames {
