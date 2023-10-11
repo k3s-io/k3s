b941ce0a7b85fe996085b43a6a6329a132fc1b44
Derek Nola <derek.nola@suse.com>
2023-10-11 11:26:36 -0700

    Convert snapshotter test in e2e test
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/testutils.go
+++ tests/e2e/testutils.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -289,6 +290,8 @@ func GenKubeConfigFile(serverName string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	re := regexp.MustCompile(`(?m)==> vagrant:.*\n`)
+	kubeConfig = re.ReplaceAllString(kubeConfig, "")
 	nodeIP, err := FetchNodeExternalIP(serverName)
 	if err != nil {
 		return "", err
