3237b0e0a2da9207f4e342f34f3cb99ec490bded
Brad Davidson <brad.davidson@rancher.com>
2023-10-12 20:18:48 +0000

    Bump busybox to v1.36.1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 9bb1ce12531ec976bdc0c62a5813d55f99c92417)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- tests/e2e/startup/startup_test.go
+++ tests/e2e/startup/startup_test.go
@@ -236,7 +236,7 @@ var _ = Describe("Various Startup Configurations", Ordered, func() {
 		})
 
 		It("Runs an interactive command a pod", func() {
-			cmd := "kubectl run busybox --rm -it --restart=Never --image=rancher/mirrored-library-busybox:1.34.1 -- uname -a"
+			cmd := "kubectl run busybox --rm -it --restart=Never --image=rancher/mirrored-library-busybox:1.36.1 -- uname -a"
 			_, err := e2e.RunCmdOnNode(cmd, serverNodeNames[0])
 			Expect(err).NotTo(HaveOccurred())
 		})
