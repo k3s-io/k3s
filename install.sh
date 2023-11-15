5fd59888a811249557f3eae96dc8b3ada17a0893
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2023-11-15 23:34:11 +0200

    Fix wrong warning from restorecon in install script
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- install.sh
+++ install.sh
@@ -967,8 +967,8 @@ create_service_file() {
 }
 
 restore_systemd_service_file_context() {
-    $SUDO restorecon -R -i ${FILE_K3S_SERVICE} 2>&1 || true
-    $SUDO restorecon -R -i ${FILE_K3S_ENV} 2>&1 || true
+    $SUDO restorecon -R -i ${FILE_K3S_SERVICE} 2>/dev/null || true
+    $SUDO restorecon -R -i ${FILE_K3S_ENV} 2>/dev/null || true
 }
 
 # --- get hashes of the current k3s bin and service files
