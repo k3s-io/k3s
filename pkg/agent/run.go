31bc0ea3cedbf1c4a1839e84ce6cfd57501184fd
Brad Davidson <brad.davidson@rancher.com>
2023-11-21 00:07:20 +0000

    Handle logging flags when parsing kube-proxy args
    Also adds a test to ensure this continues to work.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit db7091b3f699eb90e0169fd88265adbf2ca94102)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/agent/run.go
+++ pkg/agent/run.go
@@ -43,6 +43,7 @@ import (
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/tools/cache"
 	toolswatch "k8s.io/client-go/tools/watch"
+	"k8s.io/component-base/cli/globalflag"
 	app2 "k8s.io/kubernetes/cmd/kube-proxy/app"
 	kubeproxyconfig "k8s.io/kubernetes/pkg/proxy/apis/config"
 	utilsnet "k8s.io/utils/net"
@@ -195,6 +196,7 @@ func getConntrackConfig(nodeConfig *daemonconfig.Node) (*kubeproxyconfig.KubePro
 	}
 
 	cmd := app2.NewProxyCommand()
+	globalflag.AddGlobalFlags(cmd.Flags(), cmd.Name())
 	if err := cmd.ParseFlags(daemonconfig.GetArgs(map[string]string{}, nodeConfig.AgentConfig.ExtraKubeProxyArgs)); err != nil {
 		return nil, err
 	}
