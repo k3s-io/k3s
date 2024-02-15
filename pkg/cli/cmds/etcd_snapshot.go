0f21471d9586cff663283afe7a1365c98666b4c3
Brad Davidson <brad.davidson@rancher.com>
2024-02-15 22:36:52 +0000

    Move snapshot-retention to EtcdSnapshotFlags in order to support loading from config
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 0b3593205a9654a792dc5554445576585e1aa6db)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cli/cmds/etcd_snapshot.go
+++ pkg/cli/cmds/etcd_snapshot.go
@@ -37,6 +37,12 @@ var EtcdSnapshotFlags = []cli.Flag{
 		Usage:       "(db) Compress etcd snapshot",
 		Destination: &ServerConfig.EtcdSnapshotCompress,
 	},
+	&cli.IntFlag{
+		Name:        "snapshot-retention,etcd-snapshot-retention",
+		Usage:       "(db) Number of snapshots to retain.",
+		Destination: &ServerConfig.EtcdSnapshotRetention,
+		Value:       defaultSnapshotRentention,
+	},
 	&cli.BoolFlag{
 		Name:        "s3,etcd-s3",
 		Usage:       "(db) Enable backup to S3",
@@ -134,12 +140,7 @@ func NewEtcdSnapshotCommands(run, delete, list, prune, save func(ctx *cli.Contex
 				SkipFlagParsing: false,
 				SkipArgReorder:  true,
 				Action:          prune,
-				Flags: append(EtcdSnapshotFlags, &cli.IntFlag{
-					Name:        "snapshot-retention",
-					Usage:       "(db) Number of snapshots to retain.",
-					Destination: &ServerConfig.EtcdSnapshotRetention,
-					Value:       defaultSnapshotRentention,
-				}),
+				Flags:           EtcdSnapshotFlags,
 			},
 			{
 				Name:            "save",
