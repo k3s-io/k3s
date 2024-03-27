486f21058d4a8f16a4f80156fd2ed80032d09195
Derek Nola <derek.nola@suse.com>
2024-03-27 09:55:13 -0700

    Move to ubuntu 23.10 for E2E tests (#9755)
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/embeddedmirror/embeddedmirror_test.go
+++ tests/e2e/embeddedmirror/embeddedmirror_test.go
@@ -13,9 +13,9 @@ import (
 )
 
 // Valid nodeOS:
-// generic/ubuntu2204, generic/centos7, generic/rocky8,
+// generic/ubuntu2310, generic/centos7, generic/rocky8,
 // opensuse/Leap-15.3.x86_64
-var nodeOS = flag.String("nodeOS", "generic/ubuntu2204", "VM operating system")
+var nodeOS = flag.String("nodeOS", "generic/ubuntu2310", "VM operating system")
 var serverCount = flag.Int("serverCount", 1, "number of server nodes")
 var agentCount = flag.Int("agentCount", 1, "number of agent nodes")
 var ci = flag.Bool("ci", false, "running on CI")
