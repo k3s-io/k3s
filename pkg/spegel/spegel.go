f62cc4ae6fd26ae258bd3584ba0c50be1eef1877
Derek Nola <derek.nola@suse.com>
2024-04-03 13:34:54 -0700

    Bump spegel to v0.0.20-k3s1
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/spegel/spegel.go
+++ pkg/spegel/spegel.go
@@ -32,11 +32,11 @@ import (
 	"github.com/libp2p/go-libp2p/p2p/host/peerstore/pstoreds"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"github.com/xenitab/spegel/pkg/metrics"
-	"github.com/xenitab/spegel/pkg/oci"
-	"github.com/xenitab/spegel/pkg/registry"
-	"github.com/xenitab/spegel/pkg/routing"
-	"github.com/xenitab/spegel/pkg/state"
+	"github.com/spegel-org/spegel/pkg/metrics"
+	"github.com/spegel-org/spegel/pkg/oci"
+	"github.com/spegel-org/spegel/pkg/registry"
+	"github.com/spegel-org/spegel/pkg/routing"
+	"github.com/spegel-org/spegel/pkg/state"
 	"k8s.io/component-base/metrics/legacyregistry"
 )
 
