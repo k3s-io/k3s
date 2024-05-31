1e34e0773e40b1683ad06648188fb73bd278eed9
Katherine Door <me@kitty.sh>
2024-05-31 08:45:34 +0200

    Add write-kubeconfig-group flag to server (#9233)
    * Add write-kubeconfig-group flag to server
    * update kubectl unable to read config message for kubeconfig mode/group
    
    Signed-off-by: Katherine Pata <me@kitty.sh>
    (cherry picked from commit 7a0ea3c9539c9ac8efb560de32395c9611db7969)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"os"
+	"os/user"
+	"strconv"
 	"strings"
 	"time"
 
@@ -11,6 +13,27 @@ import (
 
 func SetFileModeForPath(name string, mode os.FileMode) error {
 	return os.Chmod(name, mode)
+}
+
+func SetFileGroupForPath(name string, group string) error {
+	// Try to use as group id
+	gid, err := strconv.Atoi(group)
+	if err == nil {
+		return os.Chown(name, -1, gid)
+	}
+
+	// Otherwise, it must be a group name
+	g, err := user.LookupGroup(group)
+	if err != nil {
+		return err
+	}
+
+	gid, err = strconv.Atoi(g.Gid)
+	if err != nil {
+		return err
+	}
+
+	return os.Chown(name, -1, gid)
 }
 
 func SetFileModeForFile(file *os.File, mode os.FileMode) error {
