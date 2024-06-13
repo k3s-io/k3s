4a5f69fae155bbbfa85bd4cbb3052667f287f1be
Brad Davidson <brad.davidson@rancher.com>
2024-06-13 20:27:33 +0000

    Fix agent supervisor port using apiserver port instead
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/daemons/config/types.go
+++ pkg/daemons/config/types.go
@@ -58,6 +58,7 @@ type Node struct {
 	Token                    string
 	Certificate              *tls.Certificate
 	ServerHTTPSPort          int
+	SupervisorPort           int
 	DefaultRuntime           string
 }
 
