3345d8d8b3476656d13847675e356a1aa7be0733
Arne Winter <github@arnewinter.dev>
2024-09-06 21:58:03 +0200

    Update pkg/cloudprovider/instances.go
    Co-authored-by: Brad Davidson <brad@oatmail.org>
    Signed-off-by: Arne Winter <github@arnewinter.dev>

--- pkg/cloudprovider/instances.go
+++ pkg/cloudprovider/instances.go
@@ -93,8 +93,6 @@ func (k *k3s) InstanceMetadata(ctx context.Context, node *corev1.Node) (*cloudpr
 		for _, v := range strings.Split(address, ",") {
 			metadata.NodeAddresses = append(metadata.NodeAddresses, corev1.NodeAddress{Type: corev1.NodeExternalDNS, Address: v})
 		}
-	} else if address = node.Labels[ExternalDNSKey]; address != "" {
-		metadata.NodeAddresses = append(metadata.NodeAddresses, corev1.NodeAddress{Type: corev1.NodeExternalDNS, Address: address})
 	}
 
 	// check hostname
