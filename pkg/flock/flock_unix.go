865e8c043a70f16cbd6f07ab4c7acdd062638703
Derek Nola <derek.nola@suse.com>
2024-10-16 12:22:07 -0700

    Fix Github Actions for Ubuntu-24.04 (#11112)
    * Fix vagrant/libvirt composite action for ubuntu-24.04
    
    * Don't ignore changes to internal actions
    
    * Fix unit tests for ubuntu 24.04, new lsof version
    
    * Pin os version for unit and E2E workflows
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/flock/flock_unix.go
+++ pkg/flock/flock_unix.go
@@ -19,9 +19,6 @@ limitations under the License.
 package flock
 
 import (
-	"os/exec"
-	"strings"
-
 	"golang.org/x/sys/unix"
 )
 
@@ -48,15 +45,4 @@ func AcquireShared(path string) (int, error) {
 // Release removes an existing lock held by this process.
 func Release(lock int) error {
 	return unix.Flock(lock, unix.LOCK_UN)
-}
-
-// CheckLock checks whether any process is using the lock
-func CheckLock(path string) bool {
-	lockByte, _ := exec.Command("lsof", "-w", "-F", "ln", path).Output()
-	locks := string(lockByte)
-	if locks == "" {
-		return false
-	}
-	readWriteLock := strings.Split(locks, "\n")[2]
-	return readWriteLock == "lR" || readWriteLock == "lW"
 }
