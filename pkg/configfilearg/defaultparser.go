8c69ea76b15ebbba63cfae006186884f07c0ff56
Brad Davidson <brad.davidson@rancher.com>
2024-11-05 21:03:50 +0000

    Fix MustFindString returning override flags on external CLI commands
    External CLI actions cannot short-circuit on --help or --version, so we
    cannot skip loading the config file if these flags are present when
    running these wrapped commands. The behavior of just returning the
    override flag name instead of the requested flag value was breaking
    data-dir lookup when running wrapped commands.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/configfilearg/defaultparser.go
+++ pkg/configfilearg/defaultparser.go
@@ -1,6 +1,8 @@
 package configfilearg
 
 import (
+	"slices"
+
 	"github.com/k3s-io/k3s/pkg/cli/cmds"
 	"github.com/k3s-io/k3s/pkg/version"
 	"github.com/sirupsen/logrus"
@@ -23,15 +25,31 @@ func MustParse(args []string) []string {
 	return result
 }
 
-func MustFindString(args []string, target string) string {
+func MustFindString(args []string, target string, commandsWithoutOverride ...string) string {
+	overrideFlags := []string{"--help", "-h", "--version", "-v"}
+	// Check to see if the command or subcommand being executed supports override flags.
+	// Some subcommands such as `k3s ctr` or just `ctr` need to be extracted out even to
+	// provide version or help text, and we cannot short-circuit loading the config file. For
+	// these commands, treat failure to load the config file as a warning instead of a fatal.
+	if len(args) > 0 && args[0] == version.Program {
+		args = args[1:]
+	}
+	if len(args) > 0 && slices.Contains(commandsWithoutOverride, args[0]) {
+		overrideFlags = nil
+	}
+
 	parser := &Parser{
-		OverrideFlags: []string{"--help", "-h", "--version", "-v"},
+		OverrideFlags: overrideFlags,
 		EnvName:       version.ProgramUpper + "_CONFIG_FILE",
 		DefaultConfig: "/etc/rancher/" + version.Program + "/config.yaml",
 	}
 	result, err := parser.FindString(args, target)
 	if err != nil {
-		logrus.Fatal(err)
+		if len(overrideFlags) > 0 {
+			logrus.Fatal(err)
+		} else {
+			logrus.Warn(err)
+		}
 	}
 	return result
 }
