04565d3bd3bae44b705ec2f910e640f60ee1388c
Ludo Stellingwerff <ludo.stellingwerff@gmail.com>
2024-11-07 15:50:20 +0100

    Fix the "Standalone"-mode of oidc-login in the wrapped kubectl application. This fixes: 'error: no Auth Provider found for name "oidc"' when trying to run any subcommands in kubectl that require a valid server login.
    Signed-off-by: Ludo Stellingwerff <ludo.stellingwerff@gmail.com>
    (cherry picked from commit 2441e469502faac6287d4cec0f72a67aac455eab)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/kubectl/main.go
+++ pkg/kubectl/main.go
@@ -13,6 +13,9 @@ import (
 	"k8s.io/component-base/cli"
 	"k8s.io/kubectl/pkg/cmd"
 	"k8s.io/kubectl/pkg/cmd/util"
+
+	// Import to initialize client auth plugins.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 func Main() {
