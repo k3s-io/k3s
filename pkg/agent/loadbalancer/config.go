5cc8651d0d1c5a44fd85ea4a4cf22858099c6770
Brad Davidson <brad.davidson@rancher.com>
2024-11-15 01:32:12 +0000

    Separate persistent config struct from LoadBalancer and make fields private
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 67fd5fa9e5319e0880dd24d4dd188184e923b954)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/agent/loadbalancer/config.go
+++ pkg/agent/loadbalancer/config.go
@@ -7,8 +7,18 @@ import (
 	"github.com/k3s-io/k3s/pkg/agent/util"
 )
 
+// lbConfig stores loadbalancer state that should be persisted across restarts.
+type lbConfig struct {
+	ServerURL       string   `json:"ServerURL"`
+	ServerAddresses []string `json:"ServerAddresses"`
+}
+
 func (lb *LoadBalancer) writeConfig() error {
-	configOut, err := json.MarshalIndent(lb, "", "  ")
+	config := &lbConfig{
+		ServerURL:       lb.serverURL,
+		ServerAddresses: lb.serverAddresses,
+	}
+	configOut, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return err
 	}
@@ -18,9 +28,9 @@ func (lb *LoadBalancer) writeConfig() error {
 func (lb *LoadBalancer) updateConfig() error {
 	writeConfig := true
 	if configBytes, err := os.ReadFile(lb.configFile); err == nil {
-		config := &LoadBalancer{}
+		config := &lbConfig{}
 		if err := json.Unmarshal(configBytes, config); err == nil {
-			if config.ServerURL == lb.ServerURL {
+			if config.ServerURL == lb.serverURL {
 				writeConfig = false
 				lb.setServers(config.ServerAddresses)
 			}
