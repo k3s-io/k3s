3c273b0761dd97e4e52ed550a2a702b0728cebc6
Derek Nola <derek.nola@suse.com>
2024-11-26 12:30:52 -0800

    Convert legacy docker tests from bash to golang (#11357)
    * Convert the following Docker test from Bash to Go
        - basics
        - bootstraptoken
        - cacerts
        - compat -> skew
        - etcd
        - lazypull
        - upgrade
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Add Docker go tests to GHA
    * Prebuild K3s Go Tests
    * Strip go test binaries to reduce size
    * Handle complex branch options
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Implement basic golang tests on arm and arm64 pipelines
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

---
+++ tests/docker/etcd/etcd_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	tester "github.com/k3s-io/k3s/tests/docker"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var k3sImage = flag.String("k3sImage", "", "The k3s image used to provision containers")
+var config *tester.TestConfig
+
+func Test_DockerEtcd(t *testing.T) {
+	flag.Parse()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Etcd Docker Test Suite")
+}
+
+var _ = Describe("Etcd Tests", Ordered, func() {
+
+	Context("Test a 3 server cluster", func() {
+		It("should setup the cluster configuration", func() {
+			var err error
+			config, err = tester.NewTestConfig(*k3sImage)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should provision servers", func() {
+			Expect(config.ProvisionServers(3)).To(Succeed())
+			Eventually(func() error {
+				return tester.DeploymentsReady([]string{"coredns", "local-path-provisioner", "metrics-server", "traefik"}, config.KubeconfigFile)
+			}, "60s", "5s").Should(Succeed())
+			Eventually(func(g Gomega) {
+				g.Expect(tester.ParseNodes(config.KubeconfigFile)).To(HaveLen(3))
+				g.Expect(tester.NodesReady(config.KubeconfigFile)).To(Succeed())
+			}, "60s", "5s").Should(Succeed())
+		})
+		It("should destroy the cluster", func() {
+			Expect(config.Cleanup()).To(Succeed())
+		})
+	})
+
+	Context("Test a Split Role cluster with 3 etcd, 2 control-plane, 1 agents", func() {
+		It("should setup the cluster configuration", func() {
+			var err error
+			config, err = tester.NewTestConfig(*k3sImage)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(os.Setenv("SERVER_0_ARGS", "--disable-apiserver --disable-controller-manager --disable-scheduler --cluster-init")).To(Succeed())
+			Expect(os.Setenv("SERVER_1_ARGS", "--disable-apiserver --disable-controller-manager --disable-scheduler")).To(Succeed())
+			Expect(os.Setenv("SERVER_2_ARGS", "--disable-apiserver --disable-controller-manager --disable-scheduler")).To(Succeed())
+			Expect(os.Setenv("SERVER_3_ARGS", "--disable-etcd")).To(Succeed())
+			Expect(os.Setenv("SERVER_4_ARGS", "--disable-etcd")).To(Succeed())
+		})
+		It("should provision servers and agents", func() {
+			Expect(config.ProvisionServers(5)).To(Succeed())
+			Expect(config.ProvisionAgents(1)).To(Succeed())
+			Eventually(func() error {
+				return tester.DeploymentsReady([]string{"coredns", "local-path-provisioner", "metrics-server", "traefik"}, config.KubeconfigFile)
+			}, "90s", "5s").Should(Succeed())
+			Eventually(func(g Gomega) {
+				g.Expect(tester.ParseNodes(config.KubeconfigFile)).To(HaveLen(6))
+				g.Expect(tester.NodesReady(config.KubeconfigFile)).To(Succeed())
+			}, "60s", "5s").Should(Succeed())
+		})
+	})
+})
+
+var failed bool
+var _ = AfterEach(func() {
+	failed = failed || CurrentSpecReport().Failed()
+})
+
+var _ = AfterSuite(func() {
+	if config != nil && !failed {
+		config.Cleanup()
+	}
+})
---
+++ tests/docker/lazypull/lazypull_test.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	tester "github.com/k3s-io/k3s/tests/docker"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var k3sImage = flag.String("k3sImage", "", "The k3s image used to provision containers")
+var config *tester.TestConfig
+
+func Test_DockerLazyPull(t *testing.T) {
+	flag.Parse()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "LazyPull Docker Test Suite")
+}
+
+var _ = Describe("LazyPull Tests", Ordered, func() {
+
+	Context("Setup Cluster", func() {
+		It("should provision servers", func() {
+			var err error
+			config, err = tester.NewTestConfig(*k3sImage)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(os.Setenv("SERVER_ARGS", "--snapshotter=stargz")).To(Succeed())
+			Expect(config.ProvisionServers(1)).To(Succeed())
+			Eventually(func() error {
+				return tester.DeploymentsReady([]string{"coredns", "local-path-provisioner", "metrics-server", "traefik"}, config.KubeconfigFile)
+			}, "60s", "5s").Should(Succeed())
+			Eventually(func() error {
+				return tester.NodesReady(config.KubeconfigFile)
+			}, "40s", "5s").Should(Succeed())
+		})
+	})
+
+	Context("Use Snapshot Container", func() {
+		It("should apply local storage volume", func() {
+			const snapshotTestManifest = "../resources/snapshot-test.yaml"
+
+			// Apply the manifest
+			cmd := fmt.Sprintf("kubectl apply -f %s --kubeconfig=%s", snapshotTestManifest, config.KubeconfigFile)
+			_, err := tester.RunCommand(cmd)
+			Expect(err).NotTo(HaveOccurred(), "failed to apply volume test manifest")
+		})
+		It("should have the pod come up", func() {
+			Eventually(func() (bool, error) {
+				return tester.PodReady("stargz-snapshot-test", "default", config.KubeconfigFile)
+			}, "30s", "5s").Should(BeTrue())
+		})
+		var topLayer string
+		It("extracts the topmost layer of the container", func() {
+			Eventually(func() (string, error) {
+				var err error
+				topLayer, err = getTopmostLayer(config.Servers[0].Name, "stargz-snapshot-test")
+				topLayer = strings.TrimSpace(topLayer)
+				return topLayer, err
+			}, "30s", "5s").ShouldNot(BeEmpty())
+			fmt.Println("Topmost layer: ", topLayer)
+		})
+		It("checks all layers are remote snapshots", func() {
+			Expect(lookLayers(config.Servers[0].Name, topLayer)).To(Succeed())
+		})
+	})
+})
+
+var failed bool
+var _ = AfterEach(func() {
+	failed = failed || CurrentSpecReport().Failed()
+})
+
+var _ = AfterSuite(func() {
+	if config != nil && !failed {
+		config.Cleanup()
+	}
+})
+
+func lookLayers(node, layer string) error {
+	remoteSnapshotLabel := "containerd.io/snapshot/remote"
+	layersNum := 0
+	var err error
+	for layersNum = 0; layersNum < 100; layersNum++ {
+		// We use RunCommand instead of RunCmdOnDocker because we pipe the output to jq
+		cmd := fmt.Sprintf("docker exec -i %s ctr --namespace=k8s.io snapshot --snapshotter=stargz info %s | jq -r '.Parent'", node, layer)
+		layer, err = tester.RunCommand(cmd)
+		if err != nil {
+			return fmt.Errorf("failed to get parent layer: %v", err)
+		}
+		layer = strings.TrimSpace(layer)
+		// If the layer is null, we have reached the topmost layer
+		if layer == "null" {
+			break
+		}
+		cmd = fmt.Sprintf("docker exec -i %s ctr --namespace=k8s.io snapshots --snapshotter=stargz info %s | jq -r '.Labels.\"%s\"'", node, layer, remoteSnapshotLabel)
+		label, err := tester.RunCommand(cmd)
+		if err != nil {
+			return fmt.Errorf("failed to get layer label: %v", err)
+		}
+		label = strings.TrimSpace(label)
+		fmt.Printf("Checking layer %s : %s\n", layer, label)
+		if label == "null" {
+			return fmt.Errorf("layer %s isn't remote snapshot", layer)
+		}
+	}
+
+	if layersNum == 0 {
+		return fmt.Errorf("cannot get layers")
+	} else if layersNum >= 100 {
+		return fmt.Errorf("testing image contains too many layers > 100")
+	}
+
+	return nil
+}
+
+func getTopmostLayer(node, container string) (string, error) {
+	var targetContainer string
+	cmd := fmt.Sprintf("docker exec -i %s ctr --namespace=k8s.io c ls -q labels.\"io.kubernetes.container.name\"==\"%s\" | sed -n 1p", node, container)
+	targetContainer, _ = tester.RunCommand(cmd)
+	targetContainer = strings.TrimSpace(targetContainer)
+	fmt.Println("targetContainer: ", targetContainer)
+	if targetContainer == "" {
+		return "", fmt.Errorf("failed to get target container")
+	}
+	cmd = fmt.Sprintf("docker exec -i %s ctr --namespace=k8s.io c info %s | jq -r '.SnapshotKey'", node, targetContainer)
+	layer, err := tester.RunCommand(cmd)
+	if err != nil {
+		return "", fmt.Errorf("failed to get topmost layer: %v", err)
+	}
+	return strings.TrimSpace(layer), nil
+}
