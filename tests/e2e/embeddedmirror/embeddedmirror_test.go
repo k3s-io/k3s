99f584526eb3c3bc579774b9a9cf2df895528371
Derek Nola <derek.nola@suse.com>
2025-02-21 10:33:24 -0800

    Replace e2e.ParseNodes function with general ParseNodes and NodesReady test functions
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/embeddedmirror/embeddedmirror_test.go
+++ tests/e2e/embeddedmirror/embeddedmirror_test.go
@@ -53,12 +53,8 @@ var _ = Describe("Verify Create", Ordered, func() {
 		})
 		It("Checks node and pod status", func() {
 			By("Fetching Nodes status")
-			Eventually(func(g Gomega) {
-				nodes, err := e2e.ParseNodes(tc.KubeconfigFile, false)
-				g.Expect(err).NotTo(HaveOccurred())
-				for _, node := range nodes {
-					g.Expect(node.Status).Should(Equal("Ready"))
-				}
+			Eventually(func() error {
+				return tests.NodesReady(tc.KubeconfigFile, e2e.VagrantSlice(tc.AllNodes()))
 			}, "620s", "5s").Should(Succeed())
 
 			By("Fetching pod status")
