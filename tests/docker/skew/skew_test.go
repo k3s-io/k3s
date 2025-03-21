fd4c7f8ee7e58deeee6cd70fba0fb92a8e1fd1c1
Derek Nola <derek.nola@suse.com>
2025-03-21 08:15:08 -0700

    Bump skew test timeout
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/docker/skew/skew_test.go
+++ tests/docker/skew/skew_test.go
@@ -101,7 +101,7 @@ var _ = Describe("Skew Tests", Ordered, func() {
 			Expect(config.ProvisionServers(3)).To(Succeed())
 			Eventually(func() error {
 				return tests.CheckDeployments([]string{"coredns", "local-path-provisioner", "metrics-server", "traefik"}, config.KubeconfigFile)
-			}, "120s", "5s").Should(Succeed())
+			}, "180s", "5s").Should(Succeed())
 			Eventually(func(g Gomega) {
 				g.Expect(tests.ParseNodes(config.KubeconfigFile)).To(HaveLen(3))
 				g.Expect(tests.NodesReady(config.KubeconfigFile, config.GetNodeNames())).To(Succeed())
