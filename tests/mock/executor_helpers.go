25541e48afbbb079202bab0072bd2b5b64201faa
Brad Davidson <brad.davidson@rancher.com>
2025-03-27 20:30:02 +0000

    Rework mock executor using gomock for call validation
    Generate the mock executor with mockgen and convert existing uses of the mock executor to set it up properly.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 0eeac6a622efa11f8f760d3c909fe28e7db52131)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

---
+++ tests/mock/executor_helpers.go
@@ -0,0 +1,33 @@
+package mock
+
+import (
+	"testing"
+
+	executor "github.com/k3s-io/k3s/pkg/daemons/executor"
+	"go.uber.org/mock/gomock"
+)
+
+// NewExecutorWithEmbeddedETCD creates a new mock executor, and sets it as the current executor.
+// The executor exepects calls to ETCD(), and wraps the embedded executor method of the same name.
+// The various ready channels are also mocked with immediate channel closure.
+func NewExecutorWithEmbeddedETCD(t *testing.T) {
+	mockController := gomock.NewController(t)
+	mockExecutor := NewExecutor(mockController)
+
+	embed := &executor.Embedded{}
+	initialOptions := func() (executor.InitialOptions, error) {
+		return executor.InitialOptions{}, nil
+	}
+	closedChannel := func() <-chan struct{} {
+		c := make(chan struct{})
+		close(c)
+		return c
+	}
+
+	mockExecutor.EXPECT().ETCD(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(embed.ETCD)
+	mockExecutor.EXPECT().CurrentETCDOptions().AnyTimes().DoAndReturn(initialOptions)
+	mockExecutor.EXPECT().CRIReadyChan().AnyTimes().DoAndReturn(closedChannel)
+	mockExecutor.EXPECT().ETCDReadyChan().AnyTimes().DoAndReturn(closedChannel)
+
+	executor.Set(mockExecutor)
+}
