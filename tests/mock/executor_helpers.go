becff1d6031db35daa7dec0f2c077bea698079de
Brad Davidson <brad.davidson@rancher.com>
2025-03-27 21:04:48 +0000

    Add tests for control-plane component arg generation
    Use mocked executor to ensure the correct args are being passed to components
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 1ba19856decb5f0980e52d493ca7857de0857657)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- tests/mock/executor_helpers.go
+++ tests/mock/executor_helpers.go
@@ -10,24 +10,25 @@ import (
 // NewExecutorWithEmbeddedETCD creates a new mock executor, and sets it as the current executor.
 // The executor exepects calls to ETCD(), and wraps the embedded executor method of the same name.
 // The various ready channels are also mocked with immediate channel closure.
-func NewExecutorWithEmbeddedETCD(t *testing.T) {
+func NewExecutorWithEmbeddedETCD(t *testing.T) *Executor {
 	mockController := gomock.NewController(t)
 	mockExecutor := NewExecutor(mockController)
 
 	embed := &executor.Embedded{}
-	initialOptions := func() (executor.InitialOptions, error) {
-		return executor.InitialOptions{}, nil
-	}
+	mockExecutor.EXPECT().Bootstrap(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(embed.Bootstrap)
+	mockExecutor.EXPECT().CurrentETCDOptions().AnyTimes().DoAndReturn(embed.CurrentETCDOptions)
+	mockExecutor.EXPECT().ETCD(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(embed.ETCD)
+	mockExecutor.EXPECT().ETCDReadyChan().AnyTimes().DoAndReturn(embed.ETCDReadyChan)
+
 	closedChannel := func() <-chan struct{} {
 		c := make(chan struct{})
 		close(c)
 		return c
 	}
-
-	mockExecutor.EXPECT().ETCD(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(embed.ETCD)
-	mockExecutor.EXPECT().CurrentETCDOptions().AnyTimes().DoAndReturn(initialOptions)
+	mockExecutor.EXPECT().APIServerReadyChan().AnyTimes().DoAndReturn(closedChannel)
 	mockExecutor.EXPECT().CRIReadyChan().AnyTimes().DoAndReturn(closedChannel)
-	mockExecutor.EXPECT().ETCDReadyChan().AnyTimes().DoAndReturn(closedChannel)
 
 	executor.Set(mockExecutor)
+
+	return mockExecutor
 }
