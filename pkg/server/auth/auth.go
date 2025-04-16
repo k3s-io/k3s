4f9b7b4bd4c7d5343780e5e3a33b1b95dd394f2c
Brad Davidson <brad.davidson@rancher.com>
2025-04-16 20:45:22 +0000

    Move delegating auth middleware into common package and add MaxInFlight
    Adds maximum in-flight request limits to agent join and p2p peer info
    request request handlers.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/server/auth/auth.go
+++ pkg/server/auth/auth.go
@@ -8,8 +8,20 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/k3s-io/k3s/pkg/daemons/config"
 	"github.com/k3s-io/k3s/pkg/util"
+	"github.com/k3s-io/k3s/pkg/version"
 	"github.com/sirupsen/logrus"
-	"k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/apiserver/pkg/apis/apiserver"
+	genericapifilters "k8s.io/apiserver/pkg/endpoints/filters"
+	apirequest "k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/apiserver/pkg/server"
+	genericfilters "k8s.io/apiserver/pkg/server/filters"
+	"k8s.io/apiserver/pkg/server/options"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+var (
+	requestInfoResolver = &apirequest.RequestInfoFactory{}
+	failedHandler       = genericapifilters.Unauthorized(scheme.Codecs)
 )
 
 func hasRole(mustRoles []string, roles []string) bool {
@@ -48,7 +60,7 @@ func doAuth(roles []string, serverConfig *config.Control, next http.Handler, rw 
 		return
 	}
 
-	ctx := request.WithUser(req.Context(), resp.User)
+	ctx := apirequest.WithUser(req.Context(), resp.User)
 	req = req.WithContext(ctx)
 	next.ServeHTTP(rw, req)
 }
@@ -75,5 +87,56 @@ func IsLocalOrHasRole(serverConfig *config.Control, roles ...string) mux.Middlew
 				doAuth(roles, serverConfig, next, rw, req)
 			}
 		})
+	}
+}
+
+// Delegated returns a middleware function that uses core Kubernetes
+// authentication/authorization via client certificate auth and the SubjectAccessReview API
+func Delegated(clientCA, kubeConfig string, config *server.Config) mux.MiddlewareFunc {
+	if config == nil {
+		config = &server.Config{}
+	}
+
+	authn := options.NewDelegatingAuthenticationOptions()
+	authn.Anonymous = &apiserver.AnonymousAuthConfig{
+		Enabled: false,
+	}
+	authn.SkipInClusterLookup = true
+	authn.ClientCert = options.ClientCertAuthenticationOptions{
+		ClientCA: clientCA,
+	}
+	authn.RemoteKubeConfigFile = kubeConfig
+	if err := authn.ApplyTo(&config.Authentication, config.SecureServing, nil); err != nil {
+		logrus.Fatalf("Failed to apply authentication configuration: %v", err)
+	}
+
+	authz := options.NewDelegatingAuthorizationOptions()
+	authz.AlwaysAllowPaths = []string{ // skip authz for paths that should not use SubjectAccessReview; basically everything that will use this router other than metrics
+		"/v1-" + version.Program + "/p2p", // spegel libp2p peer discovery
+		"/v2/*",                           // spegel registry mirror
+		"/debug/pprof/*",                  // profiling
+	}
+	authz.RemoteKubeConfigFile = kubeConfig
+	if err := authz.ApplyTo(&config.Authorization); err != nil {
+		logrus.Fatalf("Failed to apply authorization configuration: %v", err)
+	}
+
+	return func(handler http.Handler) http.Handler {
+		handler = genericapifilters.WithAuthorization(handler, config.Authorization.Authorizer, scheme.Codecs)
+		handler = genericapifilters.WithAuthentication(handler, config.Authentication.Authenticator, failedHandler, nil, nil)
+		handler = genericapifilters.WithRequestInfo(handler, requestInfoResolver)
+		handler = genericapifilters.WithCacheControl(handler)
+		return handler
+	}
+}
+
+// MaxInFlight returns a middleware function that limits the number of requests that are executed concurrently.
+// This is not strictly auth related, but it also uses the core Kubernetes request filters.
+func MaxInFlight(nonMutatingLimit, mutatingLimit int) mux.MiddlewareFunc {
+	return func(handler http.Handler) http.Handler {
+		handler = genericfilters.WithMaxInFlightLimit(handler, nonMutatingLimit, mutatingLimit, nil)
+		handler = genericapifilters.WithRequestInfo(handler, requestInfoResolver)
+		handler = genericapifilters.WithCacheControl(handler)
+		return handler
 	}
 }
