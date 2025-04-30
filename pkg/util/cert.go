a23983e31bfa69764320c03ee27d6c7a502c6933
bo.jiang <bo.jiang@daocloud.io>
2025-04-30 17:40:47 +0800

    Optimize certificate status check
    Signed-off-by: bo.jiang <bo.jiang@daocloud.io>
    (cherry picked from commit 4c1f014d279e7758193cefdbd00726ff4b12d4c5)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/util/cert.go
+++ pkg/util/cert.go
@@ -2,8 +2,25 @@ package util
 
 import (
 	"crypto/x509"
+	"time"
 
 	certutil "github.com/rancher/dynamiclistener/cert"
+)
+
+// cert usage constants
+const (
+	CertUsageCertSign   = "CertSign"
+	CertUsageServerAuth = "ServerAuth"
+	CertUsageClientAuth = "ClientAuth"
+	CertUsageUnknown    = "Unknown"
+)
+
+// cert status constants
+const (
+	CertStatusOK          = "OK"
+	CertStatusWarning     = "WARNING"
+	CertStatusExpired     = "EXPIRED"
+	CertStatusNotYetValid = "NOT YET VALID"
 )
 
 // EncodeCertsPEM is a wrapper around the EncodeCertPEM function to return the
@@ -14,4 +31,36 @@ func EncodeCertsPEM(cert *x509.Certificate, caCerts []*x509.Certificate) []byte 
 		pemBytes = append(pemBytes, certutil.EncodeCertPEM(caCert)...)
 	}
 	return pemBytes
+}
+
+// GetCertUsages returns a slice of strings representing the certificate usages
+func GetCertUsages(cert *x509.Certificate) []string {
+	usages := []string{}
+	if cert.KeyUsage&x509.KeyUsageCertSign != 0 {
+		usages = append(usages, CertUsageCertSign)
+	}
+	for _, eku := range cert.ExtKeyUsage {
+		switch eku {
+		case x509.ExtKeyUsageServerAuth:
+			usages = append(usages, CertUsageServerAuth)
+		case x509.ExtKeyUsageClientAuth:
+			usages = append(usages, CertUsageClientAuth)
+		}
+	}
+	if len(usages) == 0 {
+		usages = append(usages, CertUsageUnknown)
+	}
+	return usages
+}
+
+// GetCertStatus determines the status of a certificate based on its validity period
+func GetCertStatus(cert *x509.Certificate, now time.Time, warn time.Time) string {
+	if now.Before(cert.NotBefore) {
+		return CertStatusNotYetValid
+	} else if now.After(cert.NotAfter) {
+		return CertStatusExpired
+	} else if warn.After(cert.NotAfter) {
+		return CertStatusWarning
+	}
+	return CertStatusOK
 }
