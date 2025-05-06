7ae2a16aa76d6566f1f58ebfc4e3d3ddcf7de81d
Brad Davidson <brad.davidson@rancher.com>
2025-05-06 20:02:26 +0000

    Add anonymous-auth to flags gated on empty authorization-config value
    Also warn if default flags are not set due to user provided config
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 921e5029184e4887730fbfaa72ae16eb247d4d09)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/daemons/control/server.go
+++ pkg/daemons/control/server.go
@@ -208,7 +208,9 @@ func apiServer(ctx context.Context, cfg *config.Control) error {
 	argsMap["allow-privileged"] = "true"
 	argsMap["enable-bootstrap-token-auth"] = "true"
 	if authConfigFile := util.ArgValue("authorization-config", cfg.ExtraAPIArgs); authConfigFile == "" {
+		logrus.Warn("Not setting kube-apiserver 'authorization-mode' and 'anonymous-auth' flags due to user-provided 'authorization-config' file.")
 		argsMap["authorization-mode"] = strings.Join([]string{modes.ModeNode, modes.ModeRBAC}, ",")
+		argsMap["anonymous-auth"] = "false"
 	}
 	argsMap["service-account-signing-key-file"] = runtime.ServiceCurrentKey
 	argsMap["service-cluster-ip-range"] = util.JoinIPNets(cfg.ServiceIPRanges)
@@ -249,7 +251,6 @@ func apiServer(ctx context.Context, cfg *config.Control) error {
 	argsMap["requestheader-username-headers"] = "X-Remote-User"
 	argsMap["client-ca-file"] = runtime.ClientCA
 	argsMap["enable-admission-plugins"] = "NodeRestriction"
-	argsMap["anonymous-auth"] = "false"
 	argsMap["profiling"] = "false"
 	if cfg.EncryptSecrets {
 		argsMap["encryption-provider-config"] = runtime.EncryptionConfig
