e4b80ee24203ce91f2fcc0c938ea07f3270870e4
Caio Torres <caio.torres@suse.com>
2025-05-28 16:41:35 -0300

    feat: remove master role labels (#12395)
    Signed-off-by: Caio Torres <caio.torres@suse.com>

--- pkg/cloudprovider/servicelb.go
+++ pkg/cloudprovider/servicelb.go
@@ -490,11 +490,6 @@ func (k *k3s) newDaemonSet(svc *core.Service) (*apps.DaemonSet, error) {
 					SecurityContext:              securityContext,
 					Tolerations: []core.Toleration{
 						{
-							Key:      util.MasterRoleLabelKey,
-							Operator: "Exists",
-							Effect:   "NoSchedule",
-						},
-						{
 							Key:      util.ControlPlaneRoleLabelKey,
 							Operator: "Exists",
 							Effect:   "NoSchedule",
