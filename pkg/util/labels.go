43580e26a33f86f4dc335a7d5717e6cb50b12a90
Caio Torres <caio.torres@suse.com>
2025-05-28 16:41:35 -0300

    feat: remove master role labels (#12395)
    Signed-off-by: Caio Torres <caio.torres@suse.com>

--- pkg/util/labels.go
+++ pkg/util/labels.go
@@ -1,7 +1,6 @@
 package util
 
 const (
-	MasterRoleLabelKey       = "node-role.kubernetes.io/master"
 	ControlPlaneRoleLabelKey = "node-role.kubernetes.io/control-plane"
 	ETCDRoleLabelKey         = "node-role.kubernetes.io/etcd"
 )
