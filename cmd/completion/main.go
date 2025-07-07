d46cdb4395ce04b888d4607ea69345037a8e091c
Vitor Savian <vitor.savian@suse.com>
2025-07-07 17:04:31 -0300

    Refac for shell completion
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>

--- cmd/completion/main.go
+++ cmd/completion/main.go
@@ -14,7 +14,10 @@ import (
 func main() {
 	app := cmds.NewApp()
 	app.Commands = []*cli.Command{
-		cmds.NewCompletionCommand(completion.Run),
+		cmds.NewCompletionCommand(
+			completion.RunBash,
+			completion.RunZsh,
+		),
 	}
 
 	if err := app.Run(os.Args); err != nil && !errors.Is(err, context.Canceled) {
