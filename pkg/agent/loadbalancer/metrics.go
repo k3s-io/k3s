63539c8e2fcb85e70e496221a50e3af4d32ec6b4
Brad Davidson <brad.davidson@rancher.com>
2025-07-16 22:59:10 +0000

    Update metric help to be more descriptive.
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/agent/loadbalancer/metrics.go
+++ pkg/agent/loadbalancer/metrics.go
@@ -9,17 +9,18 @@ import (
 var (
 	loadbalancerConnections = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: version.Program + "_loadbalancer_server_connections",
-		Help: "Count of current connections to loadbalancer server",
+		Help: "Count of current connections to loadbalancer server, labeled by loadbalancer name and server address.",
 	}, []string{"name", "server"})
 
 	loadbalancerState = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: version.Program + "_loadbalancer_server_health",
-		Help: "Current health value of loadbalancer server",
+		Help: "Current health state of loadbalancer backend servers, labeled by loadbalancer name and server address. " +
+			"State is enum of 0=INVALID, 1=FAILED, 2=STANDBY, 3=UNCHECKED, 4=RECOVERING, 5=HEALTHY, 6=PREFERRED, 7=ACTIVE.",
 	}, []string{"name", "server"})
 
 	loadbalancerDials = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    version.Program + "_loadbalancer_dial_duration_seconds",
-		Help:    "Time taken to dial a connection to a backend server",
+		Help:    "Time in seconds taken to dial a connection to a backend server, labeled by loadbalancer name and success/failure status.",
 		Buckets: metrics.ExponentialBuckets(0.001, 2, 15),
 	}, []string{"name", "status"})
 )
