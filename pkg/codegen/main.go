dd6c906e29ceddf57d47160de19cdff6d95d143a
Emin Umut Gercek <eumutgercek@gmail.com>
2025-07-22 22:32:09 +0300

    Implement embed.go
    Signed-off-by: Emin Umut Gercek <eumutgercek@gmail.com>

--- pkg/codegen/main.go
+++ pkg/codegen/main.go
@@ -29,23 +29,6 @@ func main() {
 	bc = &bindata.Config{
 		Input: []bindata.InputConfig{
 			{
-				Path:      "manifests",
-				Recursive: true,
-			},
-		},
-		Package:    "deploy",
-		NoMetadata: true,
-		Prefix:     "manifests/",
-		Output:     "pkg/deploy/zz_generated_bindata.go",
-		Tags:       "!no_stage",
-	}
-	if err := bindata.Translate(bc); err != nil {
-		logrus.Fatal(err)
-	}
-
-	bc = &bindata.Config{
-		Input: []bindata.InputConfig{
-			{
 				Path:      "build/static",
 				Recursive: true,
 			},
