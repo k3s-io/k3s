35fec8031b408998882545c6f25bf56622e9fb86
muicoder <muicoder@gmail.com>
2025-07-29 15:50:35 +0800

    refactor: replace go-bindata with native embed package
    Signed-off-by: muicoder <muicoder@gmail.com>
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -1,1 +1,17 @@
 package data
+
+import (
+	"embed"
+
+	"github.com/k3s-io/k3s/pkg/util/bindata"
+)
+
+//go:embed embed/*
+var embedFS embed.FS
+
+var (
+	bd = bindata.Bindata{FS: &embedFS, Prefix: "embed"}
+
+	Asset      = bd.Asset
+	AssetNames = bd.AssetNames
+)
