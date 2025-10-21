2a0e5a8fd92a97723e5b7b88f1502f4e4ff8813c
Brad Davidson <brad.davidson@rancher.com>
2025-10-21 20:08:42 +0000

    Remove node password file migration code
    K3s stopped using node password files in v1.19 (92d04355f46e), so we do not need to support migrating off these any longer.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit d622b18c2008888f2ad3ca563cf7aea6403c1352)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/nodepassword/nodepassword_test.go
+++ pkg/nodepassword/nodepassword_test.go
@@ -10,10 +10,7 @@ import (
 
 	"github.com/k3s-io/k3s/tests/mock"
 	"go.uber.org/mock/gomock"
-	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
@@ -54,90 +51,6 @@ func Test_UnitEnsureDelete(t *testing.T) {
 	assertEqual(t, Ensure(secretClient, "node1", "Hello Universe"), nil)
 
 	logMemUsage(t)
-}
-
-func Test_UnitMigrateFile(t *testing.T) {
-	nodePasswordFile := generateNodePasswordFile(migrateNumNodes)
-	defer os.Remove(nodePasswordFile)
-
-	v1Mock := mock.NewV1(gomock.NewController(t))
-
-	secretClient := v1Mock.SecretMock
-	secretCache := v1Mock.SecretCache
-	secretStore := &mock.SecretStore{}
-
-	nodeClient := v1Mock.NodeMock
-	nodeCache := v1Mock.NodeCache
-	nodeStore := &mock.NodeStore{}
-
-	// Set up expected call counts for tests
-	// Expect to see 1 node list, any number of cache gets, and however many
-	// creates as we are migrating.
-	secretClient.EXPECT().Create(gomock.Any()).Times(migrateNumNodes).DoAndReturn(secretStore.Create)
-	secretClient.EXPECT().Cache().AnyTimes().Return(secretCache)
-	secretCache.EXPECT().Get(gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(secretStore.Get)
-	nodeClient.EXPECT().Cache().AnyTimes().Return(nodeCache)
-	nodeCache.EXPECT().List(gomock.Any()).Times(1).DoAndReturn(nodeStore.List)
-
-	// Run tests
-	logMemUsage(t)
-	if err := MigrateFile(secretClient, nodeClient, nodePasswordFile); err != nil {
-		t.Fatal(err)
-	}
-	logMemUsage(t)
-
-	assertNotEqual(t, Ensure(secretClient, "node1", "Hello World"), nil)
-	assertEqual(t, Ensure(secretClient, "node1", "node1"), nil)
-}
-
-func Test_UnitMigrateFileNodes(t *testing.T) {
-	nodePasswordFile := generateNodePasswordFile(migrateNumNodes)
-	defer os.Remove(nodePasswordFile)
-
-	v1Mock := mock.NewV1(gomock.NewController(t))
-
-	secretClient := v1Mock.SecretMock
-	secretCache := v1Mock.SecretCache
-	secretStore := &mock.SecretStore{}
-
-	nodeClient := v1Mock.NodeMock
-	nodeCache := v1Mock.NodeCache
-	nodeStore := &mock.NodeStore{}
-
-	for i := 0; i < createNumNodes; i++ {
-		if _, err := nodeStore.Create(&v1.Node{ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("node%d", i+1)}}); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// Set up expected call counts for tests
-	// Expect to see 1 node list, any number of cache gets, and however many
-	// creates as we are migrating - plus an extra new node at the end.
-	secretClient.EXPECT().Create(gomock.Any()).Times(migrateNumNodes + 1).DoAndReturn(secretStore.Create)
-	secretClient.EXPECT().Cache().AnyTimes().Return(secretCache)
-	secretCache.EXPECT().Get(gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(secretStore.Get)
-	nodeClient.EXPECT().Cache().AnyTimes().Return(nodeCache)
-	nodeCache.EXPECT().List(gomock.Any()).Times(1).DoAndReturn(nodeStore.List)
-
-	// Run tests
-	logMemUsage(t)
-	if err := MigrateFile(secretClient, nodeClient, nodePasswordFile); err != nil {
-		t.Fatal(err)
-	}
-	logMemUsage(t)
-
-	if nodes, err := nodeStore.List(labels.Everything()); err != nil {
-		t.Fatal(err)
-	} else {
-		for _, node := range nodes {
-			assertNotEqual(t, Ensure(secretClient, node.Name, "wrong-password"), nil)
-			assertEqual(t, Ensure(secretClient, node.Name, node.Name), nil)
-		}
-	}
-
-	newNode := fmt.Sprintf("node%d", migrateNumNodes+1)
-	assertEqual(t, Ensure(secretClient, newNode, "new-password"), nil)
-	assertNotEqual(t, Ensure(secretClient, newNode, "wrong-password"), nil)
 }
 
 func Test_PasswordError(t *testing.T) {
