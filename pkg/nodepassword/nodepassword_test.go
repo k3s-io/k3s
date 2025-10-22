27c84bbaa0b4f985e4a3014210dbe9833825554e
Brad Davidson <brad.davidson@rancher.com>
2025-10-22 08:04:53 +0000

    Move node password secrets into dedicated controller
    Move the node password secret cleanup into its own dedicated controller
    that also handles auth. We now use a filtered cache of only
    node-password secrets, instead of using the wrangler secret cache,
    which stores all secrets from all namespaces.
    
    The coredns node-hosts controller also now uses a single-resource
    watch cache on the coredns configmap, instead of reading it from
    the apiserver every time a node changes.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit d8790220ff6efefe94d4040cb1a37011898a023f)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/nodepassword/nodepassword_test.go
+++ pkg/nodepassword/nodepassword_test.go
@@ -8,8 +8,6 @@ import (
 	"runtime"
 	"testing"
 
-	"github.com/golang/mock/gomock"
-	"github.com/k3s-io/k3s/tests/mock"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -20,37 +18,6 @@ const createNumNodes = 3
 func Test_UnitAsserts(t *testing.T) {
 	assertEqual(t, 1, 1)
 	assertNotEqual(t, 1, 0)
-}
-
-func Test_UnitEnsureDelete(t *testing.T) {
-	logMemUsage(t)
-
-	v1Mock := mock.NewV1(gomock.NewController(t))
-
-	secretClient := v1Mock.SecretMock
-	secretCache := v1Mock.SecretCache
-	secretStore := &mock.SecretStore{}
-
-	// Set up expected call counts for tests
-	// Expect to see 2 creates, any number of cache gets, and 2 deletes.
-	secretClient.EXPECT().Create(gomock.Any()).Times(2).DoAndReturn(secretStore.Create)
-	secretClient.EXPECT().Delete(gomock.Any(), gomock.Any(), gomock.Any()).Times(2).DoAndReturn(secretStore.Delete)
-	secretClient.EXPECT().Cache().AnyTimes().Return(secretCache)
-	secretCache.EXPECT().Get(gomock.Any(), gomock.Any()).AnyTimes().DoAndReturn(secretStore.Get)
-
-	// Run tests
-	assertEqual(t, Ensure(secretClient, "node1", "Hello World"), nil)
-	assertEqual(t, Ensure(secretClient, "node1", "Hello World"), nil)
-	assertNotEqual(t, Ensure(secretClient, "node1", "Goodbye World"), nil)
-
-	assertEqual(t, Delete(secretClient, "node1"), nil)
-	assertNotEqual(t, Delete(secretClient, "node1"), nil)
-
-	assertEqual(t, Ensure(secretClient, "node1", "Hello Universe"), nil)
-	assertNotEqual(t, Ensure(secretClient, "node1", "Hello World"), nil)
-	assertEqual(t, Ensure(secretClient, "node1", "Hello Universe"), nil)
-
-	logMemUsage(t)
 }
 
 func Test_PasswordError(t *testing.T) {
