5ddbad77f45a75fc0e3cf5df23f7994abfc9f86a
Derek Nola <derek.nola@suse.com>
2025-12-18 15:54:49 -0800

    Allow k3s secrets-encrypt enable on existing clusters
    - Places an identity provider as a setup to enable later encryption
    - Update secrets-encryption test
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/cli/secretsencrypt/secrets_encrypt.go
+++ pkg/cli/secretsencrypt/secrets_encrypt.go
@@ -131,19 +131,21 @@ func Status(app *cli.Context) error {
 	}
 
 	var tabBuffer bytes.Buffer
-	w := tabwriter.NewWriter(&tabBuffer, 0, 0, 2, ' ', 0)
-	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, "Active\tKey Type\tName\n")
-	fmt.Fprintf(w, "------\t--------\t----\n")
-	if status.ActiveKey != "" {
-		ak := strings.Split(status.ActiveKey, " ")
-		fmt.Fprintf(w, " *\t%s\t%s\n", ak[0], ak[1])
+	if status.ActiveKey != "" || len(status.InactiveKeys) > 0 {
+		w := tabwriter.NewWriter(&tabBuffer, 0, 0, 2, ' ', 0)
+		fmt.Fprint(w, "\n")
+		fmt.Fprint(w, "Active\tKey Type\tName\n")
+		fmt.Fprint(w, "------\t--------\t----\n")
+		if status.ActiveKey != "" {
+			ak := strings.Split(status.ActiveKey, " ")
+			fmt.Fprintf(w, " *\t%s\t%s\n", ak[0], ak[1])
+		}
+		for _, k := range status.InactiveKeys {
+			ik := strings.Split(k, " ")
+			fmt.Fprintf(w, "\t%s\t%s\n", ik[0], ik[1])
+		}
+		w.Flush()
 	}
-	for _, k := range status.InactiveKeys {
-		ik := strings.Split(k, " ")
-		fmt.Fprintf(w, "\t%s\t%s\n", ik[0], ik[1])
-	}
-	w.Flush()
 	fmt.Println(statusOutput + tabBuffer.String())
 	return nil
 }
