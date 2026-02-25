80d775cddf6043a15c944f62ba4094a8b31c7bdc
Derek Nola <derek.nola@suse.com>
2026-02-25 12:31:13 -0800

    Fix support for E2E commit installs
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/scripts/latest_commit.sh
+++ tests/e2e/scripts/latest_commit.sh
@@ -3,20 +3,25 @@
 branch=$1
 output_file=$2
 # Grabs the last 10 commit SHA's from the given branch, then purges any commits that do not have a passing CI build
-iterations=0
 
-# The VMs take time on startup to hit aws, wait loop until we can
-while ! curl -s --fail https://k3s-ci-builds.s3.amazonaws.com > /dev/null; do
-    ((iterations++))
-    if [ "$iterations" -ge 30 ]; then
-        echo "Unable to hit https://k3s-ci-builds.s3.amazonaws.com"
-        exit 1
+
+# Copied and modified from install.sh
+get_commit_artifact_url() {
+    commit_id=$1
+    github_api_url=https://api.github.com/repos/k3s-io/k3s
+
+    if [ -z "${GITHUB_TOKEN}" ]; then
+        fatal "Installing commit builds requires GITHUB_TOKEN with k3s-io/k3s repo permissions"
     fi
-    sleep 1
-done
 
-if [ -n "$GH_TOKEN" ]; then
-    response=$(curl -s -H "Authorization: token $GH_TOKEN" -H 'Accept: application/vnd.github.v3+json' "https://api.github.com/repos/k3s-io/k3s/commits?per_page=10&sha=$branch")
+    # GET request to the GitHub API to retrieve the Build workflows associated with the commit that have succeeded
+    run_id=$(curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" "${github_api_url}/commits/${commit_id}/check-runs?check_name=build%20%2F%20Build&conclusion=success" | jq -r '[.check_runs | sort_by(.id) | .[].details_url | split("/")[7]] | last')
+    # Extract the artifact ID for the "k3s-amd64" artifact
+    GITHUB_ART_URL=$(curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" "${github_api_url}/actions/runs/${run_id}/artifacts" | jq -r ".artifacts[] | select(.name == \"k3s-amd64\") | .archive_download_url")
+}
+
+if [ -n "$GITHUB_TOKEN" ]; then
+    response=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.v3+json' "https://api.github.com/repos/k3s-io/k3s/commits?per_page=10&sha=$branch")
 else
     response=$(curl -s -H 'Accept: application/vnd.github.v3+json' "https://api.github.com/repos/k3s-io/k3s/commits?per_page=10&sha=$branch")
 fi
@@ -38,8 +43,10 @@ fi
 read -a commits <<< "$commits_str"
 
 for commit in "${commits[@]}"; do
-    if curl -s --fail https://k3s-ci-builds.s3.amazonaws.com/k3s-$commit.sha256sum > /dev/null; then
+    get_commit_artifact_url "$commit"
+    if [ -n "$GITHUB_ART_URL" ]; then
         echo "$commit" > "$output_file"
+        echo "Found valid commit: $commit"
         exit 0
     fi
 done
