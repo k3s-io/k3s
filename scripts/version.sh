883eb1c42da9b3842ef8dba6aa572c8eaf228adb
Brad Davidson <brad.davidson@rancher.com>
2026-03-24 23:07:11 +0000

    Bump helm-controller for job race fix
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 367c38b49ee8b889c60e77c4e6a9214f3bb1abd0)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -68,7 +68,7 @@ fi
 
 VERSION_ROOT="v0.15.0"
 
-VERSION_HELM_JOB="v0.9.14-build20260309"
+VERSION_HELM_JOB="v0.9.15-build20260324"
 
 GO_VERSION_URL="https://raw.githubusercontent.com/kubernetes/kubernetes/${BRANCH_K8S}/.go-version"
 VERSION_GOLANG="go"$(curl -sL "${GO_VERSION_URL}" | tr -d '[:space:]')
