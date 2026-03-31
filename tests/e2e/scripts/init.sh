5e578729b739458fa25bedd4d015de0b91ab3a13
Manuel Buil <mbuil@suse.com>
2026-03-31 18:17:26 +0200

    Verify sha256sum for kubelet and vagrant zip
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- tests/e2e/scripts/init.sh
+++ tests/e2e/scripts/init.sh
@@ -3,18 +3,31 @@
 
 export PATH=$PATH:/usr/local/go/bin/:/usr/local/bin
 echo 'Installing Kubectl'
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
-sudo mv kubectl /usr/local/bin/ && \
+KUBECTL_VERSION=v1.34.6
+KUBECTL_SHA256=3166155b17198c0af34ff5a360bd4d9d58db98bafadc6f3c2a57ae560563cd6
+if ! curl -fsSLo kubectl "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl" || \
+   ! echo "${KUBECTL_SHA256}  kubectl" | sha256sum --check --status; then
+    echo "[ERROR] kubectl download or SHA256 verification failed"
+    exit 1
+fi
+rm -f kubectl.sha256
+sudo mv kubectl /usr/local/bin/
 chmod a+x /usr/local/bin/kubectl
 
 echo 'Installing jq and docker'
 sudo apt-get -y install jq docker.io
 
 echo 'Installing Go'
-GO_VERSION=1.19.1
-wget  --quiet https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
-rm go$GO_VERSION.linux-amd64.tar.gz
+GO_VERSION=1.25.8
+GO_SHA256=ceb5e041bbc3893846bd1614d76cb4681c91dadee579426cf21a63f2d7e03be6
+GO_TARBALL="go${GO_VERSION}.linux-amd64.tar.gz"
+if ! curl -fsSLo "${GO_TARBALL}" "https://dl.google.com/go/${GO_TARBALL}" || \
+   ! echo "${GO_SHA256}  ${GO_TARBALL}" | sha256sum --check --status; then
+    echo "[ERROR] go download or SHA256 verification failed"
+    exit 1
+fi
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "${GO_TARBALL}"
+rm -f "${GO_TARBALL}"
 echo 
 go version
 
@@ -29,10 +42,17 @@ echo "* 10.0.0.0/8 192.168.0.0/16">>/etc/vbox/networks.conf
 
 echo 'Installing vagrant'
 sudo apt-get -y install -f unzip
-curl -O https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_linux_amd64.zip
-unzip vagrant_2.2.19_linux_amd64.zip
+VAGRANT_VERSION=2.2.19
+VAGRANT_ZIP="vagrant_${VAGRANT_VERSION}_linux_amd64.zip"
+VAGRANT_SHA256=a1df4c793902e2b9647a0fd42a23d2363c6900f54b70674b736898f9e48c1200
+curl -fsSLO "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/${VAGRANT_ZIP}"
+if ! echo "${VAGRANT_SHA256}  ${VAGRANT_ZIP}" | sha256sum --check --status; then
+    echo "[ERROR] vagrant download or SHA256 verification failed"
+    exit 1
+fi
+unzip "${VAGRANT_ZIP}"
 sudo mv vagrant /usr/local/bin/
-rm vagrant_2.2.19_linux_amd64.zip
+rm -f "${VAGRANT_ZIP}"
 vagrant --version
 sudo apt-get -y install libarchive-tools
 vagrant plugin install vagrant-k3s vagrant-reload vagrant-scp
