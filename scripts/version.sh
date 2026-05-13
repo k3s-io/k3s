92755d51d6392c23ad71b72f1648f4501f205772
Brad Davidson <brad.davidson@rancher.com>
2026-05-13 21:22:38 +0000

    Bump klipper-helm image tag
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -65,7 +65,7 @@ fi
 
 VERSION_ROOT="v0.15.0"
 
-VERSION_HELM_JOB="v0.9.17-build20260422"
+VERSION_HELM_JOB="v0.10.0-build20260513"
 
 GO_VERSION_URL="https://raw.githubusercontent.com/kubernetes/kubernetes/${VERSION_K8S}/.go-version"
 VERSION_GOLANG="go"$(curl -sL "${GO_VERSION_URL}" | tr -d '[:space:]')
