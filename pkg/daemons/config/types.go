3ef4d90369f5c274eed0d4076b72c030b65c22c3
Eran Markus <eran132@gmail.com>
2026-05-22 17:15:11 +0300

    Use KubeProxyDisabled helper instead of a new config field
    Signed-off-by: Eran Markus <eran132@gmail.com>
    (cherry picked from commit 8655399438ef2e9748e6f223c532e039ed244f6c)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/daemons/config/types.go
+++ pkg/daemons/config/types.go
@@ -153,7 +153,6 @@ type Agent struct {
 	AirgapExtraRegistry     []string
 	DisableCCM              bool
 	DisableNPC              bool
-	DisableKubeProxy        bool
 	MinTLSVersion           string
 	CipherSuites            []string
 	Rootless                bool
