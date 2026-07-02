79b2e9ee84df391eb9722c874ebf231c5a10e0fc
Brad Davidson <brad.davidson@rancher.com>
2026-07-02 23:11:50 +0000

    Add backoff to initial snapshot reconcile
    Backoff starts at 1s and doubles, up to max of 32 seconds
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -90,7 +90,8 @@ func (c *Cluster) Start(ctx context.Context, wg *sync.WaitGroup) error {
 
 					if !c.config.EtcdDisableSnapshots {
 						// do an initial reconcile of snapshots with a fast retry until it succeeds
-						wait.PollUntilContextCancel(ctx, time.Second, true, func(ctx context.Context) (bool, error) {
+						backoff := wait.Backoff{Duration: time.Second, Factor: 2, Steps: 5}
+						backoff.DelayFunc().Until(ctx, true, false, func(ctx context.Context) (bool, error) {
 							if err := c.managedDB.ReconcileSnapshotData(ctx); err != nil {
 								logrus.Errorf("Failed to record snapshots for cluster: %v", err)
 								return false, nil
