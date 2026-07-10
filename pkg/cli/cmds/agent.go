44367b191bb747ff3e5a23657c541a192de614a9
Derek Nola <derek.nola@suse.com>
2026-07-10 12:27:23 -0700

    Bump to 3.10.2
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/cli/cmds/agent.go
+++ pkg/cli/cmds/agent.go
@@ -159,7 +159,7 @@ var (
 		Name:        "pause-image",
 		Usage:       "(agent/runtime) Customized pause image for containerd or docker sandbox",
 		Destination: &AgentConfig.PauseImage,
-		Value:       "rancher/mirrored-pause:3.10",
+		Value:       "rancher/mirrored-pause:3.10.2",
 	}
 	SnapshotterFlag = &cli.StringFlag{
 		Name:        "snapshotter",
