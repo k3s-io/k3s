526a9f2645ee865c00e73a30c567c98826521cbe
Brad Davidson <brad.davidson@rancher.com>
2026-09-16 19:15:01 +0000

    Exclude kms-providers from apiserver readiness check
    Allows the kubelet to start before KMS providers are available, so that the KMS provider may be hosted in a static pod
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/util/api.go
+++ pkg/util/api.go
@@ -104,9 +104,10 @@ func WaitForAPIServerReady(ctx context.Context, kubeconfigPath string, timeout t
 		return err
 	}
 
+	req := restClient.Get().AbsPath("/readyz").Param("exclude", "kms-providers").Param("verbose", "true")
 	err = wait.PollUntilContextTimeout(ctx, time.Second*2, timeout, true, func(ctx context.Context) (bool, error) {
 		// DoRaw returns an error if the response code is < 200 OK or > 206 Partial Content
-		if _, err := restClient.Get().AbsPath("/readyz").Param("verbose", "").DoRaw(ctx); err != nil {
+		if _, err := req.DoRaw(ctx); err != nil {
 			if err.Error() != lastErr.Error() {
 				logrus.Infof("Polling for API server readiness: GET /readyz failed: %v", err)
 			} else {
